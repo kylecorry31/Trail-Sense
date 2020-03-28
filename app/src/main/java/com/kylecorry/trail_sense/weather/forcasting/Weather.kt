cd68641f68980a438bd1bdd3086414ea146b250a
Kyle Corry <kylecorry31@gmail.com>
2020-03-28 06:31:48 -0400

    Change quickly to rapidly - still sounds odd

--- app/src/main/java/com/kylecorry/trail_sense/weather/forcasting/Weather.kt
+++ app/src/main/java/com/kylecorry/trail_sense/weather/forcasting/Weather.kt
@@ -3,8 +3,8 @@ package com.kylecorry.trail_sense.weather.forcasting
 enum class Weather(val description: String) {
 
     // Weather direction
-    ImprovingFast("Quickly improving"),
-    WorseningFast("Quickly worsening"),
+    ImprovingFast("Rapidly improving"),
+    WorseningFast("Rapidly worsening"),
     ImprovingSlow("Improving"),
     WorseningSlow("Worsening"),
     NoChange("Not changing"),
