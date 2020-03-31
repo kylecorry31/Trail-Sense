a8805d1f48fe92ae968a3fdbe05c78d6ff691872
Kyle Corry <kylecorry31@gmail.com>
2020-03-31 07:52:56 -0400

    Adjust sun times for actual

--- app/src/main/java/com/kylecorry/trail_sense/astronomy/sun/SunTimesCalculators.kt
+++ app/src/main/java/com/kylecorry/trail_sense/astronomy/sun/SunTimesCalculators.kt
@@ -3,4 +3,4 @@ package com.kylecorry.trail_sense.astronomy.sun
 class CivilTwilightCalculator : BaseSunTimesCalculator(-6f)
 class NauticalTwilightCalculator : BaseSunTimesCalculator(-12f)
 class AstronomicalTwilightCalculator : BaseSunTimesCalculator(-18f)
-class ActualTwilightCalculator : BaseSunTimesCalculator(-1f)
+class ActualTwilightCalculator : BaseSunTimesCalculator(-0.833f)
