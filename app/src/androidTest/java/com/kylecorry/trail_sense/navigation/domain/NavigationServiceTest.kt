c9f547e4b1e1e2e459c212137af87f1291a86460
Kyle Corry <kylecorry31@gmail.com>
2020-04-23 16:48:52 -0400

    Add test for navigator

---
+++ app/src/androidTest/java/com/kylecorry/trail_sense/navigation/domain/NavigationServiceTest.kt
@@ -0,0 +1,26 @@
+package com.kylecorry.trail_sense.navigation.domain
+
+import android.location.Location
+import com.kylecorry.trail_sense.navigation.domain.compass.Bearing
+import com.kylecorry.trail_sense.shared.Coordinate
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class NavigationServiceTest {
+
+    @Test
+    fun navigate() {
+        val start = Coordinate(0.0, 1.0)
+        val end = Coordinate(10.0, -8.0)
+        val service = NavigationService()
+
+        val vector = service.navigate(start, end)
+
+        val expected = FloatArray(3)
+        Location.distanceBetween(0.0, 1.0, 10.0, -8.0, expected)
+
+        assertEquals(Bearing(expected[1]).value, vector.direction.value, 0.005f)
+        assertEquals(expected[0], vector.distance, 0.005f)
+    }
+}
--- app/src/main/java/com/kylecorry/trail_sense/navigation/domain/NavigationService.kt
+++ app/src/main/java/com/kylecorry/trail_sense/navigation/domain/NavigationService.kt
@@ -1,14 +1,15 @@
 package com.kylecorry.trail_sense.navigation.domain
 
+import android.location.Location
 import com.kylecorry.trail_sense.navigation.domain.compass.Bearing
 import com.kylecorry.trail_sense.shared.Coordinate
 
 class NavigationService {
 
     fun navigate(from: Coordinate, to: Coordinate): NavigationVector {
-        val direction = LocationMath.getBearing(from, to)
-        val distance = LocationMath.getDistance(from, to)
-        return NavigationVector(Bearing(direction), distance)
+        val results = FloatArray(3)
+        Location.distanceBetween(from.latitude, from.longitude, to.latitude, to.longitude, results)
+        return NavigationVector(Bearing(results[1]), results[0])
     }
 
 }
