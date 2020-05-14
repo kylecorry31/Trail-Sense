eb27cb671828a4650f74a7f6b64ae3139ad009df
Kyle Corry <kylecorry31@gmail.com>
2020-05-14 16:12:01 -0400

    Copy OSM URL to clipboard rather than just the coordinates

--- app/src/main/java/com/kylecorry/trail_sense/navigation/infrastructure/LocationSharesheet.kt
+++ app/src/main/java/com/kylecorry/trail_sense/navigation/infrastructure/LocationSharesheet.kt
@@ -8,11 +8,16 @@ class LocationSharesheet(private val context: Context): ILocationSender {
     override fun send(location: Coordinate) {
         val sendIntent = Intent().apply {
             action = Intent.ACTION_SEND
-            putExtra(Intent.EXTRA_TEXT, location.toString())
+            putExtra(Intent.EXTRA_TEXT, getOsmUrl(location))
             type = "text/plain"
         }
 
         val shareIntent = Intent.createChooser(sendIntent, null)
         context.startActivity(shareIntent)
     }
+
+    private fun getOsmUrl(coordinate: Coordinate): String {
+        return "https://www.openstreetmap.org/#map=16/${coordinate.latitude}/${coordinate.longitude}"
+    }
+
 }
