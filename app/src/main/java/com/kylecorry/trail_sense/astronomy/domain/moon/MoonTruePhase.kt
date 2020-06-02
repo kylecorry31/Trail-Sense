2490dac2c7dd3dad879a6473c0f00c4ed8998f21
Kyle Corry <kylecorry31@gmail.com>
2020-06-02 18:51:54 -0400

    Remove moon phase direction

--- app/src/main/java/com/kylecorry/trail_sense/astronomy/domain/moon/MoonTruePhase.kt
+++ app/src/main/java/com/kylecorry/trail_sense/astronomy/domain/moon/MoonTruePhase.kt
@@ -2,13 +2,13 @@ package com.kylecorry.trail_sense.astronomy.domain.moon
 
 private const val truePhaseWidth = 11.25f
 
-enum class MoonTruePhase(val longName: String, val direction: String, val startAngle: Float, val endAngle: Float) {
-    New("New Moon", "New", 360 - truePhaseWidth, truePhaseWidth),
-    WaningCrescent("Waning Crescent", "Waning", truePhaseWidth, 90 - truePhaseWidth),
-    ThirdQuarter("Third Quarter", "Waning", 90 - truePhaseWidth, 90 + truePhaseWidth),
-    WaningGibbous("Waning Gibbous", "Waning", 90 + truePhaseWidth, 180 - truePhaseWidth),
-    Full("Full Moon", "Full", 180 - truePhaseWidth, 180 + truePhaseWidth),
-    WaxingGibbous("Waxing Gibbous", "Waxing", 180 + truePhaseWidth, 270 - truePhaseWidth),
-    FirstQuarter("First Quarter", "Waxing", 270 - truePhaseWidth, 270 + truePhaseWidth),
-    WaxingCrescent("Waxing Crescent", "Waxing", 270 + truePhaseWidth, 360 - truePhaseWidth)
+enum class MoonTruePhase(val longName: String, val startAngle: Float, val endAngle: Float) {
+    New("New Moon", 360 - truePhaseWidth, truePhaseWidth),
+    WaningCrescent("Waning Crescent", truePhaseWidth, 90 - truePhaseWidth),
+    ThirdQuarter("Third Quarter", 90 - truePhaseWidth, 90 + truePhaseWidth),
+    WaningGibbous("Waning Gibbous", 90 + truePhaseWidth, 180 - truePhaseWidth),
+    Full("Full Moon", 180 - truePhaseWidth, 180 + truePhaseWidth),
+    WaxingGibbous("Waxing Gibbous", 180 + truePhaseWidth, 270 - truePhaseWidth),
+    FirstQuarter("First Quarter", 270 - truePhaseWidth, 270 + truePhaseWidth),
+    WaxingCrescent("Waxing Crescent", 270 + truePhaseWidth, 360 - truePhaseWidth)
 }
