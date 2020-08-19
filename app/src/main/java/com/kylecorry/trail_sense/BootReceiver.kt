c4b076a1c24d292d641bd13374b9c3b64fd2797a
Kyle Corry <kylecorry31@gmail.com>
2020-08-19 11:28:33 -0400

    Make the boot receiver class more generic

--- app/src/main/java/com/kylecorry/trail_sense/BootReceiver.kt
+++ app/src/main/java/com/kylecorry/trail_sense/BootReceiver.kt
@@ -1,21 +1,29 @@
-package com.kylecorry.trail_sense.weather.infrastructure
+package com.kylecorry.trail_sense
 
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.kylecorry.trail_sense.astronomy.infrastructure.SunsetAlarmReceiver
 import com.kylecorry.trail_sense.shared.UserPreferences
+import com.kylecorry.trail_sense.weather.infrastructure.BarometerService
 
-class BarometerBootReceiver: BroadcastReceiver() {
+class BootReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        if (intent?.action == "android.intent.action.BOOT_COMPLETED" && context != null){
+        if (intent?.action == Intent.ACTION_BOOT_COMPLETED && context != null){
             val prefs = UserPreferences(context)
+
+            // Start the weather service
             if (prefs.weather.shouldMonitorWeather) {
-                BarometerService.start(context)
+                BarometerService.start(
+                    context
+                )
             } else {
-                BarometerService.stop(context)
+                BarometerService.stop(
+                    context
+                )
             }
 
+            // Start the sunset alarm
             context.sendBroadcast(SunsetAlarmReceiver.intent(context))
         }
     }
