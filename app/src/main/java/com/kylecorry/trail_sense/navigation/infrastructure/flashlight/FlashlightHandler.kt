2f5a12e906cb105076ead4fbfc997cbb6670499e
Kyle Corry <kylecorry31@gmail.com>
2020-11-21 07:38:27 -0500

    Stop flashlight when the system UI button is used
    Closes #287
    
    SOS will keep going, only the torch mode will stop.

--- app/src/main/java/com/kylecorry/trail_sense/navigation/infrastructure/flashlight/FlashlightHandler.kt
+++ app/src/main/java/com/kylecorry/trail_sense/navigation/infrastructure/flashlight/FlashlightHandler.kt
@@ -1,10 +1,38 @@
 package com.kylecorry.trail_sense.navigation.infrastructure.flashlight
 
 import android.content.Context
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CameraManager.TorchCallback
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import androidx.core.content.getSystemService
 import com.kylecorry.trail_sense.navigation.domain.FlashlightState
 import com.kylecorry.trailsensecore.infrastructure.flashlight.Flashlight
 
+
 class FlashlightHandler(private val context: Context) : IFlashlightHandler {
+
+    init {
+        try {
+            val torchCallback: TorchCallback = object : TorchCallback() {
+                override fun onTorchModeChanged(cameraId: String, enabled: Boolean) {
+                    super.onTorchModeChanged(cameraId, enabled)
+                    if (!enabled && FlashlightService.isOn(context)) {
+                        off()
+                    }
+                }
+            }
+
+            context.getSystemService<CameraManager>()?.registerTorchCallback(
+                torchCallback, Handler(
+                    Looper.getMainLooper()
+                )
+            )
+        } catch (e: Exception) {
+            // Do nothing, this isn't a breaking error
+        }
+    }
 
     override fun on() {
         SosService.stop(context)
