214a20ac44834df4bab0b1d1d6ecc362a29c093d
Kyle Corry <kylecorry31@gmail.com>
2020-12-31 18:32:50 -0500

    Add note list

--- app/src/main/java/com/kylecorry/trail_sense/tools/notes/domain/Note.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/notes/domain/Note.kt
@@ -3,6 +3,7 @@ package com.kylecorry.trail_sense.tools.notes.domain
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.time.Instant
 
 @Entity(
     tableName = "notes"
@@ -10,8 +11,12 @@ import androidx.room.PrimaryKey
 data class Note(
     @ColumnInfo(name = "title") val title: String?,
     @ColumnInfo(name = "contents") val contents: String?,
+    @ColumnInfo(name = "created") val createdOn: Long
 ) {
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "_id")
     var id: Long = 0
+
+    val createdInstant: Instant
+        get() = Instant.ofEpochMilli(createdOn)
 }
