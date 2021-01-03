03a4cfc4251f951429a1b6330b98a545386dff6c
Kyle Corry <kylecorry31@gmail.com>
2021-01-03 12:38:48 -0500

    Add method to calculate map distance (no UI)

--- app/src/main/java/com/kylecorry/trail_sense/tools/ruler/ui/RulerFragment.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/ruler/ui/RulerFragment.kt
@@ -7,14 +7,24 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.kylecorry.trail_sense.databinding.FragmentToolRulerBinding
 import com.kylecorry.trail_sense.shared.FormatService
+import com.kylecorry.trail_sense.shared.UserPreferences
+import com.kylecorry.trailsensecore.domain.geo.GeoService
+import com.kylecorry.trailsensecore.domain.units.Distance
+import com.kylecorry.trailsensecore.domain.units.DistanceUnits
 
 class RulerFragment : Fragment() {
     private var _binding: FragmentToolRulerBinding? = null
     private val binding get() = _binding!!
 
     private val formatService by lazy { FormatService(requireContext()) }
+    private val geoService = GeoService()
+    private val prefs by lazy { UserPreferences(requireContext()) }
+
+    private var scaleMode = MapScaleMode.Relational
+    private var currentDistance = Distance(0f, DistanceUnits.Centimeters)
 
     private lateinit var ruler: Ruler
+    private lateinit var units: UserPreferences.DistanceUnits
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,11 +45,49 @@ class RulerFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         ruler.clearTap()
+        units = prefs.distanceUnits
         binding.measurement.text = ""
     }
 
     private fun onRulerTap(centimeters: Float){
         binding.measurement.text = formatService.formatFractionalDistance(centimeters)
+        currentDistance = Distance(centimeters, DistanceUnits.Centimeters)
+        calculateMapDistance()
+    }
+
+    private fun calculateMapDistance(){
+        val displayDistance = when (scaleMode) {
+            MapScaleMode.Relational -> {
+                val scaleFrom: Distance? = Distance(1f, DistanceUnits.Centimeters)
+                val scaleTo: Distance? = Distance(1f, DistanceUnits.Kilometers)
+
+                if (scaleFrom == null || scaleTo == null){
+                    null
+                } else {
+                    val mapDistance = geoService.getMapDistance(currentDistance, scaleFrom, scaleTo)
+                    formatService.formatDistance(mapDistance.distance, scaleTo.units)
+                }
+            }
+            MapScaleMode.Fractional -> {
+                val ratioFrom: Float? = 1f
+                val ratioTo: Float? = 10000f
+
+                if (ratioFrom == null || ratioTo == null){
+                    null
+                } else {
+                    val mapDistance = geoService.getMapDistance(currentDistance, ratioFrom, ratioTo).convertTo(DistanceUnits.Meters)
+                    formatService.formatLargeDistance(mapDistance.distance)
+                }
+            }
+        }
+
+        // TODO: Display the map distance
+    }
+
+
+    private enum class MapScaleMode {
+        Fractional,
+        Relational
     }
 
 }
