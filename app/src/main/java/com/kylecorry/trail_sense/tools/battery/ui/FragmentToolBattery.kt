3a8efba8675bf45c92de94b0c474049f4b0596c0
Kyle Corry <kylecorry31@gmail.com>
2021-01-17 09:16:51 -0500

    Improve battery tool reliability

--- app/src/main/java/com/kylecorry/trail_sense/tools/battery/ui/FragmentToolBattery.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/battery/ui/FragmentToolBattery.kt
@@ -10,6 +10,8 @@ import com.kylecorry.trail_sense.databinding.FragmentToolBatteryBinding
 import com.kylecorry.trail_sense.shared.FormatService
 import com.kylecorry.trail_sense.weather.domain.LowPassFilter
 import com.kylecorry.trailsensecore.infrastructure.sensors.battery.Battery
+import com.kylecorry.trailsensecore.infrastructure.sensors.battery.BatteryChargingMethod
+import com.kylecorry.trailsensecore.infrastructure.sensors.battery.BatteryChargingStatus
 import com.kylecorry.trailsensecore.infrastructure.sensors.battery.BatteryHealth
 import com.kylecorry.trailsensecore.infrastructure.time.Intervalometer
 import kotlin.math.absoluteValue
@@ -23,8 +25,6 @@ class FragmentToolBattery: Fragment() {
     private val formatService by lazy { FormatService(requireContext()) }
     private val battery by lazy { Battery(requireContext()) }
 
-    private var lastReading = 0f
-
     private val intervalometer = Intervalometer {
         update()
     }
@@ -32,7 +32,7 @@ class FragmentToolBattery: Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentToolBatteryBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -62,49 +62,48 @@ class FragmentToolBattery: Fragment() {
     }
 
     private fun update() {
+        // If charging, show up arrow
+        val chargingStatus = battery.chargingStatus
+        when(chargingStatus){
+            BatteryChargingStatus.Charging -> {
+                binding.batteryChargeIndicator.rotation = 0f
+                binding.batteryChargeIndicator.visibility = View.VISIBLE
+            }
+            BatteryChargingStatus.Discharging -> {
+                binding.batteryChargeIndicator.rotation = 180f
+                binding.batteryChargeIndicator.visibility = View.VISIBLE
+            }
+            else -> binding.batteryChargeIndicator.visibility = View.INVISIBLE
+        }
+
+        // If charging and current is negative, invert current
+        val current = battery.current.absoluteValue * if (chargingStatus == BatteryChargingStatus.Charging) 1 else -1
         val capacity = battery.capacity
         val pct = battery.percent.roundToInt()
-        val batteryCurrent = battery.current
+
         binding.batteryPercentage.text = formatService.formatPercentage(pct)
         binding.batteryCapacity.text = formatService.formatBatteryCapacity(capacity)
         binding.batteryCapacity.visibility = if (capacity == 0f) View.GONE else View.VISIBLE
         binding.batteryHealth.text = getString(R.string.battery_health, getHealthString(battery.health))
-
-        if (batteryCurrent != 0f) {
-            val formattedCurrent = formatService.formatCurrent(batteryCurrent.absoluteValue)
-            binding.batteryCurrent.text = when {
-                batteryCurrent > 500 -> getString(R.string.charging_fast, formattedCurrent)
-                batteryCurrent > 0 -> getString(R.string.charging_slow, formattedCurrent)
-                batteryCurrent < -500 -> getString(R.string.discharging_fast, formattedCurrent)
-                else -> getString(R.string.discharging_slow, formattedCurrent)
-            }
-        }
-
         binding.batteryLevelBar.progress = pct
 
-        if (lastReading == 0f){
-            lastReading = capacity
+        if (current != 0f) {
+            val formattedCurrent = formatService.formatCurrent(current.absoluteValue)
+            binding.batteryCurrent.text = when {
+                current > 500 -> getString(R.string.charging_fast, formattedCurrent)
+                current > 0 -> getString(R.string.charging_slow, formattedCurrent)
+                current < -500 -> getString(R.string.discharging_fast, formattedCurrent)
+                else -> getString(R.string.discharging_slow, formattedCurrent)
+            }
+        } else {
+            val chargeMethod = battery.chargingMethod
+            binding.batteryCurrent.text = when(chargeMethod){
+                BatteryChargingMethod.AC -> getString(R.string.charging_fast, getString(R.string.battery_power_ac))
+                BatteryChargingMethod.USB -> getString(R.string.charging_slow, getString(R.string.battery_power_usb))
+                BatteryChargingMethod.Wireless -> getString(R.string.charging_wireless, getString(R.string.battery_power_wireless))
+                else -> ""
+            }
         }
-
-        if (capacity - lastReading > 0){
-            // Increasing
-            binding.batteryChargeIndicator.rotation = 0f
-            binding.batteryChargeIndicator.visibility = View.VISIBLE
-        } else if (capacity - lastReading < 0){
-            // Decreasing
-            binding.batteryChargeIndicator.rotation = 180f
-            binding.batteryChargeIndicator.visibility = View.VISIBLE
-        } else if (capacity == 0f && battery.charging){
-            // Increasing
-            binding.batteryChargeIndicator.rotation = 0f
-            binding.batteryChargeIndicator.visibility = View.VISIBLE
-        } else if (capacity == 0f && !battery.charging){
-            // Decreasing
-            binding.batteryChargeIndicator.rotation = 180f
-            binding.batteryChargeIndicator.visibility = View.VISIBLE
-        }
-
-        lastReading = capacity
     }
 
 
