1a9383f8db1a71d8bfba9b0a7089941280a8d309
Kyle Corry <kylecorry31@gmail.com>
2021-01-23 10:31:24 -0500

    Improve location sharing

--- app/src/main/java/com/kylecorry/trail_sense/navigation/infrastructure/share/BeaconGeoSender.kt
+++ app/src/main/java/com/kylecorry/trail_sense/navigation/infrastructure/share/BeaconGeoSender.kt
@@ -1,12 +1,18 @@
 package com.kylecorry.trail_sense.navigation.infrastructure.share
 
 import android.content.Context
+import android.content.Intent
+import com.kylecorry.trail_sense.R
 import com.kylecorry.trailsensecore.domain.navigation.Beacon
 import com.kylecorry.trailsensecore.infrastructure.system.IntentUtils
 
 class BeaconGeoSender(private val context: Context): IBeaconSender {
 
     override fun send(beacon: Beacon) {
-        context.startActivity(IntentUtils.geo(beacon.coordinate))
+        val intent = IntentUtils.geo(beacon.coordinate)
+        val chooser = Intent.createChooser(intent, context.getString(R.string.open_beacon_in_maps))
+        if (intent.resolveActivity(context.packageManager) != null) {
+            context.startActivity(chooser)
+        }
     }
 }
