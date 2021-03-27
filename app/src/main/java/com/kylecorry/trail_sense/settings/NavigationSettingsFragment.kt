b7c37218c9fb000501035030ec8725c77f1e2f4f
Kyle Corry <kylecorry31@gmail.com>
2021-03-27 07:41:00 -0400

    Use distance picker for nearby radius

--- app/src/main/java/com/kylecorry/trail_sense/settings/NavigationSettingsFragment.kt
+++ app/src/main/java/com/kylecorry/trail_sense/settings/NavigationSettingsFragment.kt
@@ -2,34 +2,30 @@ package com.kylecorry.trail_sense.settings
 
 import android.os.Bundle
 import android.text.InputType
-import androidx.preference.EditTextPreference
 import androidx.preference.ListPreference
+import androidx.preference.Preference
 import androidx.preference.SwitchPreferenceCompat
 import com.kylecorry.trail_sense.R
-import com.kylecorry.trail_sense.shared.FormatService
-import com.kylecorry.trail_sense.shared.QuickActionUtils
-import com.kylecorry.trail_sense.shared.UserPreferences
+import com.kylecorry.trail_sense.shared.*
+import com.kylecorry.trail_sense.shared.DistanceUtils.toRelativeDistance
 import com.kylecorry.trail_sense.tools.backtrack.infrastructure.BacktrackScheduler
+import com.kylecorry.trailsensecore.domain.units.Distance
 import com.kylecorry.trailsensecore.domain.units.DistanceUnits
 import com.kylecorry.trailsensecore.domain.units.UnitService
 import com.kylecorry.trailsensecore.infrastructure.persistence.Cache
 
 class NavigationSettingsFragment : CustomPreferenceFragment() {
 
-    private var prefMaxBeaconDistanceKm: EditTextPreference? = null
-    private var prefMaxBeaconDistanceMi: EditTextPreference? = null
+    private var prefNearbyRadius: Preference? = null
     private var prefBacktrack: SwitchPreferenceCompat? = null
     private var prefLeftQuickAction: ListPreference? = null
     private var prefRightQuickAction: ListPreference? = null
-    private val unitService = UnitService()
-    private val formatService by lazy { FormatService(requireContext()) }
+    private val formatService by lazy { FormatServiceV2(requireContext()) }
 
     private lateinit var prefs: UserPreferences
-    private val cache by lazy { Cache(requireContext()) }
 
     private fun bindPreferences() {
-        prefMaxBeaconDistanceKm = editText(R.string.pref_max_beacon_distance)
-        prefMaxBeaconDistanceMi = editText(R.string.pref_max_beacon_distance_miles)
+        prefNearbyRadius = preference(R.string.pref_nearby_radius_holder)
         prefBacktrack = switch(R.string.pref_backtrack_enabled)
         prefLeftQuickAction = list(R.string.pref_navigation_quick_action_left)
         prefRightQuickAction = list(R.string.pref_navigation_quick_action_right)
@@ -78,71 +74,18 @@ class NavigationSettingsFragment : CustomPreferenceFragment() {
 
         val distanceUnits = prefs.distanceUnits
 
-        if (distanceUnits == UserPreferences.DistanceUnits.Feet) {
-            prefMaxBeaconDistanceKm?.isVisible = false
-            prefMaxBeaconDistanceMi?.isVisible = true
-        } else {
-            prefMaxBeaconDistanceKm?.isVisible = true
-            prefMaxBeaconDistanceMi?.isVisible = false
-        }
-
-
-        val maxDistance = prefs.navigation.maxBeaconDistance
-        prefMaxBeaconDistanceMi?.summary = formatService.formatDistance(
-            unitService.convert(
-                maxDistance,
-                DistanceUnits.Meters,
-                DistanceUnits.Miles
-            ), DistanceUnits.Miles
-        )
-        prefMaxBeaconDistanceKm?.summary = formatService.formatDistance(
-            unitService.convert(
-                maxDistance,
-                DistanceUnits.Meters,
-                DistanceUnits.Kilometers
-            ), DistanceUnits.Kilometers
-        )
-
-        prefMaxBeaconDistanceMi?.setOnBindEditTextListener { editText ->
-            editText.inputType = InputType.TYPE_CLASS_NUMBER.or(InputType.TYPE_NUMBER_FLAG_DECIMAL)
-        }
-
-        prefMaxBeaconDistanceKm?.setOnBindEditTextListener { editText ->
-            editText.inputType = InputType.TYPE_CLASS_NUMBER.or(InputType.TYPE_NUMBER_FLAG_DECIMAL)
-        }
-
-        prefMaxBeaconDistanceMi?.setOnPreferenceChangeListener { _, newValue ->
-            val miles = newValue.toString().toFloatOrNull() ?: 62f
-            prefs.navigation.maxBeaconDistance =
-                unitService.convert(miles, DistanceUnits.Miles, DistanceUnits.Meters)
-            prefMaxBeaconDistanceMi?.summary =
-                formatService.formatDistance(miles, DistanceUnits.Miles)
-            prefMaxBeaconDistanceKm?.summary = formatService.formatDistance(
-                unitService.convert(
-                    miles,
-                    DistanceUnits.Miles,
-                    DistanceUnits.Kilometers
-                ), DistanceUnits.Kilometers
-            )
-            true
-        }
-
-        prefMaxBeaconDistanceKm?.setOnPreferenceChangeListener { _, newValue ->
-            val km = newValue.toString().toFloatOrNull() ?: 100f
-            cache.putString(
-                getString(R.string.pref_max_beacon_distance_miles),
-                unitService.convert(km, DistanceUnits.Kilometers, DistanceUnits.Miles)
-                    .toString()
-            )
-            prefMaxBeaconDistanceMi?.summary = formatService.formatDistance(
-                unitService.convert(
-                    km,
-                    DistanceUnits.Kilometers,
-                    DistanceUnits.Miles
-                ), DistanceUnits.Miles
-            )
-            prefMaxBeaconDistanceKm?.summary =
-                formatService.formatDistance(km, DistanceUnits.Kilometers)
+        prefNearbyRadius?.setOnPreferenceClickListener {
+            CustomUiUtils.pickDistance(
+                requireContext(),
+                if (distanceUnits == UserPreferences.DistanceUnits.Meters) listOf(DistanceUnits.Meters, DistanceUnits.Kilometers) else listOf(DistanceUnits.Feet, DistanceUnits.Miles, DistanceUnits.NauticalMiles),
+                Distance.meters(userPrefs.navigation.maxBeaconDistance).convertTo(userPrefs.baseDistanceUnits).toRelativeDistance(),
+                it.title.toString()
+            ) { distance ->
+                if (distance != null && distance.distance > 0){
+                    userPrefs.navigation.maxBeaconDistance = distance.meters().distance
+                    updateNearbyRadius()
+                }
+            }
             true
         }
 
@@ -150,5 +93,14 @@ class NavigationSettingsFragment : CustomPreferenceFragment() {
             ?.setOnBindEditTextListener { editText ->
                 editText.inputType = InputType.TYPE_CLASS_NUMBER
             }
+
+        updateNearbyRadius()
+    }
+
+    private fun updateNearbyRadius() {
+        prefNearbyRadius?.summary = formatService.formatDistance(
+            Distance.meters(prefs.navigation.maxBeaconDistance).convertTo(prefs.baseDistanceUnits).toRelativeDistance(),
+            2
+        )
     }
 }
