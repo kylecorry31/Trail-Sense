2c07d3d42e1b2c39071ea5df1ad63555ae51ae6d
qwerty287 <ndev@web.de>
2021-04-18 15:00:02 +0200

    improved beacon ShareSheet

--- app/src/main/java/com/kylecorry/trail_sense/navigation/infrastructure/share/BeaconSharesheet.kt
+++ app/src/main/java/com/kylecorry/trail_sense/navigation/infrastructure/share/BeaconSharesheet.kt
@@ -2,6 +2,7 @@ package com.kylecorry.trail_sense.navigation.infrastructure.share
 
 import android.content.Context
 import android.content.Intent
+import com.kylecorry.trail_sense.R
 import com.kylecorry.trailsensecore.domain.geo.Coordinate
 import com.kylecorry.trailsensecore.domain.navigation.Beacon
 
@@ -9,7 +10,7 @@ class BeaconSharesheet(private val context: Context): IBeaconSender {
     override fun send(beacon: Beacon) {
         val sendIntent = Intent().apply {
             action = Intent.ACTION_SEND
-            putExtra(Intent.EXTRA_TEXT, getOsmUrl(beacon.coordinate))
+            putExtra(Intent.EXTRA_TEXT, getShareText(beacon))
             // TODO: Share other details
             type = "text/plain"
         }
@@ -18,8 +19,25 @@ class BeaconSharesheet(private val context: Context): IBeaconSender {
         context.startActivity(shareIntent)
     }
 
+    private fun getShareText(beacon: Beacon):String {
+        val name = beacon.name
+        val coordinate = beacon.coordinate
+        val osmUrl = getOsmUrl(coordinate)
+        val coordinateString = getCoordinateLatLon(coordinate)
+        val coordinateUtm = getCoordinateUtm(coordinate)
+        return "${name}\n${coordinateString}\nUTM: ${coordinateUtm}\n${context.getString(R.string.maps)}: $osmUrl"
+    }
+
     private fun getOsmUrl(coordinate: Coordinate): String {
         return "https://www.openstreetmap.org/#map=16/${coordinate.latitude}/${coordinate.longitude}"
+    }
+
+    private fun getCoordinateLatLon(coordinate: Coordinate): String {
+        return "${coordinate.latitude}, ${coordinate.longitude}"
+    }
+
+    private fun getCoordinateUtm(coordinate: Coordinate): String {
+        return coordinate.toUTM()
     }
 
 }
