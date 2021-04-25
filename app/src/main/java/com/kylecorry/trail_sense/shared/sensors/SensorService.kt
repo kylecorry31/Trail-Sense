05cdf301d07eb7a12f25a368db3aa2207440aac0
Kyle Corry <kylecorry31@gmail.com>
2021-04-25 14:59:07 -0400

    Update from hotfix

--- app/src/main/java/com/kylecorry/trail_sense/shared/sensors/SensorService.kt
+++ app/src/main/java/com/kylecorry/trail_sense/shared/sensors/SensorService.kt
@@ -174,7 +174,7 @@ class SensorService(ctx: Context) {
         if (!hasLocationPermission(background)){
             return NullCellSignalSensor()
         }
-        return CellSignalSensorHotfix(context)
+        return CellSignalSensor(context)
     }
 
     fun getGravity(): IAccelerometer {
