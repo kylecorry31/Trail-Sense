f80412042436b1ae350401acebe7e8c05e65dd9f
qwerty287 <ndev@web.de>
2021-05-14 11:18:43 +0200

    show TS on lock screen while navigating

--- app/src/main/java/com/kylecorry/trail_sense/navigation/ui/NavigatorFragment.kt
+++ app/src/main/java/com/kylecorry/trail_sense/navigation/ui/NavigatorFragment.kt
@@ -1,6 +1,7 @@
 package com.kylecorry.trail_sense.navigation.ui
 
 import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -649,6 +650,15 @@ class NavigatorFragment : BoundFragment<ActivityNavigatorBinding>() {
         binding.location.text = formatService.formatLocation(gps.location)
 
         updateNavigationButton()
+
+        // show on lock screen
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            if (destination != null) {
+                activity?.setShowWhenLocked(true)
+            } else {
+                activity?.setShowWhenLocked(false)
+            }
+        }
     }
 
     private fun shouldShowLinearCompass(): Boolean {
