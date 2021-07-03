adae87fdcbdfb9ccaedda22fbe8fcd8e9b45bf67
Kyle Corry <kylecorry31@gmail.com>
2021-07-03 10:15:39 -0400

    Use geo service for map urls

--- app/src/main/java/com/kylecorry/trail_sense/navigation/infrastructure/share/BeaconSharesheet.kt
+++ app/src/main/java/com/kylecorry/trail_sense/navigation/infrastructure/share/BeaconSharesheet.kt
@@ -3,10 +3,16 @@ package com.kylecorry.trail_sense.navigation.infrastructure.share
 import android.content.Context
 import android.content.Intent
 import com.kylecorry.trail_sense.R
+import com.kylecorry.trail_sense.shared.UserPreferences
 import com.kylecorry.trailsensecore.domain.geo.Coordinate
+import com.kylecorry.trailsensecore.domain.geo.GeoService
 import com.kylecorry.trailsensecore.domain.navigation.Beacon
 
 class BeaconSharesheet(private val context: Context): IBeaconSender {
+
+    private val geoService = GeoService()
+    private val prefs by lazy { UserPreferences(context) }
+
     override fun send(beacon: Beacon) {
         val sendIntent = Intent().apply {
             action = Intent.ACTION_SEND
@@ -22,14 +28,10 @@ class BeaconSharesheet(private val context: Context): IBeaconSender {
     private fun getShareText(beacon: Beacon):String {
         val name = beacon.name
         val coordinate = beacon.coordinate
-        val osmUrl = getOsmUrl(coordinate)
+        val mapUrl = geoService.getMapUrl(coordinate, prefs.mapSite)
         val coordinateString = getCoordinateLatLon(coordinate)
         val coordinateUtm = getCoordinateUtm(coordinate)
-        return "${name}\n\n${coordinateString}\n\n${context.getString(R.string.coordinate_format_utm)}: ${coordinateUtm}\n\n${context.getString(R.string.maps)}: $osmUrl"
-    }
-
-    private fun getOsmUrl(coordinate: Coordinate): String {
-        return "https://www.openstreetmap.org/#map=16/${coordinate.latitude}/${coordinate.longitude}"
+        return "${name}\n\n${coordinateString}\n\n${context.getString(R.string.coordinate_format_utm)}: ${coordinateUtm}\n\n${context.getString(R.string.maps)}: $mapUrl"
     }
 
     private fun getCoordinateLatLon(coordinate: Coordinate): String {
