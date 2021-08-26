7e80a8bd2cbe38a4f4722ce3f9c7e100870c99c9
Kyle Corry <kylecorry31@gmail.com>
2021-08-26 09:50:15 -0400

    Delete RequestCodes.kt

--- app/src/main/java/com/kylecorry/trail_sense/RequestCodes.kt
+++
@@ -1,7 +0,0 @@
-package com.kylecorry.trail_sense
-
-object RequestCodes {
-    // Main Activity
-    const val REQUEST_CODE_LOCATION_PERMISSION = 1
-    const val REQUEST_CODE_BACKGROUND_LOCATION_PERMISSION = 2
-}
