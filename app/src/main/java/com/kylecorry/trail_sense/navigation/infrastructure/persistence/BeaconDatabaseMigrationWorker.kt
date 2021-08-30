5d42cbf8b3fc89b9cce0c19da689355d9199a6db
Kyle Corry <kylecorry31@gmail.com>
2021-08-30 13:12:13 -0400

    Delete old beacon migration code

--- app/src/main/java/com/kylecorry/trail_sense/navigation/infrastructure/persistence/BeaconDatabaseMigrationWorker.kt
+++ app/src/main/java/com/kylecorry/trail_sense/navigation/infrastructure/persistence/BeaconDatabaseMigrationWorker.kt
@@ -4,9 +4,6 @@ import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.kylecorry.andromeda.preferences.Preferences
-import com.kylecorry.trail_sense.navigation.domain.BeaconEntity
-import com.kylecorry.trail_sense.navigation.domain.BeaconGroupEntity
-import com.kylecorry.trail_sense.navigation.infrastructure.database.OldBeaconRepo
 import com.kylecorry.trail_sense.navigation.ui.NavigatorFragment
 
 class BeaconDatabaseMigrationWorker(
@@ -15,49 +12,8 @@ class BeaconDatabaseMigrationWorker(
 ) : CoroutineWorker(context, workerParams) {
     override suspend fun doWork(): Result {
         try {
-            val oldDb = OldBeaconRepo.getInstance(context)
-            val oldBeacons = oldDb.get()
-            val oldGroups = oldDb.getGroups()
-
-            val beaconGroupEntities =
-                oldGroups.map { BeaconGroupEntity.from(it) }
-
-            val db = BeaconRepo.getInstance(context)
-
-            val groupIdMap = mutableMapOf<Long, Long>()
-
-            beaconGroupEntities.forEach {
-                val oldId = it.id
-                val newId = db.addBeaconGroup(it.also { it.id = 0 })
-                groupIdMap[oldId] = newId
-            }
-
-            val beaconEntities = oldBeacons.map {
-                BeaconEntity.from(it).copy(
-                    beaconGroupId = when {
-                        it.beaconGroupId == null -> {
-                            null
-                        }
-                        groupIdMap.containsKey(it.beaconGroupId) -> {
-                            groupIdMap[it.beaconGroupId]
-                        }
-                        else -> {
-                            null
-                        }
-                    }
-                ).also {
-                    it.id = 0
-                }
-            }
-
-            beaconEntities.forEach {
-                db.addBeacon(it)
-            }
-
             val cache = Preferences(context)
             cache.remove(NavigatorFragment.LAST_BEACON_ID)
-
-
             context.deleteDatabase("survive")
         } catch (e: Exception) {
             // Could not migrate
