2615cf047b027da3981244f46cc4cf8c572f6167
Kyle Corry <kylecorry31@gmail.com>
2021-09-05 18:02:11 -0400

    Update GPSDeclinationStrategy.kt

--- app/src/main/java/com/kylecorry/trail_sense/shared/declination/GPSDeclinationStrategy.kt
+++ app/src/main/java/com/kylecorry/trail_sense/shared/declination/GPSDeclinationStrategy.kt
@@ -2,11 +2,12 @@ package com.kylecorry.trail_sense.shared.declination
 
 import com.kylecorry.andromeda.location.IGPS
 import com.kylecorry.sol.science.geology.GeologyService
+import com.kylecorry.sol.science.geology.IGeologyService
 
-class GPSDeclinationStrategy(private val gps: IGPS) : IDeclinationStrategy {
-
-    private val geology = GeologyService()
-
+class GPSDeclinationStrategy(
+    private val gps: IGPS,
+    private val geology: IGeologyService = GeologyService()
+) : IDeclinationStrategy {
     override fun getDeclination(): Float {
         return geology.getMagneticDeclination(gps.location, gps.altitude)
     }
