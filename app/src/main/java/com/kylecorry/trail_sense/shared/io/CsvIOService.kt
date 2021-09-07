d41b5f446f695e798b2307c93097cadf1cb990b0
Kyle Corry <kylecorry31@gmail.com>
2021-09-07 17:56:15 -0400

    Add CSV IO Service

---
+++ app/src/main/java/com/kylecorry/trail_sense/shared/io/CsvIOService.kt
@@ -0,0 +1,19 @@
+package com.kylecorry.trail_sense.shared.io
+
+import com.kylecorry.andromeda.csv.CSVConvert
+
+class CsvIOService(private val uriPicker: UriPicker, private val uriService: UriService) :
+    IOService<List<List<String>>> {
+    override suspend fun export(data: List<List<String>>, filename: String): Boolean {
+        val uri = uriPicker.create(filename, "text/csv") ?: return false
+        val csvString = CSVConvert.toCSV(data)
+        return uriService.write(uri, csvString)
+    }
+
+    override suspend fun import(): List<List<String>>? {
+        val uri = uriPicker.open(listOf("text/csv")) ?: return null
+        val csvString = uriService.read(uri) ?: return null
+        return CSVConvert.parse(csvString)
+    }
+
+}
