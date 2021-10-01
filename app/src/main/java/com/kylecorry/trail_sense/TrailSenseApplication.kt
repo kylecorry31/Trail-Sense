2279fedda2881b28c6d4b4720948dfe145a0dac4
Kyle Corry <kylecorry31@gmail.com>
2021-10-01 15:20:14 -0400

    Run scheduler every day

--- app/src/main/java/com/kylecorry/trail_sense/TrailSenseApplication.kt
+++ app/src/main/java/com/kylecorry/trail_sense/TrailSenseApplication.kt
@@ -2,7 +2,6 @@ package com.kylecorry.trail_sense
 
 import android.app.Application
 import android.util.Log
-import com.kylecorry.andromeda.jobs.TaskSchedulerFactory
 import com.kylecorry.trail_sense.settings.migrations.PreferenceMigrator
 import com.kylecorry.trail_sense.shared.database.RepoCleanupWorker
 import java.time.Duration
@@ -14,11 +13,7 @@ class TrailSenseApplication : Application() {
         Log.d("TrailSenseApplication", "onCreate")
         NotificationChannels.createChannels(this)
         PreferenceMigrator.getInstance().migrate(this)
-
-        // TODO: Run this every day / when Trail Sense is opened
-        val scheduler =
-            TaskSchedulerFactory(this).deferrable(RepoCleanupWorker::class.java, 2739523)
-        scheduler.schedule(Duration.ZERO)
+        RepoCleanupWorker.scheduler(this).schedule(Duration.ZERO)
     }
 
 }
--- app/src/main/java/com/kylecorry/trail_sense/shared/database/RepoCleanupWorker.kt
+++ app/src/main/java/com/kylecorry/trail_sense/shared/database/RepoCleanupWorker.kt
@@ -3,12 +3,15 @@ package com.kylecorry.trail_sense.shared.database
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.kylecorry.andromeda.jobs.ITaskScheduler
+import com.kylecorry.andromeda.jobs.TaskSchedulerFactory
 import com.kylecorry.trail_sense.shared.UserPreferences
 import com.kylecorry.trail_sense.tools.backtrack.infrastructure.persistence.WaypointRepo
 import com.kylecorry.trail_sense.weather.infrastructure.clouds.CloudObservationRepo
 import com.kylecorry.trail_sense.weather.infrastructure.persistence.PressureRepo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.time.Duration
 import java.time.Instant
 
 class RepoCleanupWorker(private val context: Context, params: WorkerParameters) :
@@ -25,6 +28,19 @@ class RepoCleanupWorker(private val context: Context, params: WorkerParameters) 
         val pressure = PressureRepo.getInstance(context)
         pressure.deleteOlderThan(Instant.now().minus(PressureRepo.PRESSURE_HISTORY_DURATION))
 
+        scheduler(context).schedule(Duration.ofDays(1))
+
         Result.success()
     }
+
+
+    companion object {
+        fun scheduler(context: Context): ITaskScheduler {
+            return TaskSchedulerFactory(context.applicationContext).deferrable(
+                RepoCleanupWorker::class.java,
+                2739523
+            )
+        }
+    }
+
 }
