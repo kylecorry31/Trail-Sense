b5bc3b9c56c88b2bc683038df17ac376199d7576
Ebrahim Byagowi <ebrahim@gnu.org>
2021-10-02 23:10:29 +0330

    Upgrade to Material 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,7 +76,7 @@ dependencies {
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
     implementation("androidx.camera:camera-view:1.0.0-alpha30")
-    implementation("com.google.android.material:material:1.4.0")
+    implementation("com.google.android.material:material:1.5.0-beta01")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
 
     // Sol
