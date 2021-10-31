6b0d62305c440be45e61333adc060ebc50eededb
Kyle Corry <kylecorry31@gmail.com>
2021-10-31 08:18:32 -0400

    Fix arrow path effect

--- app/src/main/java/com/kylecorry/trail_sense/shared/paths/ArrowPathLineDrawerStrategy.kt
+++ app/src/main/java/com/kylecorry/trail_sense/shared/paths/ArrowPathLineDrawerStrategy.kt
@@ -1,14 +1,12 @@
 package com.kylecorry.trail_sense.shared.paths
 
+import com.kylecorry.andromeda.canvas.ArrowPathEffect
 import com.kylecorry.andromeda.canvas.CanvasView
 import com.kylecorry.trail_sense.shared.canvas.PixelLine
 
 class ArrowPathLineDrawerStrategy : IPathLineDrawerStrategy {
     override fun draw(canvas: CanvasView, line: PixelLine, strokeScale: Float) {
-        val arrow = PathEffectFactory().getArrowPathEffect(
-            line.start.distanceTo(line.end),
-            strokeScale
-        )
+        val arrow = ArrowPathEffect(6f / strokeScale, 10f / strokeScale)
         canvas.apply {
             pathEffect(arrow)
             noStroke()
