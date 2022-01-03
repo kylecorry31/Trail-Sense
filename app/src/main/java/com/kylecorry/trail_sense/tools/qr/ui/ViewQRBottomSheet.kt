a36bc0ef5f1f613feae5e9f446fc233e1c083d00
Kyle Corry <kylecorry31@gmail.com>
2022-01-03 07:18:36 -0500

    Set max QR length

--- app/src/main/java/com/kylecorry/trail_sense/tools/qr/ui/ViewQRBottomSheet.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/qr/ui/ViewQRBottomSheet.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.kylecorry.andromeda.alerts.toast
 import com.kylecorry.andromeda.core.system.Resources
 import com.kylecorry.andromeda.fragments.BoundBottomSheetDialogFragment
 import com.kylecorry.andromeda.qr.QR
+import com.kylecorry.trail_sense.R
 import com.kylecorry.trail_sense.databinding.FragmentQrShareSheetBinding
 
 class ViewQRBottomSheet(
@@ -26,7 +28,12 @@ class ViewQRBottomSheet(
         }
         binding.qrTitle.text = title
         val size = Resources.dp(requireContext(), 250f).toInt()
-        val bitmap = QR.encode(text, size, size)
+
+        if (text.length > MAX_LENGTH){
+            toast(getString(R.string.qr_text_too_long))
+        }
+
+        val bitmap = QR.encode(text.take(MAX_LENGTH), size, size)
         binding.qrCode.setImageBitmap(bitmap)
     }
 
@@ -36,6 +43,10 @@ class ViewQRBottomSheet(
         container: ViewGroup?
     ): FragmentQrShareSheetBinding {
         return FragmentQrShareSheetBinding.inflate(layoutInflater, container, false)
+    }
+
+    companion object {
+        const val MAX_LENGTH = 1000
     }
 
 }
