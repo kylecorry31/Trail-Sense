6bfaf5f53f77e4c1159617a6017e1291cd48c811
Kyle Corry <kylecorry31@gmail.com>
2022-01-09 06:43:46 -0500

    Improve water level estimation outside tide table

--- app/src/main/java/com/kylecorry/trail_sense/tools/tides/domain/waterlevel/TideClockWaterLevelCalculator.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/tides/domain/waterlevel/TideClockWaterLevelCalculator.kt
@@ -3,25 +3,31 @@ package com.kylecorry.trail_sense.tools.tides.domain.waterlevel
 import com.kylecorry.sol.math.SolMath.toRadians
 import com.kylecorry.sol.science.oceanography.Tide
 import com.kylecorry.sol.science.oceanography.TideConstituent
+import com.kylecorry.sol.science.oceanography.TideFrequency
 import com.kylecorry.sol.science.oceanography.TideType
 import com.kylecorry.trail_sense.tools.tides.domain.Wave
 import java.time.Duration
 import java.time.ZonedDateTime
 
-class TideClockWaterLevelCalculator(private val reference: Tide) : IWaterLevelCalculator {
+class TideClockWaterLevelCalculator(
+    private val reference: Tide,
+    private val frequency: TideFrequency = TideFrequency.Semidiurnal,
+    private val amplitude: Float = 1f,
+    private val z0: Float = 0f
+) : IWaterLevelCalculator {
 
     private val wave = getWave()
 
     override fun calculate(time: ZonedDateTime): Float {
-        // TODO: Handle if it is diurnal
         val t = Duration.between(reference.time, time).seconds / 3600f
         return wave.cosine(t)
     }
 
     private fun getWave(): Wave {
-        val frequency = TideConstituent.M2.speed
-        val amplitude = if (reference.type == TideType.Low) -1f else 1f
-        return Wave(amplitude, frequency.toRadians(), 0f, 0f)
+        val frequency =
+            if (frequency == TideFrequency.Semidiurnal) TideConstituent.M2.speed else TideConstituent.K1.speed
+        val amplitude = if (reference.type == TideType.Low) -amplitude else amplitude
+        return Wave(amplitude, frequency.toRadians(), 0f, z0)
     }
 
 }
--- app/src/main/java/com/kylecorry/trail_sense/tools/tides/domain/waterlevel/TideTableWaterLevelCalculator.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/tides/domain/waterlevel/TideTableWaterLevelCalculator.kt
@@ -2,61 +2,100 @@ package com.kylecorry.trail_sense.tools.tides.domain.waterlevel
 
 import com.kylecorry.sol.math.SolMath.toRadians
 import com.kylecorry.sol.science.oceanography.Tide
+import com.kylecorry.sol.science.oceanography.TideConstituent
 import com.kylecorry.sol.science.oceanography.TideType
 import com.kylecorry.trail_sense.tools.tides.domain.TideTable
 import com.kylecorry.trail_sense.tools.tides.domain.Wave
 import java.time.Duration
 import java.time.ZonedDateTime
 import kotlin.math.abs
+import kotlin.math.absoluteValue
 import kotlin.math.min
 
-class TideTableWaterLevelCalculator(private val table: TideTable) : IWaterLevelCalculator {
+class TideTableWaterLevelCalculator(table: TideTable) : IWaterLevelCalculator {
+
+    private val tides = table.tides.sortedBy { it.time }
 
     override fun calculate(time: ZonedDateTime): Float {
-        if (table.tides.isEmpty()) {
+        if (tides.isEmpty()) {
             return 0f
         }
 
-        if (table.tides.size == 1) {
-            val tide = table.tides[0]
+        if (tides.size == 1) {
+            val tide = tides[0]
+            // TODO: Let the user specify if this should be diurnal or semidiurnal
             return TideClockWaterLevelCalculator(tide).calculate(time)
         }
 
+        val t = Duration.between(tides[0].time, time).seconds / 3600f
+        return getWave(time).cosine(t)
+    }
 
-        val sortedTides = table.tides.sortedBy { it.time }
+    private fun getWave(time: ZonedDateTime): Wave {
         var idx = -1
 
-        for (i in 0 until sortedTides.lastIndex) {
-            if (sortedTides[i].time <= time && sortedTides[i + 1].time >= time) {
+        for (i in 0 until tides.lastIndex) {
+            if (tides[i].time <= time && tides[i + 1].time >= time) {
                 idx = i
                 break
             }
         }
 
-        val t = Duration.between(sortedTides[0].time, time).seconds / 3600f
-
-        if (idx == -1) {
-            // TODO: Maybe fall back to the tide clock
-            idx = if (time <= sortedTides[0].time) {
-                0
+        return if (idx == -1) {
+            if (time <= tides[0].time) {
+                getComputedWaveFrom(tides.first())
             } else {
-                sortedTides.lastIndex - 1
+                getComputedWaveFrom(tides.last())
             }
+        } else {
+            getWaveBetween(
+                tides[idx],
+                tides[idx + 1]
+            )
         }
-        return getWaveBetween(
-            sortedTides[idx],
-            sortedTides[idx + 1],
-            sortedTides[0].time
-        ).cosine(t)
     }
 
-    private fun getWaveBetween(tide1: Tide, tide2: Tide, reference: ZonedDateTime): Wave {
+    private fun getAverageAmplitude(): Float {
+        val averageHigh =
+            tides.sumOf { if (it.type == TideType.High) it.height.toDouble() else 0.0 }
+        val averageLow = tides.sumOf { if (it.type == TideType.Low) it.height.toDouble() else 0.0 }
+        return (averageHigh - averageLow).toFloat() / 2
+    }
+
+    private fun isSemidiurnal(): Boolean {
+        var averageFrequency = 0f
+        for (i in 0 until tides.lastIndex) {
+            // TODO: Check for gap before summing them up
+            val period = Duration.between(tides[0].time, tides[1].time).seconds / 3600f
+            val frequency = (360 / (2 * period)).toRadians()
+            averageFrequency += frequency
+        }
+        averageFrequency /= tides.size
+
+        return (TideConstituent.M2.speed.toRadians() - averageFrequency).absoluteValue <= (TideConstituent.K1.speed.toRadians() - averageFrequency).absoluteValue
+    }
+
+    private fun getComputedWaveFrom(tide: Tide): Wave {
+        val amplitude = getAverageAmplitude()
+        val z0 = amplitude.absoluteValue + (tides.minByOrNull { it.height }?.height ?: 0f)
+        val frequency = if (isSemidiurnal()) TideConstituent.M2.speed else TideConstituent.K1.speed
+        val t = Duration.between(tides[0].time, tide.time).seconds / 3600f
+        return Wave(
+            if (tide.type == TideType.Low) -amplitude else amplitude,
+            frequency.toRadians(),
+            t,
+            z0
+        )
+    }
+
+    private fun getWaveBetween(tide1: Tide, tide2: Tide): Wave {
+        // TODO: If tides are far apart, use a computed wave
         val period = Duration.between(tide1.time, tide2.time).seconds / 3600f
         val deltaHeight = abs(tide1.height - tide2.height)
         val verticalShift = deltaHeight / 2 + min(tide1.height, tide2.height)
         val frequency = (360 / (2 * period)).toRadians()
         val amplitude = (if (tide1.type == TideType.High) 1 else -1) * deltaHeight / 2
-        val t = Duration.between(reference, tide1.time).seconds / 3600f
+        val t = Duration.between(tides[0].time, tide1.time).seconds / 3600f
         return Wave(amplitude, frequency, t, verticalShift)
     }
 }
