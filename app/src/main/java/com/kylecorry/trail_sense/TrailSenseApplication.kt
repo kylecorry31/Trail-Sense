f620a3184e65fe553083815082391c5b81e8cb73
Kyle Corry <kylecorry31@gmail.com>
2022-02-08 16:38:00 -0500

    Fix crash when using camera

--- app/src/main/java/com/kylecorry/trail_sense/TrailSenseApplication.kt
+++ app/src/main/java/com/kylecorry/trail_sense/TrailSenseApplication.kt
@@ -2,11 +2,14 @@ package com.kylecorry.trail_sense
 
 import android.app.Application
 import android.util.Log
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.CameraXConfig
 import com.kylecorry.trail_sense.settings.migrations.PreferenceMigrator
 import com.kylecorry.trail_sense.shared.database.RepoCleanupWorker
 import java.time.Duration
 
-class TrailSenseApplication : Application() {
+class TrailSenseApplication : Application(), CameraXConfig.Provider {
 
     override fun onCreate() {
         super.onCreate()
@@ -14,6 +17,12 @@ class TrailSenseApplication : Application() {
         NotificationChannels.createChannels(this)
         PreferenceMigrator.getInstance().migrate(this)
         RepoCleanupWorker.scheduler(this).interval(Duration.ofHours(6))
+    }
+
+    override fun getCameraXConfig(): CameraXConfig {
+        return CameraXConfig.Builder.fromConfig(Camera2Config.defaultConfig())
+            .setAvailableCamerasLimiter(CameraSelector.DEFAULT_BACK_CAMERA)
+            .setMinimumLoggingLevel(Log.ERROR).build()
     }
 
 }
