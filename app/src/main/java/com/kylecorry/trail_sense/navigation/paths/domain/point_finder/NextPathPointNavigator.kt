42e61c8052232a65112d7d7657234a03ca68cfcd
Kyle Corry <kylecorry31@gmail.com>
2022-02-26 17:24:10 -0500

    Add todo comment

--- app/src/main/java/com/kylecorry/trail_sense/navigation/paths/domain/point_finder/NextPathPointNavigator.kt
+++ app/src/main/java/com/kylecorry/trail_sense/navigation/paths/domain/point_finder/NextPathPointNavigator.kt
@@ -10,6 +10,7 @@ import com.kylecorry.trail_sense.shared.specifications.InGeofenceSpecification
 class NextPathPointNavigator(private val geology: IGeologyService = GeologyService()) :
     IPathPointNavigator {
     override suspend fun getNextPoint(path: List<PathPoint>, location: Coordinate): PathPoint? {
+        // TODO: This doesn't take into consideration which points you've already reached - if the path is a out and back type, it will not work properly
         val line = NearestPathLineCalculator(geology).calculate(location, path) ?: return null
         return if (isAtPoint(location, line.second)) {
             line.second
