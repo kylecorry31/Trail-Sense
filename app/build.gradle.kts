ca70f2fe0c17b9e1d888f22c0466f0f08f648bc3
Kyle Corry <kylecorry31@gmail.com>
2022-03-10 06:20:42 -0500

    Update version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.kylecorry.trail_sense"
         minSdk = 23
         targetSdk = 30
-        versionCode = 70
-        versionName = "3.6.1"
+        versionCode = 71
+        versionName = "3.7.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {
