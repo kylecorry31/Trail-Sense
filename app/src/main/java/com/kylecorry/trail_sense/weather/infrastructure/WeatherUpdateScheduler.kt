cc2ef230bbefcd5529d2ecc2a9280d493baca0fc
Kyle Corry <kylecorry31@gmail.com>
2022-03-25 07:03:50 -0400

    Enable always on weather if foreground is not allowed

--- app/src/main/java/com/kylecorry/trail_sense/weather/infrastructure/WeatherUpdateScheduler.kt
+++ app/src/main/java/com/kylecorry/trail_sense/weather/infrastructure/WeatherUpdateScheduler.kt
@@ -3,6 +3,7 @@ package com.kylecorry.trail_sense.weather.infrastructure
 import android.content.Context
 import com.kylecorry.andromeda.jobs.IOneTimeTaskScheduler
 import com.kylecorry.andromeda.notify.Notify
+import com.kylecorry.trail_sense.shared.permissions.AreForegroundWorkersAllowed
 
 object WeatherUpdateScheduler {
 
@@ -10,11 +11,21 @@ object WeatherUpdateScheduler {
         if (!WeatherMonitorIsAvailable().isSatisfiedBy(context)) {
             return
         }
+
         val scheduler = getScheduler(context)
-        scheduler.once()
+
+        if (AreForegroundWorkersAllowed().isSatisfiedBy(context)) {
+            WeatherMonitorAlwaysOnService.stop(context)
+            scheduler.once()
+        } else {
+            // Default to always on if it can't run in the background
+            WeatherMonitorAlwaysOnService.start(context)
+            scheduler.cancel()
+        }
     }
 
     fun stop(context: Context) {
+        WeatherMonitorAlwaysOnService.stop(context)
         Notify.cancel(context, WEATHER_NOTIFICATION_ID)
         val scheduler = getScheduler(context)
         scheduler.cancel()
