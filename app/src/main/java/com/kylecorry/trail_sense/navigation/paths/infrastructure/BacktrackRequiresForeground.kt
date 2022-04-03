e9439a94356a87059cee715315bbbaceb4464704
Kyle Corry <kylecorry31@gmail.com>
2022-04-03 06:33:13 -0400

    Backtrack always requires foreground unless always on
    This will reset backtrack's behavior to pre-Android 12, so the only change will be the running in background service

--- app/src/main/java/com/kylecorry/trail_sense/navigation/paths/infrastructure/BacktrackRequiresForeground.kt
+++ app/src/main/java/com/kylecorry/trail_sense/navigation/paths/infrastructure/BacktrackRequiresForeground.kt
@@ -2,29 +2,12 @@ package com.kylecorry.trail_sense.navigation.paths.infrastructure
 
 import android.content.Context
 import com.kylecorry.andromeda.core.specifications.Specification
-import com.kylecorry.andromeda.location.GPS
-import com.kylecorry.andromeda.permissions.Permissions
 import com.kylecorry.trail_sense.shared.UserPreferences
 import java.time.Duration
 
 class BacktrackRequiresForeground : Specification<Context>() {
     override fun isSatisfiedBy(value: Context): Boolean {
         val prefs = UserPreferences(value)
-
-        val recordsCellSignal = prefs.backtrackSaveCellHistory
-        if (recordsCellSignal) {
-            return true
-        }
-
-        val usesOverride = !prefs.useAutoLocation
-        val noLocationAccess = !Permissions.canGetFineLocation(value)
-        val noGps = !GPS.isAvailable(value)
-        val alwaysOn = prefs.backtrackRecordFrequency < Duration.ofMinutes(15)
-
-        if (usesOverride || noLocationAccess || noGps || alwaysOn) {
-            return false
-        }
-
-        return !Permissions.isBackgroundLocationEnabled(value)
+        return prefs.backtrackRecordFrequency >= Duration.ofMinutes(15)
     }
 }
