6da13cb289ec66c6fd457bc469d81c0160be8654
Kyle Corry <kylecorry31@gmail.com>
2022-04-08 06:06:25 -0400

    Use controlled runner for thumbnail loading

--- app/src/main/java/com/kylecorry/trail_sense/shared/io/ImageThumbnailManager.kt
+++ app/src/main/java/com/kylecorry/trail_sense/shared/io/ImageThumbnailManager.kt
@@ -2,17 +2,17 @@ package com.kylecorry.trail_sense.shared.io
 
 import android.graphics.Bitmap
 import android.widget.ImageView
+import com.kylecorry.andromeda.core.coroutines.ControlledRunner
 import com.kylecorry.trail_sense.shared.extensions.onIO
 import com.kylecorry.trail_sense.shared.extensions.onMain
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class ImageThumbnailManager {
 
     private val bitmaps = mutableMapOf<Int, Bitmap>()
-    private val jobs = mutableMapOf<Int, Job>()
     private val views = mutableMapOf<Int, ImageView>()
+    private val runners = mutableMapOf<Int, ControlledRunner<Unit>>()
 
     fun setImage(
         scope: CoroutineScope,
@@ -20,30 +20,41 @@ class ImageThumbnailManager {
         load: suspend CoroutineScope.() -> Bitmap?
     ) {
         synchronized(this) {
+            val runner = runners.getOrPut(view.hashCode()) { ControlledRunner() }
             views[view.hashCode()] = view
-            jobs[view.hashCode()]?.cancel()
-            view.setImageDrawable(null)
-            jobs[view.hashCode()] = scope.launch {
-                onIO {
-                    val previous = bitmaps[view.hashCode()]
-                    val loaded = load() ?: return@onIO
-                    bitmaps[view.hashCode()] = loaded
+            scope.launch {
+                runner.cancelPreviousThenRun {
                     onMain {
-                        view.setImageBitmap(bitmaps[view.hashCode()])
+                        view.setImageDrawable(null)
                     }
-                    previous?.recycle()
+                    onIO {
+                        removeBitmap(view.hashCode())
+                        val loaded = load() ?: return@onIO
+                        bitmaps[view.hashCode()] = loaded
+                        onMain {
+                            view.setImageBitmap(bitmaps[view.hashCode()])
+                        }
+                    }
                 }
             }
         }
     }
 
+    private fun removeBitmap(id: Int) {
+        synchronized(this) {
+            val previous = bitmaps[id]
+            bitmaps.remove(id)
+            previous?.recycle()
+        }
+    }
+
     fun clear() {
         synchronized(this) {
-            jobs.forEach { it.value.cancel() }
+            runners.forEach { it.value.cancel() }
             views.forEach { it.value.setImageDrawable(null) }
             bitmaps.forEach { it.value.recycle() }
             views.clear()
-            jobs.clear()
+            runners.clear()
             bitmaps.clear()
         }
     }
