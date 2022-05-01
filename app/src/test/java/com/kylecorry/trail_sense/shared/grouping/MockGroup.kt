58137023e7dab4fc4c7815e815391baf580d1866
Kyle Corry <kylecorry31@gmail.com>
2022-05-01 12:42:15 -0400

    Fix build

--- app/src/test/java/com/kylecorry/trail_sense/shared/grouping/MockGroup.kt
+++ app/src/test/java/com/kylecorry/trail_sense/shared/grouping/MockGroup.kt
@@ -3,5 +3,6 @@ package com.kylecorry.trail_sense.shared.grouping
 data class MockGroup(
     override val id: Long,
     override val isGroup: Boolean,
-    override val parentId: Long? = null
+    override val parentId: Long? = null,
+    override val count: Int? = null
 ) : Groupable
