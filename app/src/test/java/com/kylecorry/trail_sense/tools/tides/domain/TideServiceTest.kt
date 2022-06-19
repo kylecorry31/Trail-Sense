90ae392440703e75cb5d499ba0451df8e39eb75c
Kyle Corry <kylecorry31@gmail.com>
2022-06-19 06:44:57 -0400

    Add more tests for semidiurnal tides

--- app/src/test/java/com/kylecorry/trail_sense/tools/tides/domain/TideServiceTest.kt
+++ app/src/test/java/com/kylecorry/trail_sense/tools/tides/domain/TideServiceTest.kt
@@ -1,6 +1,7 @@
 package com.kylecorry.trail_sense.tools.tides.domain
 
 import com.kylecorry.sol.science.oceanography.Tide
+import com.kylecorry.sol.units.Distance
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
@@ -83,13 +84,13 @@ internal class TideServiceTest {
         expected: List<Tide>,
         isSemidiurnal: Boolean
     ) {
-        val table = TideTable(0, references, isSemidiurnal = isSemidiurnal)
+        val table = TideTable(0, references.map { toMeters(it) }, isSemidiurnal = isSemidiurnal)
         val service = TideService()
         val actual = mutableListOf<Tide>()
-        for (date in dates){
+        for (date in dates) {
             actual.addAll(service.getTides(table, date))
         }
-        check(actual, expected, expected.map { false })
+        check(actual, expected.map { toMeters(it) }, expected.map { false })
     }
 
     companion object {
@@ -124,21 +125,110 @@ internal class TideServiceTest {
                 tide(2022, 6, 4, 21, 3, false, 2.18f),
             )
 
+            val maineAug1 = listOf(
+                tide(2022, 8, 1, 1, 33, true, 11.12f),
+                tide(2022, 8, 1, 7, 54, false, 0.36f),
+                tide(2022, 8, 1, 14, 3, true, 10.48f),
+                tide(2022, 8, 1, 20, 6, false, 1.24f),
+            )
+
+            val maineAug2 = listOf(
+                tide(2022, 8, 2, 2, 11, true, 10.99f),
+                tide(2022, 8, 2, 8, 30, false, 0.42f),
+                tide(2022, 8, 2, 14, 40, true, 10.66f),
+                tide(2022, 8, 2, 20, 48, false, 1.12f),
+            )
+
+            val maineAug3 = listOf(
+                tide(2022, 8, 3, 2, 53, true, 10.79f),
+                tide(2022, 8, 3, 9, 9, false, 0.52f),
+                tide(2022, 8, 3, 15, 21, true, 10.84f),
+                tide(2022, 8, 3, 21, 33, false, 0.99f),
+            )
+
+            val maineJan1 = listOf(
+                tide(2023, 1, 1, 6, 14, true, 10.95f),
+                tide(2023, 1, 1, 12, 39, false, 0.43f),
+                tide(2023, 1, 1, 18, 48, true, 9.934f),
+            )
+
+            val maineJan2 = listOf(
+                tide(2023, 1, 2, 0, 54, false, 0.95f),
+                tide(2023, 1, 2, 7, 9, true, 11f),
+                tide(2023, 1, 2, 13, 37, false, 0.27f),
+                tide(2023, 1, 2, 19, 47, true, 9.83f),
+            )
+
+            val maineJune = maineJune1 + maineJune2 + maineJune3 + maineJune4
+            val maineAug = maineAug1 + maineAug2 + maineAug3
+            val maineJan = maineJan1 + maineJan2
+
             // Reference, date, expected
             return Stream.of(
                 Arguments.of(
                     maineJune1,
-                    listOf(date(2022, 6, 1)),
+                    dates(2022, 6, 1, 1),
                     maineJune1,
                     true
                 ),
                 Arguments.of(
                     maineJune1 + maineJune2,
-                    listOf(date(2022, 6, 1), date(2022, 6, 2)),
+                    dates(2022, 6, 1, 2),
                     maineJune1 + maineJune2,
                     true
                 ),
+                Arguments.of(
+                    maineJune1.take(3) + maineJune2 + maineJune4,
+                    dates(2022, 6, 1, 4),
+                    maineJune,
+                    true
+                ),
+                Arguments.of(
+                    maineJune1 + maineJune2,
+                    dates(2022, 6, 1, 4),
+                    maineJune,
+                    true
+                ),
+                Arguments.of(
+                    maineJune1 + maineJune2,
+                    dates(2022, 8, 1, 1),
+                    maineAug1,
+                    true
+                ),
+                Arguments.of(
+                    maineJune1 + maineJune2 + maineAug + maineJan,
+                    dates(2022, 6, 1, 2) + dates(2022, 8, 1, 3) + dates(2023, 1, 1, 2),
+                    maineJune1 + maineJune2 + maineAug + maineJan,
+                    true
+                ),
+                Arguments.of(
+                    maineJune1.take(1),
+                    dates(2022, 6, 1, 2),
+                    (maineJune1 + maineJune2).map { it.copy(height = if (it.isHigh) it.height else 8.24f) },
+                    true
+                ),
             )
+        }
+
+        private fun toMeters(tide: Tide): Tide {
+            return tide.copy(
+                height = if (tide.height == null) null else Distance.feet(tide.height!!).meters().distance
+            )
+        }
+
+        private fun dates(
+            startYear: Int,
+            startMonth: Int,
+            startDay: Int,
+            days: Int
+        ): List<LocalDate> {
+            val dates = mutableListOf<LocalDate>()
+            val start = date(startYear, startMonth, startDay)
+            dates.add(start)
+            while (dates.size < days) {
+                dates.add(start.plusDays(dates.size.toLong()))
+            }
+            return dates
         }
 
         private fun date(year: Int, month: Int, day: Int): LocalDate {
@@ -192,7 +282,7 @@ internal class TideServiceTest {
             Assertions.assertEquals(
                 expected.height!!,
                 actual.height!!,
-                if (exact) 0.0001f else 0.2f
+                if (exact) 0.0001f else 1.5f
             )
             val delta = Duration.between(actual.time, expected.time).seconds / 60f
             Assertions.assertEquals(0f, delta, if (exact) 0f else 90f)
