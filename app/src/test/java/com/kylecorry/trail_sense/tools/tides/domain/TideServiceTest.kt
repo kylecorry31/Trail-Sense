98de609495c02cf85950fe94538755bfe42629e2
Kyle Corry <kylecorry31@gmail.com>
2022-06-18 20:14:33 -0400

    Start adding real world tide tests

--- app/src/test/java/com/kylecorry/trail_sense/tools/tides/domain/TideServiceTest.kt
+++ app/src/test/java/com/kylecorry/trail_sense/tools/tides/domain/TideServiceTest.kt
@@ -3,7 +3,11 @@ package com.kylecorry.trail_sense.tools.tides.domain
 import com.kylecorry.sol.science.oceanography.Tide
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import java.time.*
+import java.util.stream.Stream
 
 internal class TideServiceTest {
 
@@ -71,33 +75,128 @@ internal class TideServiceTest {
         )
     }
 
-    private fun check(
-        actual: List<Tide>,
+    @ParameterizedTest
+    @MethodSource("provideTides")
+    fun testRealWorldAccuracyHighLow(
+        references: List<Tide>,
+        dates: List<LocalDate>,
         expected: List<Tide>,
-        exact: List<Boolean>
+        isSemidiurnal: Boolean
     ) {
-        Assertions.assertEquals(expected.size, actual.size)
-        actual.zip(expected.zip(exact)).forEach {
-            check(it.first, it.second.first, it.second.second)
+        val table = TideTable(0, references, isSemidiurnal = isSemidiurnal)
+        val service = TideService()
+        val actual = mutableListOf<Tide>()
+        for (date in dates){
+            actual.addAll(service.getTides(table, date))
+        }
+        check(actual, expected, expected.map { false })
+    }
+
+    companion object {
+        @JvmStatic
+        fun provideTides(): Stream<Arguments> {
+
+            val maineJune1 = listOf(
+                tide(2022, 6, 1, 0, 28, true, 11.24f),
+                tide(2022, 6, 1, 6, 57, false, 0.26f),
+                tide(2022, 6, 1, 13, 6, true, 10.02f),
+                tide(2022, 6, 1, 19, 2, false, 1.74f),
+            )
+
+            val maineJune2 = listOf(
+                tide(2022, 6, 2, 1, 6, true, 11.05f),
+                tide(2022, 6, 2, 7, 35, false, 0.47f),
+                tide(2022, 6, 2, 13, 44, true, 9.85f),
+                tide(2022, 6, 2, 19, 40, false, 1.93f),
+            )
+
+            val maineJune3 = listOf(
+                tide(2022, 6, 3, 1, 45, true, 10.84f),
+                tide(2022, 6, 3, 8, 14, false, 0.69f),
+                tide(2022, 6, 3, 14, 24, true, 9.7f),
+                tide(2022, 6, 3, 20, 20, false, 2.08f),
+            )
+
+            val maineJune4 = listOf(
+                tide(2022, 6, 4, 2, 25, true, 10.62f),
+                tide(2022, 6, 4, 8, 54, false, 0.87f),
+                tide(2022, 6, 4, 15, 5, true, 9.59f),
+                tide(2022, 6, 4, 21, 3, false, 2.18f),
+            )
+
+            // Reference, date, expected
+            return Stream.of(
+                Arguments.of(
+                    maineJune1,
+                    listOf(date(2022, 6, 1)),
+                    maineJune1,
+                    true
+                ),
+                Arguments.of(
+                    maineJune1 + maineJune2,
+                    listOf(date(2022, 6, 1), date(2022, 6, 2)),
+                    maineJune1 + maineJune2,
+                    true
+                ),
+            )
+        }
+
+        private fun date(year: Int, month: Int, day: Int): LocalDate {
+            return LocalDate.of(year, month, day)
+        }
+
+        private fun time(day: Int, hour: Int, minute: Int): ZonedDateTime {
+            return ZonedDateTime.of(
+                LocalDate.of(2022, 1, day),
+                LocalTime.of(hour, minute, 0),
+                ZoneId.systemDefault()
+            )
+        }
+
+        private fun tide(
+            year: Int,
+            month: Int,
+            day: Int,
+            hour: Int,
+            minute: Int,
+            high: Boolean,
+            height: Float? = null,
+            zone: String = "America/New_York"
+        ): Tide {
+            val time = ZonedDateTime.of(
+                LocalDate.of(year, month, day),
+                LocalTime.of(hour, minute, 0),
+                ZoneId.of(zone)
+            )
+
+            return Tide(time, high, height)
+        }
+
+        private fun check(
+            actual: List<Tide>,
+            expected: List<Tide>,
+            exact: List<Boolean>
+        ) {
+            Assertions.assertEquals(expected.size, actual.size)
+            actual.zip(expected.zip(exact)).forEach {
+                check(it.first, it.second.first, it.second.second)
+            }
+        }
+
+        private fun check(
+            actual: Tide,
+            expected: Tide,
+            exact: Boolean = false
+        ) {
+            Assertions.assertEquals(expected.isHigh, actual.isHigh)
+            Assertions.assertEquals(
+                expected.height!!,
+                actual.height!!,
+                if (exact) 0.0001f else 0.2f
+            )
+            val delta = Duration.between(actual.time, expected.time).seconds / 60f
+            Assertions.assertEquals(0f, delta, if (exact) 0f else 90f)
         }
     }
 
-    private fun check(
-        actual: Tide,
-        expected: Tide,
-        exact: Boolean = false
-    ) {
-        Assertions.assertEquals(expected.isHigh, actual.isHigh)
-        Assertions.assertEquals(expected.height!!, actual.height!!, if (exact) 0.0001f else 0.2f)
-        val delta = Duration.between(actual.time, expected.time).seconds / 60f
-        Assertions.assertEquals(0f, delta, if (exact) 0f else 90f)
-    }
-
-    private fun time(day: Int, hour: Int, minute: Int): ZonedDateTime {
-        return ZonedDateTime.of(
-            LocalDate.of(2022, 1, day),
-            LocalTime.of(hour, minute, 0),
-            ZoneId.systemDefault()
-        )
-    }
 }
