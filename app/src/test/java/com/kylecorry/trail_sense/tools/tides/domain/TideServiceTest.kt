1fc46823233bbe6f12d220fcc0c50b04755030df
Kyle Corry <kylecorry31@gmail.com>
2022-06-20 18:23:58 -0400

    Add tests for diurnal tides

--- app/src/test/java/com/kylecorry/trail_sense/tools/tides/domain/TideServiceTest.kt
+++ app/src/test/java/com/kylecorry/trail_sense/tools/tides/domain/TideServiceTest.kt
@@ -163,8 +163,59 @@ internal class TideServiceTest {
             val maineAug = maineAug1 + maineAug2 + maineAug3
             val maineJan = maineJan1 + maineJan2
 
+
+            val laJune1 = listOf(
+                tide(2022, 6, 1, 10, 54, true, 1.34f),
+                tide(2022, 6, 1, 22, 59, false, 0.13f),
+            )
+
+            val laJune2 = listOf(
+                tide(2022, 6, 2, 11, 37, true, 1.32f),
+                tide(2022, 6, 2, 23, 41, false, 0.16f),
+            )
+
+            val laJune3 = listOf(
+                tide(2022, 6, 3, 12, 54, true, 1.28f),
+            )
+
+            val laJune4 = listOf(
+                tide(2022, 6, 4, 0, 22, false, 0.2f),
+                tide(2022, 6, 4, 13, 10, true, 1.24f),
+            )
+
+            val laAug1 = listOf(
+                tide(2022, 8, 1, 12, 26, true, 0.75f),
+                tide(2022, 8, 1, 23, 16, false, 0.23f),
+            )
+
+            val laAug2 = listOf(
+                tide(2022, 8, 2, 12, 28, true, 0.62f),
+                tide(2022, 8, 2, 21, 36, false, 0.34f),
+            )
+
+            val laAug3 = listOf(
+                tide(2022, 8, 3, 6, 19, true, 0.52f),
+                tide(2022, 8, 3, 19, 48, false, 0.39f),
+            )
+
+            val laJan1 = listOf(
+                tide(2023, 1, 1, 5, 32, false, -0.28f),
+                tide(2023, 1, 1, 18, 58, true, 0.6f),
+            )
+
+            val laJan2 = listOf(
+                tide(2023, 1, 2, 6, 8, false, -0.41f),
+                tide(2023, 1, 2, 19, 16, true, 0.7f),
+            )
+
+            val laJune = laJune1 + laJune2 + laJune3 + laJune4
+            val laAug = laAug1 + laAug2 + laAug3
+            val laJan = laJan1 + laJan2
+
+
             // Reference, date, expected
             return Stream.of(
+                // ME
                 Arguments.of(
                     maineJune1,
                     dates(2022, 6, 1, 1),
@@ -203,9 +254,59 @@ internal class TideServiceTest {
                 ),
                 Arguments.of(
                     maineJune1.take(1),
-                    dates(2022, 6, 1, 2),
-                    (maineJune1 + maineJune2).map { it.copy(height = if (it.isHigh) it.height else 8.24f) },
+                    dates(2022, 6, 1, 3),
+                    (maineJune1 + maineJune2 + maineJune3).map { it.copy(height = if (it.isHigh) it.height else 8.24f) },
                     true
+                ),
+
+                // LA
+                Arguments.of(
+                    laJune1,
+                    dates(2022, 6, 1, 1),
+                    laJune1,
+                    false
+                ),
+                Arguments.of(
+                    laJune1 + laJune2,
+                    dates(2022, 6, 1, 2),
+                    laJune1 + laJune2,
+                    false
+                ),
+                Arguments.of(
+                    laJune1.take(1) + laJune2 + laJune4,
+                    dates(2022, 6, 1, 4),
+                    laJune,
+                    false
+                ),
+                Arguments.of(
+                    laJune1 + laJune2,
+                    dates(2022, 6, 1, 4),
+                    laJune,
+                    false
+                ),
+                Arguments.of(
+                    laJune1 + laJune2,
+                    dates(2022, 8, 1, 1),
+                    laAug1.take(1),
+                    false
+                ), // Near end of day, so it is predicting it will be in the morning instead of evening
+                Arguments.of(
+                    laJune1 + laJune2,
+                    dates(2022, 8, 2, 1),
+                    laAug1.takeLast(1) + laAug2.take(1),
+                    false
+                ), // Near end of day, so it is predicting it will be in the morning instead of evening
+                Arguments.of(
+                    laJune1 + laJune2 + laAug + laJan,
+                    dates(2022, 6, 1, 2) + dates(2022, 8, 2, 2) + dates(2023, 1, 1, 1),
+                    laJune1 + laJune2 + laAug2 + laAug3 + laJan1,
+                    false
+                ), // Tweaked to handle late night / early morning tides
+                Arguments.of(
+                    laJune1.take(1),
+                    dates(2022, 6, 1, 3),
+                    (laJune1 + laJune2 + laJune3).map { it.copy(height = if (it.isHigh) it.height else -1.66f) },
+                    false
                 ),
             )
         }
