32707a9da5baf48bbffb2fef243300444cfb9927
Kyle Corry <kylecorry31@gmail.com>
2022-07-09 07:19:38 -0400

    Use a path for my location layer

--- app/src/main/java/com/kylecorry/trail_sense/navigation/ui/layers/MyLocationLayer.kt
+++ app/src/main/java/com/kylecorry/trail_sense/navigation/ui/layers/MyLocationLayer.kt
@@ -1,19 +1,19 @@
 package com.kylecorry.trail_sense.navigation.ui.layers
 
-import android.graphics.Bitmap
 import android.graphics.Color
+import android.graphics.Path
 import androidx.annotation.ColorInt
 import com.kylecorry.andromeda.canvas.ICanvasDrawer
 import com.kylecorry.sol.units.Bearing
 import com.kylecorry.sol.units.Coordinate
-import com.kylecorry.trail_sense.R
-import com.kylecorry.trail_sense.navigation.ui.markers.BitmapMapMarker
+import com.kylecorry.trail_sense.navigation.ui.markers.PathMapMarker
 
 class MyLocationLayer : BaseLayer() {
 
     private var _location: Coordinate? = null
     private var _azimuth: Bearing? = null
-    private var _image: Bitmap? = null
+    private var _path: Path? = null
+
     @ColorInt
     private var _color: Int = Color.WHITE
 
@@ -27,26 +27,52 @@ class MyLocationLayer : BaseLayer() {
         invalidate()
     }
 
-    fun setColor(@ColorInt color: Int){
+    fun setColor(@ColorInt color: Int) {
         _color = color
         invalidate()
     }
 
     override fun draw(drawer: ICanvasDrawer, map: IMapView) {
-        val size = drawer.dp(16f).toInt()
-        val image = _image ?: drawer.loadImage(R.drawable.ic_beacon, size, size)
-        _image = image
+        val path = _path ?: Path()
+        if (_path == null) {
+            val size = drawer.dp(16f)
+            // Bottom Left
+            path.moveTo(-size / 2.5f, size / 2f)
+
+            // Top
+            path.lineTo(0f, -size / 2f)
+
+            // Bottom right
+            path.lineTo(size / 2.5f, size / 2f)
+
+            // Middle dip
+            path.lineTo(0f, size / 3f)
+
+            path.close()
+
+            _path = path
+        }
         clearMarkers()
-        // TODO: Convert the drawable to a path
-        // Outline
-        addMarker(BitmapMapMarker(_location ?: map.mapCenter, image, 16f, (_azimuth?.value ?: 0f) + map.mapRotation, Color.WHITE))
-        // Fill
-        addMarker(BitmapMapMarker(_location ?: map.mapCenter, image, 12f, (_azimuth?.value ?: 0f) + map.mapRotation, _color))
+        addMarker(
+            PathMapMarker(
+                _location ?: map.mapCenter,
+                path,
+                size = 16f,
+                color = _color,
+                strokeColor = Color.WHITE,
+                strokeWeight = 2f,
+                rotation = (_azimuth?.value ?: 0f) + map.mapRotation
+            )
+        )
+//        // TODO: Convert the drawable to a path
+//        // Outline
+//        addMarker(BitmapMapMarker(_location ?: map.mapCenter, image, 16f, (_azimuth?.value ?: 0f) + map.mapRotation, Color.WHITE))
+//        // Fill
+//        addMarker(BitmapMapMarker(_location ?: map.mapCenter, image, 12f, (_azimuth?.value ?: 0f) + map.mapRotation, _color))
         super.draw(drawer, map)
     }
 
     protected fun finalize() {
-        _image?.recycle()
-        _image = null
+        _path = null
     }
 }
---
+++ app/src/main/java/com/kylecorry/trail_sense/navigation/ui/markers/PathMapMarker.kt
@@ -0,0 +1,60 @@
+package com.kylecorry.trail_sense.navigation.ui.markers
+
+import android.graphics.Path
+import android.graphics.PathEffect
+import androidx.annotation.ColorInt
+import com.kylecorry.andromeda.canvas.ICanvasDrawer
+import com.kylecorry.andromeda.core.units.PixelCoordinate
+import com.kylecorry.sol.units.Coordinate
+import kotlin.math.max
+
+class PathMapMarker(
+    override val location: Coordinate,
+    private val path: Path,
+    override val size: Float = 10f,
+    @ColorInt private val color: Int?,
+    @ColorInt private val strokeColor: Int? = null,
+    private val strokeWeight: Float = 0f,
+    private val pathEffect: PathEffect? = null,
+    private val pathCenter: PixelCoordinate = PixelCoordinate(0f, 0f),
+    private val rotation: Float? = null,
+    private val onClickFn: () -> Boolean = { false }
+) : MapMarker {
+    override fun draw(
+        drawer: ICanvasDrawer,
+        anchor: PixelCoordinate,
+        scale: Float,
+        rotation: Float
+    ) {
+        val requestedSize = drawer.dp(size) * scale
+        val dimensions = drawer.pathDimensions(path)
+        val neededScale = requestedSize / max(dimensions.first, dimensions.second)
+        drawer.push()
+        if (pathEffect != null) {
+            drawer.pathEffect(pathEffect)
+        } else {
+            drawer.noPathEffect()
+        }
+        if (strokeWeight > 0f && strokeColor != null) {
+            drawer.strokeWeight(strokeWeight)
+            drawer.stroke(strokeColor)
+        } else {
+            drawer.noStroke()
+        }
+        if (color != null) {
+            drawer.fill(color)
+        } else {
+            drawer.noFill()
+        }
+        drawer.rotate(this.rotation ?: rotation, anchor.x, anchor.y)
+        drawer.translate(anchor.x + pathCenter.x, anchor.y + pathCenter.y)
+        drawer.scale(neededScale, neededScale)
+        drawer.path(path)
+        drawer.pop()
+        drawer.noPathEffect()
+    }
+
+    override fun onClick(): Boolean {
+        return onClickFn()
+    }
+}
