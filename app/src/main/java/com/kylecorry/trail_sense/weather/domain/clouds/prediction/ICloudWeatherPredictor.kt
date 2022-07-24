07c9527153dc4cd9604e44630580cddcdecb186d
Kyle Corry <kylecorry31@gmail.com>
2022-07-24 06:58:13 -0400

    Remove cloud overall weather prediction
    The cloud weather forecasts already handle this, and it will be better to combine this with pressure and cloud history if possible

--- app/src/main/java/com/kylecorry/trail_sense/weather/domain/clouds/prediction/ICloudWeatherPredictor.kt
+++
@@ -1,9 +0,0 @@
-package com.kylecorry.trail_sense.weather.domain.clouds.prediction
-
-import com.kylecorry.sol.science.meteorology.Weather
-import com.kylecorry.sol.science.meteorology.clouds.CloudGenus
-import com.kylecorry.trail_sense.shared.ClassificationResult
-
-interface ICloudWeatherPredictor {
-    fun predict(clouds: List<ClassificationResult<CloudGenus>>): Weather
-}
