dcbf00ec0ef8ae75326fcc8fe9310bac8a97c7a9
Kyle Corry <kylecorry31@gmail.com>
2022-08-21 07:33:21 -0400

    Add android 13 per app language support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,7 @@ android {
         versionCode = 82
         versionName = "4.4.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        resourceConfigurations.addAll(listOf("en", "af", "ar", "bg", "bn", "bs", "ca", "cs", "da", "de", "es", "eu", "fa-rIR", "fi", "fr", "hi", "hu", "hy", "in", "it", "iw", "js", "nb-rNO", "ne", "nl", "ny", "pl", "pt", "pt-rBR", "ro", "ru", "sk", "tr", "uk", "vi", "zh", "zh-rTW"))
     }
     buildFeatures {
         viewBinding = true
