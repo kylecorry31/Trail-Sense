297a27bea1dd27b92a2c25746e69316f49e00ff7
Kyle Corry <kylecorry31@gmail.com>
2022-09-04 12:35:44 -0400

    Add ruins icon

--- app/src/main/java/com/kylecorry/trail_sense/navigation/beacons/domain/BeaconIcon.kt
+++ app/src/main/java/com/kylecorry/trail_sense/navigation/beacons/domain/BeaconIcon.kt
@@ -4,7 +4,7 @@ import androidx.annotation.DrawableRes
 import com.kylecorry.trail_sense.R
 import com.kylecorry.trail_sense.shared.database.Identifiable
 
-// Last ID: 46
+// Last ID: 45
 enum class BeaconIcon(override val id: Long, @DrawableRes val icon: Int) : Identifiable {
     // Activities
     Trail(26, R.drawable.ic_trail),
@@ -21,11 +21,10 @@ enum class BeaconIcon(override val id: Long, @DrawableRes val icon: Int) : Ident
     BodyOfWater(17, R.drawable.ic_tide_table),
     River(18, R.drawable.ic_river),
     Waterfall(20, R.drawable.ic_waterfall),
-    Beach(19, R.drawable.ic_beach), // TODO: Get a better icon
+    Beach(19, R.drawable.ic_beach),
     Tree(21, R.drawable.tree),
     Field(37, R.drawable.ic_grass),
     Plant(38, R.drawable.ic_category_natural),
-//    Boulder(31, R.drawable.ic_boulder),
 
     // Amenities
     Food(12, R.drawable.ic_category_food),
@@ -43,14 +42,14 @@ enum class BeaconIcon(override val id: Long, @DrawableRes val icon: Int) : Ident
     Tent(5, R.drawable.ic_category_shelter),
     House(10, R.drawable.ic_house),
     Building(39, R.drawable.ic_building),
+    Cabin(31, R.drawable.ic_cabin),
     Barn(40, R.drawable.ic_barn),
-    // Historic(33, R.drawable.ic_historic),
+     Historic(33, R.drawable.ic_ruins),
     Cemetery(41, R.drawable.ic_grave),
     FireTower(42, R.drawable.ic_fire_tower),
     Bridge(43, R.drawable.ic_bridge),
     Lighthouse(44, R.drawable.ic_lighthouse),
     VisitorCenter(45, R.drawable.ic_visitor_center),
-    Cabin(46, R.drawable.ic_cabin),
 
 
     // Vehicles
