a6c6b69c15419b743e23358ccb5926c688571ea2
Kyle Corry <kylecorry31@gmail.com>
2022-10-28 06:38:15 -0400

    Update sol

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,7 +89,7 @@ dependencies {
     implementation("com.github.kylecorry31:subsampling-scale-image-view:v3.11.1")
 
     // Sol
-    implementation("com.github.kylecorry31:sol:6.1.1")
+    implementation("com.github.kylecorry31:sol:6.1.2")
 
     // Andromeda
     val andromedaVersion = "5.0.0-beta18"
