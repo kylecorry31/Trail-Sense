8944c11395882f5e229e2531889ac3e4f30eb13f
Kyle Corry <kylecorry31@gmail.com>
2022-10-29 08:27:47 -0400

    Remove test code

--- app/src/main/java/com/kylecorry/trail_sense/shared/sensors/altimeter/MedianAltimeter.kt
+++ app/src/main/java/com/kylecorry/trail_sense/shared/sensors/altimeter/MedianAltimeter.kt
@@ -2,18 +2,13 @@ package com.kylecorry.trail_sense.shared.sensors.altimeter
 
 import com.kylecorry.andromeda.core.sensors.AbstractSensor
 import com.kylecorry.andromeda.core.sensors.IAltimeter
-import com.kylecorry.andromeda.location.IGPS
-import com.kylecorry.sol.math.SolMath
 import com.kylecorry.sol.math.filters.MedianFilter
-import com.kylecorry.sol.math.sumOfFloat
-import kotlin.math.sqrt
 
 class MedianAltimeter(val altimeter: IAltimeter, private val samples: Int = 3) :
     AbstractSensor(), IAltimeter {
 
     private val filter = MedianFilter(samples)
     private var count = 0
-    private val buffer = mutableListOf<Pair<Float, Float>>()
 
     override fun startImpl() {
         count = 0
@@ -33,33 +28,10 @@ class MedianAltimeter(val altimeter: IAltimeter, private val samples: Int = 3) :
 
     private fun onReading(): Boolean {
         count++
-        buffer.add(
-            altimeter.altitude to (if (altimeter is IGPS) altimeter.verticalAccuracy
-                ?: 10f else 10f)
-        )
-        if (buffer.size > samples) {
-            buffer.removeFirst()
-        }
-        val calculated = calculate()
-        println(calculated)
         altitude = filter.filter(altimeter.altitude)
-        println(altitude)
         if (hasValidReading) {
             notifyListeners()
         }
         return true
-    }
-
-
-    private fun calculate(): Pair<Float, Float> {
-        val sum = buffer.sumOfFloat { 1 / it.second }
-        val weights = buffer.map { (1 / it.second) / sum }
-        var mean = 0f
-        var variance = 0f
-        for (i in buffer.indices) {
-            mean += weights[i] * buffer[i].first
-            variance += SolMath.square(weights[i] * buffer[i].second)
-        }
-        return mean to sqrt(variance)
     }
 }
