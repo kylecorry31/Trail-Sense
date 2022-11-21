98decf142d524451f650572ffd2294c3dce4a86f
Kyle Corry <kylecorry31@gmail.com>
2022-11-21 16:18:39 -0500

    Show hours on humidity chart

--- app/src/main/java/com/kylecorry/trail_sense/weather/ui/charts/HumidityChart.kt
+++ app/src/main/java/com/kylecorry/trail_sense/weather/ui/charts/HumidityChart.kt
@@ -1,14 +1,17 @@
 package com.kylecorry.trail_sense.weather.ui.charts
 
 import com.kylecorry.andromeda.core.system.Resources
-import com.kylecorry.sol.units.Reading
-import com.kylecorry.trail_sense.R
 import com.kylecorry.ceres.chart.Chart
 import com.kylecorry.ceres.chart.data.LineChartLayer
+import com.kylecorry.sol.units.Reading
+import com.kylecorry.trail_sense.R
+import com.kylecorry.trail_sense.shared.views.chart.label.HourChartLabelFormatter
+import java.time.Instant
 
 class HumidityChart(private val chart: Chart) {
 
     private val color = Resources.getAndroidColorAttr(chart.context, R.attr.colorPrimary)
+    private var startTime = Instant.now()
 
     init {
         chart.configureYAxis(
@@ -20,14 +23,16 @@ class HumidityChart(private val chart: Chart) {
 
         chart.configureXAxis(
             labelCount = 0,
-            drawGridLines = false
+            drawGridLines = false,
+            labelFormatter = HourChartLabelFormatter(chart.context) { startTime }
         )
 
         chart.emptyText = chart.context.getString(R.string.no_data)
     }
 
     fun plot(data: List<Reading<Float>>) {
-        val values = Chart.getDataFromReadings(data) { it }
+        startTime = data.firstOrNull()?.time ?: Instant.now()
+        val values = Chart.getDataFromReadings(data, startTime) { it }
         chart.plot(LineChartLayer(values, color))
     }
 }
