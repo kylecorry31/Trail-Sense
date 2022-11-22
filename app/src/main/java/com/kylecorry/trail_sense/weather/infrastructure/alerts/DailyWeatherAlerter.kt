28c3bd5c68afb68bf3e566a248014e45be7680f2
Kyle Corry <kylecorry31@gmail.com>
2022-11-22 07:11:25 -0500

    Show high / low in daily weather notification

--- app/src/main/java/com/kylecorry/trail_sense/weather/infrastructure/alerts/DailyWeatherAlerter.kt
+++ app/src/main/java/com/kylecorry/trail_sense/weather/infrastructure/alerts/DailyWeatherAlerter.kt
@@ -6,6 +6,7 @@ import com.kylecorry.trail_sense.NotificationChannels
 import com.kylecorry.trail_sense.R
 import com.kylecorry.trail_sense.shared.FormatService
 import com.kylecorry.trail_sense.shared.NavigationUtils
+import com.kylecorry.trail_sense.shared.UserPreferences
 import com.kylecorry.trail_sense.shared.alerts.IValueAlerter
 import com.kylecorry.trail_sense.weather.infrastructure.IWeatherPreferences
 import com.kylecorry.trail_sense.weather.infrastructure.WeatherPrediction
@@ -18,7 +19,29 @@ class DailyWeatherAlerter(
     override fun alert(value: WeatherPrediction) {
         // If daily is unchanging, show hourly instead
         val icon = formatter.getWeatherImage(value.primaryDaily ?: value.primaryHourly)
-        val description = formatter.formatWeather(value.primaryDaily ?: value.primaryHourly)
+        val condition = formatter.formatWeather(value.primaryDaily ?: value.primaryHourly)
+
+        val low = value.temperature?.low
+        val high = value.temperature?.high
+
+        val temperatureStr = if (high != null && low != null) {
+            val units = UserPreferences(context).temperatureUnits
+            val lowValue = formatter.formatTemperature(
+                low.convertTo(units)
+            )
+            val highValue = formatter.formatTemperature(
+                high.convertTo(units)
+            )
+            "$highValue / $lowValue"
+        } else {
+            null
+        }
+
+        val description = if (temperatureStr != null) {
+            context.getString(R.string.dash_separated_pair, temperatureStr, condition)
+        } else {
+            condition
+        }
 
         val openIntent = NavigationUtils.pendingIntent(context, R.id.action_weather)
 
