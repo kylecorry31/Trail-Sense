49215d48431ed3484083de249e1796b4e2aa58d1
Kyle Corry <kylecorry31@gmail.com>
2022-12-04 07:30:43 -0500

    Fix color filter on emulator
    Closes #1546

--- app/src/main/java/com/kylecorry/trail_sense/ColorFilterConstraintLayout.kt
+++ app/src/main/java/com/kylecorry/trail_sense/ColorFilterConstraintLayout.kt
@@ -1,7 +1,6 @@
 package com.kylecorry.trail_sense
 
 import android.content.Context
-import android.graphics.Canvas
 import android.graphics.ColorFilter
 import android.graphics.Paint
 import android.util.AttributeSet
@@ -11,28 +10,15 @@ import androidx.constraintlayout.widget.ConstraintLayout
 class ColorFilterConstraintLayout(context: Context, attrs: AttributeSet?) :
     ConstraintLayout(context, attrs) {
 
-    private lateinit var mPaint: Paint
+    private var mPaint = Paint()
 
-    init {
-        initialize()
-    }
-
-    private fun initialize() {
-        mPaint = Paint()
-    }
-
-    override fun dispatchDraw(canvas: Canvas) {
-        if (mPaint.colorFilter != null) {
-            canvas.saveLayer(null, mPaint)
-            super.dispatchDraw(canvas)
-            canvas.restore()
-        } else {
-            super.dispatchDraw(canvas)
-        }
-    }
-
-    fun setColorFilter(filter: ColorFilter?){
+    fun setColorFilter(filter: ColorFilter?) {
         mPaint.colorFilter = filter
+        if (filter != null) {
+            setLayerType(LAYER_TYPE_HARDWARE, mPaint)
+        } else {
+            setLayerType(LAYER_TYPE_NONE, mPaint)
+        }
     }
 
 }
