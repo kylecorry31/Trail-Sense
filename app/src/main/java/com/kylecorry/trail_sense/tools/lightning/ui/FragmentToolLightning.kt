68d9646b4acb540077ae6271ae8b4e87a18d41a1
Kyle Corry <kylecorry31@gmail.com>
2023-01-06 19:09:50 -0500

    Record lightning strikes

--- app/src/main/java/com/kylecorry/trail_sense/tools/lightning/ui/FragmentToolLightning.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/lightning/ui/FragmentToolLightning.kt
@@ -11,6 +11,7 @@ import com.kylecorry.andromeda.fragments.BoundFragment
 import com.kylecorry.sol.science.meteorology.Meteorology
 import com.kylecorry.sol.units.Distance
 import com.kylecorry.sol.units.DistanceUnits
+import com.kylecorry.sol.units.Reading
 import com.kylecorry.trail_sense.R
 import com.kylecorry.trail_sense.databinding.FragmentToolLightningBinding
 import com.kylecorry.trail_sense.shared.CustomUiUtils
@@ -20,24 +21,25 @@ import com.kylecorry.trail_sense.shared.FormatService
 import com.kylecorry.trail_sense.shared.Units
 import com.kylecorry.trail_sense.shared.UserPreferences
 import com.kylecorry.trail_sense.shared.colors.AppColor
+import com.kylecorry.trail_sense.shared.extensions.inBackground
+import com.kylecorry.trail_sense.weather.domain.lightning.LightningStrike
+import com.kylecorry.trail_sense.weather.infrastructure.persistence.LightningRepo
 import java.time.Instant
 
 class FragmentToolLightning : BoundFragment<FragmentToolLightningBinding>() {
     private val formatService by lazy { FormatService(requireContext()) }
     private val prefs by lazy { UserPreferences(requireContext()) }
+    private val repo by lazy { LightningRepo.getInstance(requireContext()) }
 
     private lateinit var units: DistanceUnits
 
     private var lightningTime: Instant? = null
-    private var distance: Distance? = null
 
-    private val intervalometer = Timer {
-        val lightning = lightningTime
-        if (lightning != null) {
-            val d =
-                Distance.meters(Meteorology.getLightningStrikeDistance(lightning, Instant.now()))
-                    .convertTo(units)
-                    .toRelativeDistance()
+    private val timer = Timer {
+        lightningTime?.let {
+            val d = getDistance(it)
+                .convertTo(units)
+                .toRelativeDistance()
             binding.lightningTitle.title.text = formatService.formatDistance(
                 d, Units.getDecimalPlaces(d.units),
                 false
@@ -59,22 +61,36 @@ class FragmentToolLightning : BoundFragment<FragmentToolLightningBinding>() {
         binding.lightningTitle.subtitle.isVisible = false
 
         binding.startBtn.setOnClickListener {
-            val lightning = lightningTime
-            if (lightning == null) {
-                lightningTime = Instant.now()
-                distance = null
-                binding.startBtn.setImageResource(R.drawable.ic_thunder)
-                binding.startBtn.setText(getString(R.string.thunder))
-                binding.startBtn.setState(true)
-                binding.startBtn.keepScreenOn = true
+            if (lightningTime == null) {
+                onLightning()
             } else {
-                lightningTime = null
-                binding.startBtn.setImageResource(R.drawable.ic_torch_on)
-                binding.startBtn.setText(getString(R.string.lightning))
-                binding.startBtn.setState(false)
-                binding.startBtn.keepScreenOn = false
+                onThunder()
             }
         }
+    }
+
+    private fun onLightning() {
+        lightningTime = Instant.now()
+        binding.startBtn.setImageResource(R.drawable.ic_thunder)
+        binding.startBtn.setText(getString(R.string.thunder))
+        binding.startBtn.setState(true)
+        binding.startBtn.keepScreenOn = true
+    }
+
+    private fun onThunder() {
+        lightningTime?.let { record(it) }
+        reset(false)
+    }
+
+    private fun reset(clearDistance: Boolean) {
+        lightningTime = null
+        if (clearDistance) {
+            binding.lightningTitle.title.text = ""
+        }
+        binding.startBtn.setImageResource(R.drawable.ic_torch_on)
+        binding.startBtn.setText(getString(R.string.lightning))
+        binding.startBtn.setState(false)
+        binding.startBtn.keepScreenOn = false
     }
 
     override fun generateBinding(
@@ -87,18 +103,24 @@ class FragmentToolLightning : BoundFragment<FragmentToolLightningBinding>() {
     override fun onResume() {
         super.onResume()
         units = prefs.baseDistanceUnits
-        distance = null
-        lightningTime = null
-        binding.lightningTitle.title.text = ""
-        binding.startBtn.setImageResource(R.drawable.ic_torch_on)
-        binding.startBtn.setText(getString(R.string.lightning))
-        binding.startBtn.setState(false)
-        intervalometer.interval(20)
+        reset(true)
+        timer.interval(20)
     }
 
     override fun onPause() {
         super.onPause()
-        intervalometer.stop()
+        timer.stop()
+    }
+
+    private fun record(lightning: Instant) {
+        val distance = getDistance(lightning)
+        inBackground {
+            repo.add(Reading(LightningStrike(0, distance), Instant.now()))
+        }
+    }
+
+    private fun getDistance(lightning: Instant): Distance {
+        return Distance.meters(Meteorology.getLightningStrikeDistance(lightning, Instant.now()))
     }
 
 }
