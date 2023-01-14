6f367b8cc3dfe8b918cbad2b494f8fb58125a3cf
Kyle Corry <kylecorry31@gmail.com>
2023-01-14 06:46:53 -0500

    Use generic share sheet

--- app/src/main/java/com/kylecorry/trail_sense/shared/sharing/Share.kt
+++ app/src/main/java/com/kylecorry/trail_sense/shared/sharing/Share.kt
@@ -2,6 +2,7 @@ package com.kylecorry.trail_sense.shared.sharing
 
 import androidx.fragment.app.Fragment
 import com.kylecorry.andromeda.fragments.show
+import com.kylecorry.trail_sense.R
 
 object Share {
 
@@ -11,19 +12,32 @@ object Share {
         actions: List<ShareAction>,
         onAction: (action: ShareAction?) -> Unit
     ) {
-        var called = false
 
-        val customOnAction = { action: ShareAction?, sheet: ShareSheet ->
-            if (!called) {
-                called = true
-                if (action != null) {
-                    sheet.dismiss()
-                }
-                onAction(action)
+        val titles = mapOf(
+            ShareAction.Copy to fragment.getString(R.string.copy),
+            ShareAction.QR to fragment.getString(R.string.qr_code),
+            ShareAction.Send to fragment.getString(R.string.share_action_send),
+            ShareAction.Maps to fragment.getString(R.string.maps),
+            ShareAction.File to fragment.getString(R.string.file)
+        )
+
+        val icons = mapOf(
+            ShareAction.Copy to R.drawable.ic_copy,
+            ShareAction.QR to R.drawable.ic_qr_code,
+            ShareAction.Send to R.drawable.ic_send,
+            ShareAction.Maps to R.drawable.maps,
+            ShareAction.File to R.drawable.ic_file
+        )
+
+        val actionItems = actions.sortedBy { it.ordinal }.map {
+            ActionItem(titles[it] ?: "", icons[it] ?: R.drawable.ic_send) {
+                onAction(it)
             }
         }
-        val sheet = ShareSheet(title, actions, customOnAction)
-        sheet.show(fragment)
+
+        actions(fragment, title, actionItems) {
+            onAction(null)
+        }
     }
 
     fun actions(
