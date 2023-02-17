e8507cca408f03ba159d7bb45de876fad3e124e6
Christian Weber <christian.m.weber.94@gmail.com>
2023-02-17 01:26:06 +0100

    Solution for #831

--- app/src/main/java/com/kylecorry/trail_sense/shared/UserPreferences.kt
+++ app/src/main/java/com/kylecorry/trail_sense/shared/UserPreferences.kt
@@ -90,6 +90,17 @@ class UserPreferences(private val context: Context) : IDeclinationPreferences {
 
         }
 
+    val addLeadingZero: Boolean
+        get() {
+            val value = cache.getBoolean(context.getString(R.string.pref_include_leading_zero))
+            return if (value == null) {
+                cache.putBoolean(context.getString(R.string.pref_include_leading_zero), false)
+                false
+            } else {
+                value
+            }
+        }
+
     val theme: Theme
         get() {
             if (isLowPowerModeOn) {
