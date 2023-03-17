d9e329bb04459ee1e4aca309684c1098b12bb8af
Kyle Corry <kylecorry31@gmail.com>
2023-03-17 14:43:24 -0400

    Improve eclipse accuracy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,7 +88,7 @@ dependencies {
     implementation("com.github.kylecorry31:subsampling-scale-image-view:3.11.9")
 
     // Sol
-    implementation("com.github.kylecorry31:sol:6.5.0-beta02")
+    implementation("com.github.kylecorry31:sol:6.5.0")
 
     // Andromeda
     val andromedaVersion = "5.0.0-beta43"
