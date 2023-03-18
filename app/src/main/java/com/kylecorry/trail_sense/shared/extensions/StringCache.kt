f301ecdbd140900c1d4aa269e3958198652ae223
Kyle Corry <kylecorry31@gmail.com>
2023-03-18 09:39:31 -0400

    Invalidate strings if locale changes

--- app/src/main/java/com/kylecorry/trail_sense/shared/extensions/StringCache.kt
+++ app/src/main/java/com/kylecorry/trail_sense/shared/extensions/StringCache.kt
@@ -2,14 +2,22 @@ package com.kylecorry.trail_sense.shared.extensions
 
 import android.content.res.Resources
 import android.os.Build
+import java.util.*
 
 class StringCache(private val resources: Resources) {
 
+    private var lastLocale = getLocale()
     private val cache = mutableMapOf<Int, String>()
     private val lock = Any()
 
     fun getString(id: Int): String {
         synchronized(lock) {
+            val currentLocale = getLocale()
+            if (lastLocale != currentLocale) {
+                cache.clear()
+                lastLocale = currentLocale
+            }
+
             if (cache.containsKey(id)) {
                 return cache[id]!!
             }
@@ -21,12 +29,15 @@ class StringCache(private val resources: Resources) {
 
     fun getString(id: Int, vararg formatArgs: Any): String {
         val raw = getString(id)
+        return String.format(lastLocale, raw, *formatArgs)
+    }
+
+    private fun getLocale(): Locale {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            val locale = resources.configuration.locales[0]
-            String.format(locale, raw, *formatArgs)
+            resources.configuration.locales[0]
         } else {
             @Suppress("DEPRECATION")
-            String.format(resources.configuration.locale, raw, *formatArgs)
+            resources.configuration.locale
         }
     }
 }
