1b29c90e4a58b0f9d1435131982c361eaad24379
Kyle Corry <kylecorry31@gmail.com>
2023-03-18 11:00:01 -0400

    Create beacon markers in the background

--- app/src/main/java/com/kylecorry/trail_sense/navigation/ui/layers/BeaconLayer.kt
+++ app/src/main/java/com/kylecorry/trail_sense/navigation/ui/layers/BeaconLayer.kt
@@ -3,11 +3,16 @@ package com.kylecorry.trail_sense.navigation.ui.layers
 import android.graphics.Color
 import androidx.annotation.ColorInt
 import com.kylecorry.andromeda.canvas.ICanvasDrawer
+import com.kylecorry.andromeda.core.coroutines.ControlledRunner
 import com.kylecorry.andromeda.core.ui.Colors
 import com.kylecorry.trail_sense.navigation.beacons.domain.Beacon
 import com.kylecorry.trail_sense.navigation.ui.DrawerBitmapLoader
 import com.kylecorry.trail_sense.navigation.ui.markers.BitmapMapMarker
 import com.kylecorry.trail_sense.navigation.ui.markers.CircleMapMarker
+import com.kylecorry.trail_sense.navigation.ui.markers.MapMarker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class BeaconLayer(
     private val size: Float = 12f,
@@ -18,7 +23,6 @@ class BeaconLayer(
     private val _beacons = mutableListOf<Beacon>()
     private var _highlighted: Beacon? = null
 
-    private var _beaconUpToDate = false
     private var _loader: DrawerBitmapLoader? = null
     private var _imageSize = 8f
 
@@ -27,11 +31,13 @@ class BeaconLayer(
 
     private val lock = Any()
 
+    private val runner = ControlledRunner<Unit>()
+    private val scope = CoroutineScope(Dispatchers.IO)
+
     fun setBeacons(beacons: List<Beacon>) {
         synchronized(lock) {
             _beacons.clear()
             _beacons.addAll(beacons)
-            _beaconUpToDate = false
         }
         updateMarkers()
     }
@@ -40,9 +46,6 @@ class BeaconLayer(
         if (_loader == null) {
             _imageSize = drawer.dp(24f)
             _loader = DrawerBitmapLoader(drawer)
-        }
-        if (!_beaconUpToDate) {
-            updateMarkers()
         }
         super.draw(drawer, map)
     }
@@ -62,41 +65,61 @@ class BeaconLayer(
     }
 
     private fun updateMarkers() {
-        synchronized(lock) {
-            val drawer = _loader ?: return
-            val size = _imageSize.toInt()
-            _beaconUpToDate = true
-            clearMarkers()
-            _beacons.forEach {
-                val opacity = if (_highlighted == null || _highlighted?.id == it.id) {
-                    255
-                } else {
-                    127
-                }
-                addMarker(
-                    CircleMapMarker(
-                        it.coordinate,
-                        it.color,
-                        backgroundColor,
-                        opacity,
-                        this.size
-                    ) {
-                        onBeaconClick(it)
-                    })
-                if (it.icon != null) {
-                    val image = drawer.load(it.icon.icon, size)
-                    val color = Colors.mostContrastingColor(Color.WHITE, Color.BLACK, it.color)
-                    addMarker(
-                        BitmapMapMarker(
-                            it.coordinate,
-                            image,
-                            size = this.size * 0.75f,
-                            tint = color
-                        ) {
-                            onBeaconClick(it)
-                        })
+        scope.launch {
+            runner.cancelPreviousThenRun {
+                synchronized(lock) {
+                    val markers = convertToMarkers(_beacons)
+                    clearMarkers()
+                    for (marker in markers) {
+                        addMarker(marker)
+                    }
                 }
             }
         }
     }
+
+    private fun convertToMarkers(beacons: List<Beacon>): List<MapMarker> {
+        val loader = _loader ?: return emptyList()
+        val size = _imageSize.toInt()
+        val markers = mutableListOf<MapMarker>()
+        beacons.forEach {
+
+            // Reduce the opacity if the beacon is not highlighted
+            val opacity = if (_highlighted == null || _highlighted?.id == it.id) {
+                255
+            } else {
+                127
+            }
+
+            // Create the marker background
+            markers.add(
+                CircleMapMarker(
+                    it.coordinate,
+                    it.color,
+                    backgroundColor,
+                    opacity,
+                    this.size
+                ) {
+                    onBeaconClick(it)
+                })
+
+            // Create the icon for the marker
+            if (it.icon != null) {
+                val image = loader.load(it.icon.icon, size)
+                val color =
+                    Colors.mostContrastingColor(Color.WHITE, Color.BLACK, it.color)
+                markers.add(
+                    BitmapMapMarker(
+                        it.coordinate,
+                        image,
+                        size = this.size * 0.75f,
+                        tint = color
+                    ) {
+                        onBeaconClick(it)
+                    })
+            }
+        }
+        return markers
+    }
+
 }
