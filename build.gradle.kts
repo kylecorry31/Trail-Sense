31c9c3dd23ac55033611e13897a53c35738f735e
Kyle Corry <kylecorry31@gmail.com>
2023-05-05 16:11:46 -0400

    Update AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.0.0")
+        classpath("com.android.tools.build:gradle:8.0.1")
         classpath(kotlin("gradle-plugin", version = "1.7.10"))
     }
 }
