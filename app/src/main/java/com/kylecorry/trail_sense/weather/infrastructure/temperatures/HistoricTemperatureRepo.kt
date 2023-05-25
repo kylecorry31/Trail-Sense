502116a711066ebfb55dd92fc3aba6c9232f6ef6
Kyle Corry <kylecorry31@gmail.com>
2023-05-24 20:33:35 -0400

    Use cubic interpolation for temperatures

--- app/src/main/java/com/kylecorry/trail_sense/weather/infrastructure/temperatures/HistoricTemperatureRepo.kt
+++ app/src/main/java/com/kylecorry/trail_sense/weather/infrastructure/temperatures/HistoricTemperatureRepo.kt
@@ -2,68 +2,31 @@ package com.kylecorry.trail_sense.weather.infrastructure.temperatures
 
 import android.content.Context
 import com.kylecorry.sol.math.Range
-import com.kylecorry.sol.math.SolMath
 import com.kylecorry.sol.units.Coordinate
 import com.kylecorry.sol.units.Reading
 import com.kylecorry.sol.units.Temperature
 import com.kylecorry.trail_sense.shared.extensions.getReadings
+import com.kylecorry.trail_sense.shared.extensions.onDefault
 import com.kylecorry.trail_sense.weather.infrastructure.temperatures.calculators.DailyTemperatureCalculator
 import java.time.Duration
 import java.time.LocalDate
 import java.time.Month
 import java.time.ZonedDateTime
+import kotlin.math.max
+import kotlin.math.min
 
 internal class HistoricTemperatureRepo(private val context: Context) : ITemperatureRepo {
 
     override suspend fun getYearlyTemperatures(
         year: Int,
         location: Coordinate
-    ): List<Pair<LocalDate, Range<Temperature>>> {
-        val byMonth = Month.values().map {
-            val date = LocalDate.of(year, it, 15)
-            date to getDailyTemperatureRange(location, date)
+    ): List<Pair<LocalDate, Range<Temperature>>> = onDefault {
+        val monthly =
+            HistoricMonthlyTemperatureRangeRepo.getMonthlyTemperatureRanges(context, location)
+
+        getYearlyValues(year){
+            getDailyRange(location, it, monthly)
         }
-
-        var date = LocalDate.of(year, Month.JANUARY, 1)
-
-        val readings = mutableListOf<Pair<LocalDate, Range<Temperature>>>()
-
-        while (date.year == year) {
-
-            if (date.dayOfMonth == 15) {
-                readings.add(byMonth[date.monthValue - 1])
-                date = date.plusDays(1)
-                continue
-            }
-
-            val startDate = if (date.dayOfMonth > 15) {
-                date.withDayOfMonth(15)
-            } else {
-                date.withDayOfMonth(15).minusMonths(1)
-            }
-
-            val endDate = if (date.dayOfMonth > 15) {
-                date.withDayOfMonth(15).plusMonths(1)
-            } else {
-                date.withDayOfMonth(15)
-            }
-
-            val start = byMonth[startDate.monthValue - 1]
-            val end = byMonth[endDate.monthValue - 1]
-
-            val daysSinceStart =
-                Duration.between(startDate.atStartOfDay(), date.atStartOfDay()).toDays()
-            val daysBetweenMonths =
-                Duration.between(startDate.atStartOfDay(), endDate.atStartOfDay()).toDays()
-
-            val pct = daysSinceStart / daysBetweenMonths.toFloat()
-
-            readings.add(date to lerp(pct, start.second, end.second))
-
-            date = date.plusDays(1)
-        }
-
-        return readings
     }
 
     override suspend fun getTemperatures(
@@ -71,23 +34,15 @@ internal class HistoricTemperatureRepo(private val context: Context) : ITemperat
         start: ZonedDateTime,
         end: ZonedDateTime
     ): List<Reading<Temperature>> {
-        val calculator = DailyTemperatureCalculator(location) { location, date ->
-            getDailyTemperatureRange(location, date)
-        }
+        val calculator = DailyTemperatureCalculator(location, this::getDailyRange)
 
-        return getReadings(
-            start,
-            end,
-            Duration.ofMinutes(10)
-        ) {
+        return getReadings(start, end, Duration.ofMinutes(10)) {
             calculator.calculate(it)
         }
     }
 
     override suspend fun getTemperature(location: Coordinate, time: ZonedDateTime): Temperature {
-        val calculator = DailyTemperatureCalculator(location) { location, date ->
-            getDailyTemperatureRange(location, date)
-        }
+        val calculator = DailyTemperatureCalculator(location, this::getDailyRange)
         return calculator.calculate(time)
     }
 
@@ -95,71 +50,108 @@ internal class HistoricTemperatureRepo(private val context: Context) : ITemperat
         location: Coordinate,
         date: LocalDate
     ): Range<Temperature> {
-        return if (date.dayOfMonth == 15) {
-            getMonthlyTemp(location, date)
-        } else if (date.dayOfMonth > 15) {
-            // TODO: Expose a method on the HistoricMonthlyTemperatureRangeRepo to get multiple months / full year
-            // Load this month and next
-            val thisMonth = getMonthlyTemp(
-                location,
-                date
+        return getDailyRange(location, date)
+    }
+
+    private suspend fun getDailyRange(
+        location: Coordinate,
+        date: LocalDate,
+        monthlyRanges: Map<Month, Range<Temperature>>? = null
+    ): Range<Temperature> = onDefault {
+        val months =
+            monthlyRanges ?: HistoricMonthlyTemperatureRangeRepo.getMonthlyTemperatureRanges(
+                context, location
             )
-            val nextMonth = getMonthlyTemp(
-                location,
-                date.plusMonths(1)
+
+        val lookupMonths = getSurroundingMonths(date)
+
+        val start = lookupMonths.first()
+
+        val xs = lookupMonths.map {
+            daysBetween(start, it).toFloat()
+        }
+
+        val lows = lookupMonths.map {
+            months[it.month]?.start?.temperature ?: 0f
+        }
+
+        val highs = lookupMonths.map {
+            months[it.month]?.end?.temperature ?: 0f
+        }
+
+        val x = daysBetween(start, date).toFloat()
+
+        val low = cubicInterpolation(
+            x, xs[0], lows[0], xs[1], lows[1], xs[2], lows[2], xs[3], lows[3]
+        )
+        val high = cubicInterpolation(
+            x, xs[0], highs[0], xs[1], highs[1], xs[2], highs[2], xs[3], highs[3]
+        )
+
+        Range(Temperature.celsius(min(low, high)), Temperature.celsius(max(low, high)))
+    }
+
+    // TODO: Extract these methods to Sol
+
+    private inline fun <T> getYearlyValues(year: Int, valueProvider: (date: LocalDate) -> T): List<Pair<LocalDate, T>> {
+        val values = mutableListOf<Pair<LocalDate, T>>()
+        var date = LocalDate.of(year, Month.JANUARY, 1)
+
+        while (date.year == year) {
+            values.add(date to valueProvider(date))
+            date = date.plusDays(1)
+        }
+
+        return values
+    }
+
+    private fun getSurroundingMonths(date: LocalDate): List<LocalDate> {
+        val midMonth = LocalDate.of(date.year, date.month, 15)
+        return if (date > midMonth) {
+            listOf(
+                midMonth.minusMonths(1),
+                midMonth,
+                // The date is between these
+                midMonth.plusMonths(1),
+                midMonth.plusMonths(2)
             )
-            val daysSinceMiddle = Duration.between(
-                date.withDayOfMonth(15).atStartOfDay(),
-                date.atStartOfDay()
-            )
-            val daysBetweenMonths = Duration.between(
-                date.withDayOfMonth(15).atStartOfDay(),
-                date.plusMonths(1).withDayOfMonth(15).atStartOfDay()
-            )
-            val pct = daysSinceMiddle.toDays() / daysBetweenMonths.toDays().toDouble()
-            lerp(pct.toFloat(), thisMonth, nextMonth)
         } else {
-            // Load this month and previous
-            val thisMonth = getMonthlyTemp(
-                location,
-                date
+            listOf(
+                midMonth.minusMonths(2),
+                midMonth.minusMonths(1),
+                // The date is between these
+                midMonth,
+                midMonth.plusMonths(1)
             )
-            val prevMonth = getMonthlyTemp(
-                location,
-                date.minusMonths(1)
-            )
-            val daysSinceMiddle = Duration.between(
-                date.minusMonths(1).withDayOfMonth(15).atStartOfDay(),
-                date.atStartOfDay()
-            )
-            val daysBetweenMonths = Duration.between(
-                date.minusMonths(1).withDayOfMonth(15).atStartOfDay(),
-                date.withDayOfMonth(15).atStartOfDay()
-            )
-            val pct = daysSinceMiddle.toDays() / daysBetweenMonths.toDays().toDouble()
-            lerp(pct.toFloat(), prevMonth, thisMonth)
         }
     }
 
-    private suspend fun getMonthlyTemp(
-        location: Coordinate,
-        date: LocalDate
-    ): Range<Temperature> {
-        return HistoricMonthlyTemperatureRangeRepo.getMonthlyTemperatureRange(
-            context,
-            location,
-            date.month
-        )
+    private fun daysBetween(start: LocalDate, end: LocalDate): Int {
+        return Duration.between(start.atStartOfDay(), end.atStartOfDay()).toDays().toInt()
     }
 
-    private fun lerp(
-        pct: Float,
-        lower: Range<Temperature>,
-        upper: Range<Temperature>
-    ): Range<Temperature> {
-        val lerpedLow = SolMath.lerp(pct, lower.start.temperature, upper.start.temperature)
-        val lerpedHigh = SolMath.lerp(pct, lower.end.temperature, upper.end.temperature)
-        return Range(Temperature.celsius(lerpedLow), Temperature.celsius(lerpedHigh))
+    fun cubicInterpolation(
+        x: Float,
+        x0: Float,
+        y0: Float,
+        x1: Float,
+        y1: Float,
+        x2: Float,
+        y2: Float,
+        x3: Float,
+        y3: Float
+    ): Float {
+        val t = (x - x1) / (x2 - x1)
+        val t2 = t * t
+        val t3 = t2 * t
+
+        val c0 = y1
+        val c1 = 0.5f * (y2 - y0)
+        val c2 = y0 - 2.5f * y1 + 2.0f * y2 - 0.5f * y3
+        val c3 = 0.5f * (y3 - y0) + 1.5f * (y1 - y2)
+
+        return c0 + c1 * t + c2 * t2 + c3 * t3
     }
+
 
 }
