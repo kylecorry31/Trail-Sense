0ee5010fde1050d7c97a872a0f989c29d96bc227
Kyle Corry <kylecorry31@gmail.com>
2023-06-07 08:14:26 -0400

    Update ceres

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -126,7 +126,7 @@ dependencies {
     implementation("com.github.kylecorry31.andromeda:print:$andromedaVersion")
 
     // Ceres
-    val ceresVersion = "c7b8de2b63"
+    val ceresVersion = "6a7e225d89"
     implementation("com.github.kylecorry31.ceres:list:$ceresVersion")
     implementation("com.github.kylecorry31.ceres:toolbar:$ceresVersion")
     implementation("com.github.kylecorry31.ceres:badge:$ceresVersion")
