3f51ace3543f952b4d11861ca0dd0acb892a79c7
Kyle Corry <kylecorry31@gmail.com>
2023-06-10 08:00:07 -0400

    Improve build speed

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.0.1")
+        classpath("com.android.tools.build:gradle:8.0.2")
         classpath(kotlin("gradle-plugin", version = "1.7.10"))
     }
 }
