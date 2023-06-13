79f81ff33268364ee1af83d1405621fe03eb56e3
Kyle Corry <kylecorry31@gmail.com>
2023-06-13 19:42:08 -0400

    Add disclaimer about cropping the map

--- app/src/main/java/com/kylecorry/trail_sense/settings/ui/MapSettingsFragment.kt
+++ app/src/main/java/com/kylecorry/trail_sense/settings/ui/MapSettingsFragment.kt
@@ -3,11 +3,29 @@ package com.kylecorry.trail_sense.settings.ui
 import android.os.Bundle
 import com.kylecorry.andromeda.fragments.AndromedaPreferenceFragment
 import com.kylecorry.trail_sense.R
+import com.kylecorry.trail_sense.shared.CustomUiUtils
 
 class MapSettingsFragment : AndromedaPreferenceFragment() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.map_preferences, rootKey)
+
+        val reduceResolutionPreference = switch(R.string.pref_low_resolution_maps)
+
+        onClick(reduceResolutionPreference) {
+            if (reduceResolutionPreference?.isChecked == true) {
+                return@onClick
+            }
+
+            CustomUiUtils.disclaimer(
+                requireContext(),
+                getString(R.string.reduce_map_resolution),
+                getString(R.string.reduce_map_resolution_crop_disclaimer),
+                getString(R.string.reduce_map_resolution_crop_disclaimer_shown),
+                cancelText = null
+            )
+        }
+
     }
 
 }
