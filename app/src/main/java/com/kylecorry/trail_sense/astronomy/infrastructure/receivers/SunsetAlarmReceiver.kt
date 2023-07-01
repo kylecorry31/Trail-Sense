5602ccd11414d20a5df73d707d60fc2348df1580
Kyle Corry <kylecorry31@gmail.com>
2023-07-01 06:53:43 -0400

    Remove foreground service type from sunset alarm

--- app/src/main/java/com/kylecorry/trail_sense/astronomy/infrastructure/receivers/SunsetAlarmReceiver.kt
+++ app/src/main/java/com/kylecorry/trail_sense/astronomy/infrastructure/receivers/SunsetAlarmReceiver.kt
@@ -18,7 +18,7 @@ class SunsetAlarmReceiver : BroadcastReceiver() {
             return
         }
 
-        Intents.startService(context, SunsetAlarmService.intent(context), false)
+        Intents.startService(context, SunsetAlarmService.intent(context))
     }
 
     companion object {
