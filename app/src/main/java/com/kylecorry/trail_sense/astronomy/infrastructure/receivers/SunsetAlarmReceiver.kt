063a61a9ddd15c91c0239880ec05c59c7b80f4b2
Kyle Corry <kylecorry31@gmail.com>
2023-07-04 13:31:25 -0400

    Switch sunset alert back to an exact alarm

--- app/src/main/java/com/kylecorry/trail_sense/astronomy/infrastructure/receivers/SunsetAlarmReceiver.kt
+++ app/src/main/java/com/kylecorry/trail_sense/astronomy/infrastructure/receivers/SunsetAlarmReceiver.kt
@@ -3,12 +3,11 @@ package com.kylecorry.trail_sense.astronomy.infrastructure.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.kylecorry.andromeda.background.AlarmBroadcastTaskScheduler
 import com.kylecorry.andromeda.background.IOneTimeTaskScheduler
+import com.kylecorry.andromeda.background.OneTimeTaskSchedulerFactory
 import com.kylecorry.andromeda.core.system.Intents
 import com.kylecorry.trail_sense.astronomy.infrastructure.SunsetAlarmService
 import com.kylecorry.trail_sense.shared.UserPreferences
-import java.time.Duration
 
 class SunsetAlarmReceiver : BroadcastReceiver() {
 
@@ -27,15 +26,7 @@ class SunsetAlarmReceiver : BroadcastReceiver() {
         private const val PI_ID = 8309
 
         fun scheduler(context: Context): IOneTimeTaskScheduler {
-            return AlarmBroadcastTaskScheduler(
-                context.applicationContext,
-                SunsetAlarmReceiver::class.java,
-                PI_ID,
-                exact = false,
-                inexactWindow = Duration.ofMinutes(10),
-                isWindowCentered = true,
-                allowWhileIdle = true
-            )
+            return OneTimeTaskSchedulerFactory(context).exact(SunsetAlarmReceiver::class.java, PI_ID)
         }
 
         fun start(context: Context) {
