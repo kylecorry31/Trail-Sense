29038093ea5df6d294c1278d19c6244ace8fb4de
Kyle Corry <kylecorry31@gmail.com>
2023-07-05 15:02:50 -0400

    Request exact alarm permission for sunset alerts

--- app/src/main/java/com/kylecorry/trail_sense/settings/ui/AstronomySettingsFragment.kt
+++ app/src/main/java/com/kylecorry/trail_sense/settings/ui/AstronomySettingsFragment.kt
@@ -1,12 +1,16 @@
 package com.kylecorry.trail_sense.settings.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.preference.ListPreference
+import com.kylecorry.andromeda.core.topics.generic.asLiveData
 import com.kylecorry.andromeda.fragments.AndromedaPreferenceFragment
 import com.kylecorry.trail_sense.R
 import com.kylecorry.trail_sense.astronomy.infrastructure.receivers.SunsetAlarmReceiver
 import com.kylecorry.trail_sense.shared.QuickActionUtils
 import com.kylecorry.trail_sense.shared.UserPreferences
+import com.kylecorry.trail_sense.shared.permissions.requestScheduleExactAlarms
+import com.kylecorry.trail_sense.shared.preferences.PreferencesSubsystem
 
 class AstronomySettingsFragment : AndromedaPreferenceFragment() {
 
@@ -20,13 +24,6 @@ class AstronomySettingsFragment : AndromedaPreferenceFragment() {
         val userPrefs = UserPreferences(requireContext())
         prefs = userPrefs
 
-        list(R.string.pref_sunset_alert_time)?.setOnPreferenceClickListener {
-            context?.apply {
-                    SunsetAlarmReceiver.start(this)
-                }
-                true
-            }
-
         prefleftButton = list(R.string.pref_astronomy_quick_action_left)
         prefrightButton = list(R.string.pref_astronomy_quick_action_right)
 
@@ -39,8 +36,36 @@ class AstronomySettingsFragment : AndromedaPreferenceFragment() {
 
         prefleftButton?.entryValues = actionValues.toTypedArray()
         prefrightButton?.entryValues = actionValues.toTypedArray()
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val alertTimePrefKey = getString(R.string.pref_sunset_alert_time)
+        val alertPrefKey = getString(R.string.pref_sunset_alerts)
 
+        PreferencesSubsystem.getInstance(requireContext()).preferences.onChange.asLiveData()
+            .observe(viewLifecycleOwner) {
+                if (it == alertTimePrefKey){
+                    restartSunsetAlerts(false)
+                } else if (it == alertPrefKey){
+                    restartSunsetAlerts(true)
+                }
+            }
+    }
+
+    private fun restartSunsetAlerts(shouldRequestPermissions: Boolean){
+        if (!prefs.astronomy.sendSunsetAlerts) {
+            return
+        }
+
+        if (shouldRequestPermissions){
+            // TODO: Show toast to background location permission - since it may be beneficial if the user is traveling
+            requestScheduleExactAlarms {
+                SunsetAlarmReceiver.start(requireContext())
+            }
+        } else {
+            SunsetAlarmReceiver.start(requireContext())
+        }
     }
 
 }
