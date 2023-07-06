4db542b68bc521237accf843605a045a2ac3ad44
Kyle Corry <kylecorry31@gmail.com>
2023-07-06 16:03:43 -0400

    Improve string fixer

--- scripts/string-fixer.py
+++ scripts/string-fixer.py
@@ -2,13 +2,17 @@ import xml.etree.ElementTree as ET
 import os
 import re
 
+should_fix_issues = False
+show_warnings = True
+
+
 def read_xml(file) -> ET.ElementTree:
     ET.register_namespace('tools', 'http://schemas.android.com/tools')
     parser = ET.XMLParser(target=ET.TreeBuilder(insert_comments=True))
     return ET.parse(file, parser=parser)
 
-def write_xml(tree, file):
-    tree.write(file, encoding='utf-8', xml_declaration=True)
+def write_xml(tree: ET.ElementTree, file):
+    tree.write(file, encoding='utf-8', xml_declaration=True, short_empty_elements=False)
 
 def delete_element(tree, element):
     root = tree.getroot()
@@ -24,44 +28,182 @@ def get_string_element(tree, name):
     root = tree.getroot()
     return root.find('string[@name="' + name + '"]')
 
+def address_issue(tree, element, diagnostic, file):
+    diagnostic_name = diagnostic.__class__.__name__
+    file_parent_folder = file.split('/')[-2]
+    file_name = file.split('/')[-1]
+    if should_fix_issues and diagnostic.fix(tree, tree, element):
+        print(f'[Fixed] {diagnostic_name} {element.get("name")} in {file_parent_folder}/{file_name}')
+    elif not diagnostic.is_warning():
+        print(f'[Fail] {diagnostic_name} {element.get("name")} in {file_parent_folder}/{file_name}')
+    elif show_warnings:
+        print(f'[Warn] {diagnostic_name} {element.get("name")} in {file_parent_folder}/{file_name}')
+
 class StringDiagnostic(object):
     def check(self, source_tree, tree, element) -> bool:
         return True
 
-    def fix(self, source_tree, tree, element):
-        pass
+    def fix(self, source_tree, tree, element) -> bool:
+        return False
+    
+    def is_warning(self) -> bool:
+        return False
 
-class NonTranslatableDiagnostic(StringDiagnostic):
+class NonTranslatableTranslated(StringDiagnostic):
     def check(self, source_tree, tree, element) -> bool:
         if tree == source_tree:
             return False
         source_element = get_string_element(source_tree, element.get('name'))
         return source_element.get('translatable') == 'false'
 
-    def fix(self, source_tree, tree, element):
+    def fix(self, source_tree, tree, element) -> bool:
         delete_element(tree, element)
+        return True
+    
+    def is_warning(self) -> bool:
+        return True
 
+class PreferenceKeyTranslatable(StringDiagnostic):
+    def check(self, source_tree, tree, element) -> bool:
+        if source_tree != tree:
+            return False
+        string_value = element.text.strip()
+        if '_' in string_value and all(char.islower() or char.isdigit() or char == '_' for char in string_value):
+            if not string_elem.get('translatable') or string_elem.get('translatable').lower() != 'false':
+                return True
+        return False
 
-script_dir = os.path.dirname(os.path.realpath(__file__))
+    def fix(self, source_tree, tree, element) -> bool:
+        add_attribute(element, 'translatable', 'false')
+        return True
+    
+    def is_warning(self) -> bool:
+        return True
+
+class FormattingDoesNotMatch(StringDiagnostic):
+    def check(self, source_tree, tree, element) -> bool:
+        if source_tree == tree:
+            return False
+        source_element = get_string_element(source_tree, element.get('name'))
+        if source_element is None:
+            return False
+        # Count % signs in the reference string
+        reference_count = source_element.text.count('%')
+        # Count % signs in the string
+        string_count = element.text.count('%')
+        return string_count != reference_count
+
+    def fix(self, source_tree, tree, element) -> bool:
+        return False
+    
+    def is_warning(self) -> bool:
+        return False
+
+class PositionalFormattingUnspecified(StringDiagnostic):
+    def check(self, source_tree, tree, element) -> bool:
+        # Regex to get all format arguments without a positional index
+        r = r'%[sd]'
+        # Find all matches
+        matches = re.findall(r, element.text)
+        # If there are no matches, continue
+        if not matches or len(matches) < 2:
+            return False
+        return True
+
+    def fix(self, source_tree, tree, element) -> bool:
+        # Regex to get all format arguments without a positional index
+        r = r'%[sd]'
+        # Find all matches
+        matches = re.findall(r, element.text)
+        # Add the positional index to the matches within the string
+        for i, match in enumerate(matches):
+            replace_text(element, element.text.replace(match, '%' + str(i + 1) + '$' + match[1], 1))
+        return True
+    
+    def is_warning(self) -> bool:
+        return True
+
+class NotInSource(StringDiagnostic):
+    def check(self, source_tree, tree, element) -> bool:
+        if source_tree == tree:
+            return False
+        source_element = get_string_element(source_tree, element.get('name'))
+        return source_element is None
+
+    def fix(self, source_tree, tree, element) -> bool:
+        delete_element(tree, element)
+        return True
+    
+    def is_warning(self) -> bool:
+        return True
+
+class TranslatedAppName(StringDiagnostic):
+    def check(self, source_tree, tree, element) -> bool:
+        app_name = get_string_element(source_tree, 'app_name').text
+        source_element = get_string_element(source_tree, element.get('name'))
+        if source_element is not None and app_name in source_element.text:
+            return app_name not in element.text
+        return False
+
+    def fix(self, source_tree, tree, element) -> bool:
+        return False
+    
+    def is_warning(self) -> bool:
+        return True
+
+class HardCodedAppName(StringDiagnostic):
+    def check(self, source_tree, tree, element) -> bool:
+        if source_tree != tree:
+            return False
+        app_name = get_string_element(source_tree, 'app_name').text
+        source_element = get_string_element(source_tree, element.get('name'))
+        if source_element is not None and app_name in source_element.text and element.attrib.get('translatable') != 'false':
+            return app_name in element.text
+        return False
+
+    def fix(self, source_tree, tree, element) -> bool:
+        return False
+    
+    def is_warning(self) -> bool:
+        return True
+
+script_dir = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/')
 
 reference_file = script_dir + '/../app/src/main/res/values/strings.xml'
 reference_tree = read_xml(reference_file)
 
 diagnostics = [
-    NonTranslatableDiagnostic()
+    PreferenceKeyTranslatable(),
+    NonTranslatableTranslated(),
+    FormattingDoesNotMatch(),
+    NotInSource(),
+    PositionalFormattingUnspecified(),
+    TranslatedAppName(),
+    HardCodedAppName()
 ]
+
+
+# Run diagnostics on reference file
+for diagnostic in diagnostics:
+    for string_elem in reference_tree.iter('string'):
+        if diagnostic.check(reference_tree, reference_tree, string_elem):
+            address_issue(reference_tree, string_elem, diagnostic, reference_file)
+if should_fix_issues:
+    write_xml(reference_tree, reference_file)
+
 
 # Find all strings.xml files
 for root, dirs, files in os.walk(script_dir + '/../app/src/main/res'):
     for file in files:
         if file == 'strings.xml':
-            strings_file_path = os.path.join(root, file)
+            strings_file_path = os.path.join(root, file).replace('\\', '/')
+            if strings_file_path == reference_file:
+                continue
+            tree = read_xml(strings_file_path)
+            root = tree.getroot()
             for diagnostic in diagnostics:
-                tree = read_xml(strings_file_path)
-                root = tree.getroot()
                 for string_elem in root.iter('string'):
                     if diagnostic.check(reference_tree, tree, string_elem):
-                        diagnostic.fix(reference_tree, tree, string_elem)
-                        print(f'Fixed {string_elem.get("name")} in {strings_file_path}')
-                        break
-            write_xml(tree, strings_file_path)
+                        address_issue(tree, string_elem, diagnostic, strings_file_path)
+            if should_fix_issues:
+                write_xml(tree, strings_file_path)
