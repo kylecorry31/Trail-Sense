8c1347a93021ba9c8d427e6e38b750aee718d660
Kyle Corry <kylecorry31@gmail.com>
2023-08-20 16:12:27 -0400

    Cache declination

--- app/src/main/java/com/kylecorry/trail_sense/shared/declination/GPSDeclinationStrategy.kt
+++ app/src/main/java/com/kylecorry/trail_sense/shared/declination/GPSDeclinationStrategy.kt
@@ -3,12 +3,26 @@ package com.kylecorry.trail_sense.shared.declination
 import com.kylecorry.andromeda.location.IGPS
 import com.kylecorry.sol.science.geology.Geology
 import com.kylecorry.sol.science.geology.IGeologyService
+import com.kylecorry.sol.units.Coordinate
+import com.kylecorry.sol.units.Distance
 
 class GPSDeclinationStrategy(
     private val gps: IGPS,
-    private val geology: IGeologyService = Geology
+    private val geology: IGeologyService = Geology,
+    private val cacheRadiusMeters: Float = 1000f
 ) : IDeclinationStrategy {
+
+    private var cachedDeclination: Float? = null
+    private var cachedLocation: Coordinate? = null
+    private val lock = Any()
+
     override fun getDeclination(): Float {
-        return geology.getGeomagneticDeclination(gps.location, gps.altitude)
+        synchronized(lock) {
+            if (cachedLocation == null || cachedLocation!!.distanceTo(gps.location) > cacheRadiusMeters) {
+                cachedLocation = gps.location
+                cachedDeclination = geology.getGeomagneticDeclination(gps.location, gps.altitude)
+            }
+            return cachedDeclination!!
+        }
     }
 }
