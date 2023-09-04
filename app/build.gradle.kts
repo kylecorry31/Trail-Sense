c9ff4d6d98647dcfa9c940a23f5e484bb4fb4d6e
Kyle Corry <kylecorry31@gmail.com>
2023-09-04 08:42:24 -0400

    Don't restart app on error
    Closes #1952

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,7 +119,7 @@ dependencies {
     implementation("com.google.android.flexbox:flexbox:3.0.0")
 
     // Andromeda
-    val andromedaVersion = "b0ae4d1e21"
+    val andromedaVersion = "143295626d"
     implementation("com.github.kylecorry31.andromeda:core:$andromedaVersion")
     implementation("com.github.kylecorry31.andromeda:fragments:$andromedaVersion")
     implementation("com.github.kylecorry31.andromeda:forms:$andromedaVersion")
