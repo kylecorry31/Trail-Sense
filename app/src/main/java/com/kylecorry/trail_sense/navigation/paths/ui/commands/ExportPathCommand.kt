57e50aec217c0e42a0f889dfd2b8cc8ec9f4c9ba
Kyle Corry <kylecorry31@gmail.com>
2023-09-09 13:47:04 -0400

    Add internal support for path group export

--- app/src/main/java/com/kylecorry/trail_sense/navigation/paths/ui/commands/ExportPathCommand.kt
+++ app/src/main/java/com/kylecorry/trail_sense/navigation/paths/ui/commands/ExportPathCommand.kt
@@ -4,13 +4,18 @@ import android.content.Context
 import androidx.lifecycle.LifecycleOwner
 import com.kylecorry.andromeda.alerts.Alerts
 import com.kylecorry.andromeda.core.coroutines.BackgroundMinimumState
+import com.kylecorry.andromeda.core.coroutines.onIO
+import com.kylecorry.andromeda.core.coroutines.onMain
 import com.kylecorry.andromeda.fragments.inBackground
 import com.kylecorry.andromeda.gpx.GPXData
 import com.kylecorry.trail_sense.R
 import com.kylecorry.trail_sense.navigation.paths.domain.FullPath
+import com.kylecorry.trail_sense.navigation.paths.domain.IPath
 import com.kylecorry.trail_sense.navigation.paths.domain.IPathService
 import com.kylecorry.trail_sense.navigation.paths.domain.Path
 import com.kylecorry.trail_sense.navigation.paths.domain.PathGPXConverter
+import com.kylecorry.trail_sense.navigation.paths.domain.PathGroup
+import com.kylecorry.trail_sense.navigation.paths.infrastructure.PathGroupLoader
 import com.kylecorry.trail_sense.navigation.paths.infrastructure.persistence.PathService
 import com.kylecorry.trail_sense.shared.io.IOService
 import kotlinx.coroutines.Dispatchers
@@ -23,34 +28,50 @@ class ExportPathCommand(
     private val lifecycleOwner: LifecycleOwner,
     private val gpxService: IOService<GPXData>,
     private val pathService: IPathService = PathService.getInstance(context)
-) : IPathCommand {
+) {
 
-    // TODO: Take in an IPath
-    override fun execute(path: Path) {
+    fun execute(path: IPath) {
         lifecycleOwner.inBackground(BackgroundMinimumState.Created) {
-            // TODO: Load all children of the path (if any)
-            // TODO: Ask the user what paths to export (if the path is a Path, don't ask)
-            // TODO: Load all waypoints for the paths
-            // TODO: Assign parent groups and convert to FullPath
-            val waypoints = pathService.getWaypoints(path.id)
-            val parent = pathService.getGroup(path.parentId)
-            val full = FullPath(path, waypoints, parent)
-            val gpx = PathGPXConverter().toGPX(full)
+
+            val all = getPaths(path)
+            val paths = all.filterIsInstance<Path>()
+            val groups = all.filterIsInstance<PathGroup>().associateBy { it.id }
+
+            // TODO: Ask the user what paths to export, unless the path is a Path
+            val chosenIds = paths.map { it.id }
+
+            // Load the waypoints and associate them with the paths
+            val waypoints = pathService.getWaypoints(chosenIds)
+            val pathsToExport = paths
+                .filter { chosenIds.contains(it.id) }
+                .map {
+                    val parent = it.parentId?.let { id -> groups[id] }
+                    FullPath(it, waypoints[it.id] ?: emptyList(), parent)
+                }
+
+            // Export to a GPX file
+            val gpx = PathGPXConverter().toGPX(pathsToExport)
             val exportFile = "trail-sense-${Instant.now().epochSecond}.gpx"
             val success = gpxService.export(gpx, exportFile)
-            withContext(Dispatchers.Main) {
+
+            // Notify the user
+            onMain {
                 if (success) {
-                    Alerts.toast(
-                        context,
-                        context.getString(R.string.path_exported)
-                    )
+                    Alerts.toast(context, context.getString(R.string.path_exported))
                 } else {
-                    Alerts.toast(
-                        context,
-                        context.getString(R.string.export_path_error)
-                    )
+                    Alerts.toast(context, context.getString(R.string.export_path_error))
                 }
             }
         }
     }
+
+    private suspend fun getPaths(path: IPath): List<IPath> = onIO {
+        if (path is Path) {
+            listOf(path)
+        } else {
+            listOf(path) + pathService.loader().getChildren(path.id)
+        }
+    }
+
+
 }
