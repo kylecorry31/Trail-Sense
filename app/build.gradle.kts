b0c68256e4982fad69fb3dacbc0f48336cb5569b
Kyle Corry <kylecorry31@gmail.com>
2023-11-15 16:19:44 -0500

    Use gridview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -128,7 +128,7 @@ dependencies {
     implementation("com.google.android.flexbox:flexbox:3.0.0")
 
     // Andromeda
-    val andromedaVersion = "fa3bb4e4f8"
+    val andromedaVersion = "062b83e922"
     implementation("com.github.kylecorry31.andromeda:core:$andromedaVersion")
     implementation("com.github.kylecorry31.andromeda:fragments:$andromedaVersion")
     implementation("com.github.kylecorry31.andromeda:forms:$andromedaVersion")
@@ -162,7 +162,7 @@ dependencies {
     implementation("com.github.kylecorry31.andromeda:list:$andromedaVersion")
 
     // Ceres
-    val ceresVersion = "e087704b28"
+    val ceresVersion = "53f10aa952"
     implementation("com.github.kylecorry31.ceres:list:$ceresVersion")
     implementation("com.github.kylecorry31.ceres:toolbar:$ceresVersion")
     implementation("com.github.kylecorry31.ceres:badge:$ceresVersion")
