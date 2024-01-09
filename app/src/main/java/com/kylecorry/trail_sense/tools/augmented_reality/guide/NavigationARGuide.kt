5e847e6952f8208fe8f1b47a0539c99ef6ed0bb2
Kyle Corry <kylecorry31@gmail.com>
2024-01-09 17:23:44 -0500

    Add navigation guide to AR

--- app/src/main/java/com/kylecorry/trail_sense/tools/augmented_reality/guide/NavigationARGuide.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/augmented_reality/guide/NavigationARGuide.kt
@@ -1,7 +1,16 @@
 package com.kylecorry.trail_sense.tools.augmented_reality.guide
 
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.view.LayoutInflater
 import android.widget.FrameLayout
+import androidx.core.view.isVisible
+import com.kylecorry.andromeda.core.ui.Colors
+import com.kylecorry.trail_sense.R
+import com.kylecorry.trail_sense.databinding.ViewArNavigationGuideBinding
 import com.kylecorry.trail_sense.navigation.infrastructure.Navigator
+import com.kylecorry.trail_sense.shared.CustomUiUtils
+import com.kylecorry.trail_sense.shared.extensions.onMain
 import com.kylecorry.trail_sense.tools.augmented_reality.AugmentedRealityView
 import com.kylecorry.trail_sense.tools.augmented_reality.position.GeographicARPoint
 import kotlinx.coroutines.CoroutineScope
@@ -16,11 +25,31 @@ class NavigationARGuide(private val navigator: Navigator) : ARGuide {
 
     override fun start(arView: AugmentedRealityView, panel: FrameLayout) {
         job?.cancel()
+        val layoutInflater = LayoutInflater.from(panel.context)
+        val binding = ViewArNavigationGuideBinding.inflate(layoutInflater, panel, true)
+        binding.arGuideCancel.setOnClickListener {
+            navigator.cancelNavigation()
+        }
         job = scope.launch {
             navigator.destination.collect {
                 if (it == null) {
+                    onMain {
+                        panel.isVisible = false
+                    }
                     arView.clearGuide()
                 } else {
+                    onMain {
+                        panel.isVisible = true
+                        binding.arGuideIcon.setImageResource(
+                            it.icon?.icon ?: R.drawable.ic_location
+                        )
+                        binding.arGuideIcon.backgroundTintList = ColorStateList.valueOf(it.color)
+                        Colors.setImageColor(
+                            binding.arGuideIcon,
+                            Colors.mostContrastingColor(Color.WHITE, Color.BLACK, it.color)
+                        )
+                        binding.arGuideName.text = it.name
+                    }
                     arView.guideTo(GeographicARPoint(it.coordinate, it.elevation)) {
                         // Do nothing when reached
                     }
@@ -30,6 +59,7 @@ class NavigationARGuide(private val navigator: Navigator) : ARGuide {
     }
 
     override fun stop(arView: AugmentedRealityView, panel: FrameLayout) {
+        panel.removeAllViews()
         job?.cancel()
         arView.clearGuide()
     }
