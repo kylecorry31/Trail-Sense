03ccef5092624f6adf9fb84aa55f7a3bd7c3105c
Kyle Corry <kylecorry31@gmail.com>
2024-02-02 07:28:04 -0500

    Use distance pref helper on nav settings

--- app/src/main/java/com/kylecorry/trail_sense/tools/navigation/ui/NavigationSettingsFragment.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/navigation/ui/NavigationSettingsFragment.kt
@@ -3,23 +3,21 @@ package com.kylecorry.trail_sense.tools.navigation.ui
 import android.os.Bundle
 import android.text.InputType
 import androidx.preference.ListPreference
-import androidx.preference.Preference
 import com.kylecorry.andromeda.fragments.AndromedaPreferenceFragment
 import com.kylecorry.sol.units.Distance
 import com.kylecorry.trail_sense.R
-import com.kylecorry.trail_sense.shared.CustomUiUtils
 import com.kylecorry.trail_sense.shared.DistanceUtils
 import com.kylecorry.trail_sense.shared.DistanceUtils.toRelativeDistance
 import com.kylecorry.trail_sense.shared.FormatService
 import com.kylecorry.trail_sense.shared.UserPreferences
 import com.kylecorry.trail_sense.shared.permissions.alertNoActivityRecognitionPermission
 import com.kylecorry.trail_sense.shared.permissions.requestActivityRecognition
+import com.kylecorry.trail_sense.shared.preferences.setupDistanceSetting
 import com.kylecorry.trail_sense.shared.sensors.SensorService
 import com.kylecorry.trail_sense.tools.tools.ui.Tools
 
 class NavigationSettingsFragment : AndromedaPreferenceFragment() {
 
-    private var prefNearbyRadius: Preference? = null
     private var prefLeftButton: ListPreference? = null
     private var prefRightButton: ListPreference? = null
     private val formatService by lazy { FormatService.getInstance(requireContext()) }
@@ -28,7 +26,6 @@ class NavigationSettingsFragment : AndromedaPreferenceFragment() {
     private lateinit var prefs: UserPreferences
 
     private fun bindPreferences() {
-        prefNearbyRadius = preference(R.string.pref_nearby_radius_holder)
         prefLeftButton = list(R.string.pref_navigation_quick_action_left)
         prefRightButton = list(R.string.pref_navigation_quick_action_right)
     }
@@ -49,24 +46,16 @@ class NavigationSettingsFragment : AndromedaPreferenceFragment() {
         prefLeftButton?.entryValues = actionValues.toTypedArray()
         prefRightButton?.entryValues = actionValues.toTypedArray()
 
-
-        prefNearbyRadius?.setOnPreferenceClickListener {
-            val units = formatService.sortDistanceUnits(DistanceUtils.hikingDistanceUnits)
-
-            CustomUiUtils.pickDistance(
-                requireContext(),
-                units,
-                Distance.meters(userPrefs.navigation.maxBeaconDistance)
-                    .convertTo(userPrefs.baseDistanceUnits).toRelativeDistance(),
-                it.title.toString()
-            ) { distance, _ ->
+        setupDistanceSetting(
+            getString(R.string.pref_nearby_radius_holder),
+            { relative(Distance.meters(userPrefs.navigation.maxBeaconDistance), userPrefs) },
+            { distance ->
                 if (distance != null && distance.distance > 0) {
                     userPrefs.navigation.maxBeaconDistance = distance.meters().distance
-                    updateNearbyRadius()
                 }
-            }
-            true
-        }
+            },
+            DistanceUtils.hikingDistanceUnits
+        )
 
         editText(R.string.pref_num_visible_beacons)
             ?.setOnBindEditTextListener { editText ->
@@ -96,8 +85,10 @@ class NavigationSettingsFragment : AndromedaPreferenceFragment() {
         } else {
             preference(R.string.pref_show_dial_ticks_when_no_compass)?.isVisible = false
         }
+    }
 
-        updateNearbyRadius()
+    private fun relative(distance: Distance, prefs: UserPreferences): Distance {
+        return distance.convertTo(prefs.baseDistanceUnits).toRelativeDistance()
     }
 
     private fun onCurrentPaceSpeedometerSelected() {
@@ -106,13 +97,5 @@ class NavigationSettingsFragment : AndromedaPreferenceFragment() {
                 alertNoActivityRecognitionPermission()
             }
         }
-    }
-
-    private fun updateNearbyRadius() {
-        prefNearbyRadius?.summary = formatService.formatDistance(
-            Distance.meters(prefs.navigation.maxBeaconDistance).convertTo(prefs.baseDistanceUnits)
-                .toRelativeDistance(),
-            2
-        )
     }
 }
