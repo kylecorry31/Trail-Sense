c18f5fa703213323a4fd59dce8d649caff2cdb0b
Alessandro Frangiamone <alessandro.frangiamone96@gmail.com>
2024-02-18 02:21:55 +0100

    Update Andromeda library version + Update gitignore to exclude shelf on idea folder

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -133,7 +133,7 @@ dependencies {
     implementation("com.google.android.flexbox:flexbox:3.0.0")
 
     // Andromeda
-    val andromedaVersion = "819c64764e"
+    val andromedaVersion = "6e071b6b3a"
     implementation("com.github.kylecorry31.andromeda:core:$andromedaVersion")
     implementation("com.github.kylecorry31.andromeda:fragments:$andromedaVersion")
     implementation("com.github.kylecorry31.andromeda:forms:$andromedaVersion")
