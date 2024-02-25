97edacd63103aa12ce68b8f6032a65e6f61f1b18
Kyle Corry <kylecorry31@gmail.com>
2024-02-25 06:51:11 -0500

    Render AR astronomy paths using Path

--- app/src/main/java/com/kylecorry/trail_sense/tools/augmented_reality/ui/layers/ARLineLayer.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/augmented_reality/ui/layers/ARLineLayer.kt
@@ -1,19 +1,24 @@
 package com.kylecorry.trail_sense.tools.augmented_reality.ui.layers
 
+import android.graphics.Path
 import com.kylecorry.andromeda.canvas.ICanvasDrawer
 import com.kylecorry.andromeda.canvas.StrokeCap
+import com.kylecorry.andromeda.core.cache.ObjectPool
 import com.kylecorry.andromeda.core.units.PixelCoordinate
 import com.kylecorry.trail_sense.shared.canvas.LineClipper
+import com.kylecorry.trail_sense.shared.extensions.drawLines
 import com.kylecorry.trail_sense.shared.getBounds
 import com.kylecorry.trail_sense.tools.augmented_reality.domain.position.AugmentedRealityCoordinate
 import com.kylecorry.trail_sense.tools.augmented_reality.ui.ARLine
 import com.kylecorry.trail_sense.tools.augmented_reality.ui.AugmentedRealityView
 
-class ARLineLayer : ARLayer {
+class ARLineLayer(private val renderWithPaths: Boolean = false) : ARLayer {
 
     private val lines = mutableListOf<ARLine>()
     private val lineLock = Any()
-
+    private var renderedLines: List<Pair<ARLine, FloatArray>> = emptyList()
+    private val pathPool = ObjectPool { Path() }
+    private var renderedPaths: List<Pair<ARLine, Path>> = emptyList()
     private val clipper = LineClipper()
 
     fun setLines(lines: List<ARLine>) {
@@ -29,12 +34,26 @@ class ARLineLayer : ARLayer {
         }
     }
 
-    private var renderedLines: List<Pair<ARLine, FloatArray>> = emptyList()
 
     override suspend fun update(drawer: ICanvasDrawer, view: AugmentedRealityView) {
         renderedLines = synchronized(lineLock) {
             lines.map {
                 it to render(it.points.map { it.getAugmentedRealityCoordinate(view) }, view)
+            }
+        }
+
+        if (renderWithPaths) {
+            renderedPaths = synchronized(lineLock) {
+                // Free current paths
+                renderedPaths.forEach {
+                    pathPool.release(it.second)
+                }
+                renderedLines.map {
+                    val path = pathPool.get()
+                    path.reset()
+                    path.drawLines(it.second)
+                    it.first to path
+                }
             }
         }
     }
@@ -48,8 +67,14 @@ class ARLineLayer : ARLayer {
             renderedLines.toList()
         }
 
+        val renderedPaths = synchronized(lineLock) {
+            renderedPaths.toList()
+        }
+
         // Draw lines
-        for ((line, points) in rendered) {
+        for (i in rendered.indices) {
+            val (line, points) = rendered[i]
+            val path = renderedPaths.getOrNull(i)?.second
             if (line.outlineColor != null) {
                 drawer.stroke(line.outlineColor)
                 val outlinePx = when (line.thicknessUnits) {
@@ -57,7 +82,11 @@ class ARLineLayer : ARLayer {
                     ARLine.ThicknessUnits.Angle -> view.sizeToPixel(line.thickness + 2 * line.outlineThickness)
                 }
                 drawer.strokeWeight(outlinePx)
-                drawer.lines(points)
+                if (renderWithPaths && path != null) {
+                    drawer.path(path)
+                } else {
+                    drawer.lines(points)
+                }
             }
 
             drawer.stroke(line.color)
@@ -66,7 +95,11 @@ class ARLineLayer : ARLayer {
                 ARLine.ThicknessUnits.Angle -> view.sizeToPixel(line.thickness)
             }
             drawer.strokeWeight(thicknessPx)
-            drawer.lines(points)
+            if (renderWithPaths && path != null) {
+                drawer.path(path)
+            } else {
+                drawer.lines(points)
+            }
         }
 
         drawer.noStroke()
