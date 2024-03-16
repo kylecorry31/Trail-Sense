a6ebac31e15ef3a60692edc7cf5d37677ef2a3a4
Muraino Yakubu <murainoy@yahoo.com>
2024-03-16 14:57:27 +0000

    optimize import

--- app/src/main/java/com/kylecorry/trail_sense/tools/metaldetector/ui/FragmentToolMetalDetector.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/metaldetector/ui/FragmentToolMetalDetector.kt
@@ -1,7 +1,6 @@
 package com.kylecorry.trail_sense.tools.metaldetector.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
