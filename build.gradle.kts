65119ec1d5cd5ff086abcadcb7303441ae81295f
Kyle Corry <kylecorry31@gmail.com>
2024-03-21 18:10:35 -0400

    Update AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.3.0" apply false
+    id("com.android.application") version "8.3.1" apply false
     id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
 }
