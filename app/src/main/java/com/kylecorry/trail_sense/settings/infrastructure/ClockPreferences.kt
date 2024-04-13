b68d7e1a2a5fbccb22654562d7edc6c11904af73
Muraino Yakubu <murainoy@yahoo.com>
2024-04-13 08:54:44 +0100

    created ClockPreferences to manage any clock settings

---
+++ app/src/main/java/com/kylecorry/trail_sense/settings/infrastructure/ClockPreferences.kt
@@ -0,0 +1,15 @@
+package com.kylecorry.trail_sense.settings.infrastructure
+
+import android.content.Context
+import com.kylecorry.andromeda.preferences.BooleanPreference
+import com.kylecorry.trail_sense.R
+
+class ClockPreferences(context: Context) : PreferenceRepo(context) {
+
+    var enabledAnalogView by BooleanPreference(
+        cache,
+        context.getString(R.string.pref_enable_analog_clock),
+        true
+    )
+
+}
