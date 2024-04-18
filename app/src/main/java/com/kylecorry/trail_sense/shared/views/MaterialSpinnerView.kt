8be2b80e91733a75d9f7eff986d728c5861a75e1
Kyle Corry <kylecorry31@gmail.com>
2024-04-18 18:06:02 -0400

    Use item picker for material spinner view

--- app/src/main/java/com/kylecorry/trail_sense/shared/views/MaterialSpinnerView.kt
+++ app/src/main/java/com/kylecorry/trail_sense/shared/views/MaterialSpinnerView.kt
@@ -2,75 +2,50 @@ package com.kylecorry.trail_sense.shared.views
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import android.widget.FrameLayout
-import android.widget.Spinner
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import com.kylecorry.andromeda.pickers.Pickers
 import com.kylecorry.trail_sense.R
 
 class MaterialSpinnerView(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
 
-    private val spinner: Spinner
     private val edittext: TextInputEditText
     private val holder: TextInputLayout
     private var listener: ((Int?) -> Unit)? = null
+    private var items: List<String> = listOf()
 
-    val selectedItemPosition: Int
-        get() = spinner.selectedItemPosition
+    var selectedItemPosition: Int = 0
+        private set
 
     val selectedItem: String?
-        get() = spinner.selectedItem?.toString()
+        get() = items.getOrNull(selectedItemPosition)
 
     init {
         inflate(context, R.layout.view_material_spinner, this)
-        spinner = findViewById(R.id.material_spinner_spinner)
         edittext = findViewById(R.id.material_spinner_edittext)
         holder = findViewById(R.id.material_spinner_holder)
 
         edittext.setOnClickListener {
-            spinner.performClick()
-        }
-
-        spinner.onItemSelectedListener = object :
-            AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                listener?.invoke(position)
-                edittext.setText(spinner.selectedItem.toString())
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                listener?.invoke(null)
-                edittext.setText("")
+            Pickers.item(context, holder.hint ?: "", items, selectedItemPosition) {
+                it ?: return@item
+                setSelection(it)
             }
         }
-
     }
 
     fun setItems(items: List<String>) {
-        val adapter = ArrayAdapter(
-            context,
-            R.layout.spinner_item_plain,
-            R.id.item_name,
-            items
-        )
-        spinner.adapter = adapter
+        this.items = items
     }
 
     fun setSelection(position: Int) {
-        spinner.setSelection(position)
+        selectedItemPosition = position
+        listener?.invoke(position)
+        edittext.setText(items.getOrNull(position) ?: "")
     }
 
     fun setHint(hint: String) {
         holder.hint = hint
-        spinner.prompt = hint
     }
 
     fun setOnItemSelectedListener(listener: (Int?) -> Unit) {
