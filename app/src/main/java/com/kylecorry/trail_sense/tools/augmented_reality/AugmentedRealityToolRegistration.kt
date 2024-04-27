69a84174a3cca98f659bbedc7fe48620a1077841
Kyle Corry <kylecorry31@gmail.com>
2024-04-27 09:22:04 -0400

    Disable flashlight volume toggle when on AR tool

--- app/src/main/java/com/kylecorry/trail_sense/tools/augmented_reality/AugmentedRealityToolRegistration.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/augmented_reality/AugmentedRealityToolRegistration.kt
@@ -3,9 +3,12 @@ package com.kylecorry.trail_sense.tools.augmented_reality
 import android.content.Context
 import com.kylecorry.trail_sense.R
 import com.kylecorry.trail_sense.shared.sensors.SensorService
+import com.kylecorry.trail_sense.shared.volume.SystemVolumeAction
 import com.kylecorry.trail_sense.tools.tools.infrastructure.ToolRegistration
 import com.kylecorry.trail_sense.tools.tools.infrastructure.Tool
 import com.kylecorry.trail_sense.tools.tools.infrastructure.ToolCategory
+import com.kylecorry.trail_sense.tools.tools.infrastructure.ToolVolumeAction
+import com.kylecorry.trail_sense.tools.tools.infrastructure.ToolVolumeActionPriority
 import com.kylecorry.trail_sense.tools.tools.infrastructure.Tools
 
 object AugmentedRealityToolRegistration : ToolRegistration {
@@ -19,7 +22,14 @@ object AugmentedRealityToolRegistration : ToolRegistration {
             context.getString(R.string.augmented_reality_description),
             guideId = R.raw.guide_tool_augmented_reality,
             settingsNavAction = R.id.augmentedRealitySettingsFragment,
-            isAvailable = { SensorService(it).hasCompass() }
+            isAvailable = { SensorService(it).hasCompass() },
+            volumeActions = listOf(
+                ToolVolumeAction(
+                    ToolVolumeActionPriority.Normal,
+                    { _, isOpen -> isOpen },
+                    ::SystemVolumeAction
+                )
+            )
         )
     }
 }
