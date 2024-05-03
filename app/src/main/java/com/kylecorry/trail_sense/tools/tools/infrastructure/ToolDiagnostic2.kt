41956b659938e0eb6aec30a2517de9dda99d578a
Kyle Corry <kylecorry31@gmail.com>
2024-05-03 16:03:27 -0400

    Add tool diagnostic action helper methods

--- app/src/main/java/com/kylecorry/trail_sense/tools/tools/infrastructure/ToolDiagnostic2.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/tools/infrastructure/ToolDiagnostic2.kt
@@ -1,7 +1,15 @@
 package com.kylecorry.trail_sense.tools.tools.infrastructure
 
+import android.content.Context
+import android.content.Intent
+import androidx.annotation.IdRes
+import androidx.navigation.fragment.findNavController
 import com.kylecorry.andromeda.core.sensors.Quality
+import com.kylecorry.andromeda.core.system.Intents
 import com.kylecorry.andromeda.fragments.AndromedaFragment
+import com.kylecorry.trail_sense.R
+import com.kylecorry.trail_sense.shared.commands.Command
+import com.kylecorry.trail_sense.shared.navigateWithAnimation
 import com.kylecorry.trail_sense.shared.sensors.SensorService
 import com.kylecorry.trail_sense.tools.diagnostics.domain.Severity
 import com.kylecorry.trail_sense.tools.flashlight.infrastructure.FlashlightSubsystem
@@ -31,7 +39,47 @@ data class ToolDiagnosticResult(
 data class ToolDiagnosticAction(
     val name: String,
     val action: (fragment: AndromedaFragment) -> Unit
-)
+) {
+    companion object {
+        fun navigate(
+            @IdRes to: Int,
+            title: String
+        ): ToolDiagnosticAction {
+            return ToolDiagnosticAction(title) {
+                it.findNavController().navigateWithAnimation(to)
+            }
+        }
+
+        fun command(
+            command: Command,
+            title: String
+        ): ToolDiagnosticAction {
+            return ToolDiagnosticAction(title) {
+                command.execute()
+            }
+        }
+
+        fun intent(
+            intent: Intent,
+            title: String
+        ): ToolDiagnosticAction {
+            return ToolDiagnosticAction(title) {
+                it.startActivity(intent)
+            }
+        }
+
+        fun permissions(context: Context): ToolDiagnosticAction {
+            return intent(Intents.appSettings(context), context.getString(R.string.settings))
+        }
+
+        fun notification(context: Context, channel: String?): ToolDiagnosticAction {
+            return intent(
+                Intents.notificationSettings(context, channel),
+                context.getString(R.string.settings)
+            )
+        }
+    }
+}
 
 // An example diagnostic that checks for the presence of a flashlight
 //val flashlightDiagnostic = ToolDiagnostic2(
