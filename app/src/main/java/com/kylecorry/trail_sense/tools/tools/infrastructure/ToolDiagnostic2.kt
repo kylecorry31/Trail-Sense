16886ec37e01b46fedd91e9dc9bc251697b4545c
Kyle Corry <kylecorry31@gmail.com>
2024-05-03 13:12:34 -0400

    WIP classes for new diagnostics

---
+++ app/src/main/java/com/kylecorry/trail_sense/tools/tools/infrastructure/ToolDiagnostic2.kt
@@ -0,0 +1,80 @@
+package com.kylecorry.trail_sense.tools.tools.infrastructure
+
+import com.kylecorry.andromeda.core.sensors.Quality
+import com.kylecorry.andromeda.fragments.AndromedaFragment
+import com.kylecorry.trail_sense.shared.sensors.SensorService
+import com.kylecorry.trail_sense.tools.diagnostics.domain.Severity
+import com.kylecorry.trail_sense.tools.flashlight.infrastructure.FlashlightSubsystem
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+
+data class ToolDiagnostic2(
+    val id: String,
+    val name: String,
+    val quickScan: (fragment: AndromedaFragment) -> List<ToolDiagnosticResult>,
+    val fullScan: (fragment: AndromedaFragment) -> Flow<List<ToolDiagnosticResult>> = {
+        flowOf(quickScan(it))
+    }
+)
+
+data class ToolDiagnosticResult(
+    val id: String,
+    val severity: Severity,
+    val name: String,
+    val description: String,
+    val resolution: String? = null,
+    val action: ToolDiagnosticAction? = null
+)
+
+data class ToolDiagnosticAction(
+    val name: String,
+    val action: (fragment: AndromedaFragment) -> Unit
+)
+
+// An example diagnostic that checks for the presence of a flashlight
+//val flashlightDiagnostic = ToolDiagnostic2(
+//    "flashlight-diagnostic",
+//    "Flashlight",
+//    quickScan = {
+//        if (!FlashlightSubsystem.getInstance(it.requireContext()).isAvailable()) {
+//            listOf(
+//                ToolDiagnosticResult(
+//                    "flashlight-diagnostic",
+//                    Severity.Warning,
+//                    "Flashlight not available",
+//                    "The flashlight is not available on this device"
+//                )
+//            )
+//        } else {
+//            emptyList()
+//        }
+//    })
+//
+//val gpsQualityDiagnostic = ToolDiagnostic2(
+//    "gps-quality-diagnostic",
+//    "GPS Quality",
+//    quickScan = {
+//        emptyList()
+//    },
+//    fullScan = {
+//        val gps = SensorService(it.requireContext()).getGPS()
+//        gps.flow.map {
+//            if (gps.quality == Quality.Poor) {
+//                listOf(
+//                    ToolDiagnosticResult(
+//                        "gps-quality-diagnostic",
+//                        Severity.Warning,
+//                        "GPS Quality",
+//                        "The GPS signal quality is poor",
+//                        "Move to an area with a better view of the sky"
+//                    )
+//                )
+//            } else {
+//                emptyList()
+//            }
+//        }
+//            .distinctUntilChanged()
+//    }
+//)
