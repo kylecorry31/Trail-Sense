caec70ec06b9563cfe6a95555f867b17abe936cc
BloodWalrus <BloodWalrus@users.noreply.github.com>
2024-05-25 19:13:25 +0100

    Reverted AppColor enum because everything went blue

--- app/src/main/java/com/kylecorry/trail_sense/shared/colors/AppColor.kt
+++ app/src/main/java/com/kylecorry/trail_sense/shared/colors/AppColor.kt
@@ -1,20 +1,21 @@
 package com.kylecorry.trail_sense.shared.colors
 
 import androidx.annotation.ColorInt
+import androidx.core.content.res.ResourcesCompat
 import com.kylecorry.trail_sense.shared.data.Identifiable
 import com.kylecorry.trail_sense.R
 
 enum class AppColor(override val id: Long, @ColorInt override val color: Int): IAppColor {
-    Red(0, R.color.red), // #ef5350
-    Orange(1, R.color.orange), // #FF6D00
-    Yellow(2, R.color.yellow), // #DDCE2C
-    Green(3, R.color.green), // #81c784
-    Blue(4, R.color.blue), // #a0caff
-    Purple(5, R.color.purple), // #c158dc
-    Pink(6, R.color.pink), // #ff77a9
-    Gray(7, R.color.gray), // #9e9e9e
-    Brown(8, R.color.brown), // #a98274
-    DarkBlue(9, R.color.dark_blue) // #304ffe
+    Red(0, -1092784), // #ef5350
+    Orange(1, -37632), // #FF6D00
+    Yellow(2, -2240980), // #DDCE2C
+    Green(3, -8271996), // #81c784
+    Blue(4, -6239489), // #a0caff
+    Purple(5, -4106020), // #c158dc
+    Pink(6, -34903), // #ff77a9
+    Gray(7, -6381922), // #9e9e9e
+    Brown(8, -5668236), // #a98274
+    DarkBlue(9, -13611010) // #304ffe
 }
 
 fun Array<AppColor>.fromColor(@ColorInt color: Int): AppColor? {
