b3e05bd85834cd9bc7c0e5d6c178403230830cc7
Kyle Corry <kylecorry31@gmail.com>
2024-06-08 14:42:29 -0400

    Update sol

--- app/src/main/java/com/kylecorry/trail_sense/tools/weather/infrastructure/subsystem/WeatherSubsystem.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/weather/infrastructure/subsystem/WeatherSubsystem.kt
@@ -336,7 +336,7 @@ class WeatherSubsystem private constructor(private val context: Context) : IWeat
         val last = history.lastOrNull()
         val (location, elevation) = getLocationAndElevation(last?.id)
         val forecaster = getWeatherForecaster(location, elevation)
-        return forecaster.forecast(history, allClouds)
+        return forecaster.forecast(history, allClouds, location)
     }
 
     private suspend fun getLocationAndElevation(lastReadingId: Long? = null): Pair<Coordinate, Distance> {
