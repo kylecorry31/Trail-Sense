f20bd59e742c99bf7b3df300dcbe1cd3071145b7
Kyle Corry <kylecorry31@gmail.com>
2024-07-05 15:21:01 -0400

    Add backtrack tool service (unused)

---
+++ app/src/main/java/com/kylecorry/trail_sense/tools/paths/services/BacktrackToolService.kt
@@ -0,0 +1,79 @@
+package com.kylecorry.trail_sense.tools.paths.services
+
+import android.content.Context
+import android.util.Log
+import com.kylecorry.andromeda.permissions.Permissions
+import com.kylecorry.trail_sense.R
+import com.kylecorry.trail_sense.receivers.ServiceRestartAlerter
+import com.kylecorry.trail_sense.shared.UserPreferences
+import com.kylecorry.trail_sense.shared.extensions.tryStartForegroundOrNotify
+import com.kylecorry.trail_sense.shared.permissions.canStartLocationForgroundService
+import com.kylecorry.trail_sense.tools.paths.PathsToolRegistration
+import com.kylecorry.trail_sense.tools.paths.infrastructure.BacktrackScheduler
+import com.kylecorry.trail_sense.tools.tools.infrastructure.ToolService2
+import com.kylecorry.trail_sense.tools.tools.infrastructure.Tools
+import java.time.Duration
+
+class BacktrackToolService(private val context: Context) : ToolService2 {
+
+    private val prefs = UserPreferences(context)
+
+    override val name: String = context.getString(R.string.backtrack)
+
+    override fun getFrequency(): Duration {
+        return prefs.backtrackRecordFrequency
+    }
+
+    override fun isRunning(): Boolean {
+        return isEnabled() && !isBlocked()
+    }
+
+    override fun isEnabled(): Boolean {
+        return prefs.backtrackEnabled
+    }
+
+    override fun isBlocked(): Boolean {
+        return prefs.isLowPowerModeOn && prefs.lowPowerModeDisablesBacktrack
+    }
+
+    override suspend fun enable() {
+        if (!Permissions.canStartLocationForgroundService(context)) {
+            ServiceRestartAlerter(context).alert()
+            Log.d("BacktrackSubsystem", "Cannot start backtrack")
+            return
+        }
+
+
+        prefs.backtrackEnabled = true
+        Tools.broadcast(PathsToolRegistration.BROADCAST_BACKTRACK_ENABLED)
+        if (!isBlocked()) {
+            start(true)
+        }
+    }
+
+    override suspend fun disable() {
+        prefs.backtrackEnabled = false
+        Tools.broadcast(PathsToolRegistration.BROADCAST_BACKTRACK_DISABLED)
+        stop()
+    }
+
+    override suspend fun restart() {
+        if (isEnabled() && !isBlocked()) {
+            start(false)
+        } else {
+            stop()
+        }
+    }
+
+    override suspend fun stop() {
+        BacktrackScheduler.stop(context)
+        // TODO: Broadcast
+    }
+
+    private suspend fun start(startNewPath: Boolean) {
+        tryStartForegroundOrNotify(context) {
+            BacktrackScheduler.start(context, startNewPath)
+            // TODO: Broadcast
+        }
+    }
+}
