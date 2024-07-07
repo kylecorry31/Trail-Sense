4110bceab1d0e0b85d2f9dd976317548e9b1ebaf
Kyle Corry <kylecorry31@gmail.com>
2024-07-07 10:42:55 -0400

    Add tool service tile

---
+++ app/src/main/java/com/kylecorry/trail_sense/shared/tiles/ToolServiceTile.kt
@@ -0,0 +1,81 @@
+package com.kylecorry.trail_sense.shared.tiles
+
+import android.os.Build
+import android.os.Bundle
+import android.service.quicksettings.Tile
+import androidx.annotation.RequiresApi
+import com.kylecorry.andromeda.background.services.AndromedaTileService
+import com.kylecorry.andromeda.core.tryOrLog
+import com.kylecorry.trail_sense.shared.FeatureState
+import com.kylecorry.trail_sense.shared.FormatService
+import com.kylecorry.trail_sense.tools.tools.infrastructure.Tools
+import com.kylecorry.trail_sense.tools.tools.infrastructure.getFeatureState
+
+
+@RequiresApi(Build.VERSION_CODES.N)
+abstract class ToolServiceTile(
+    private val serviceId: String,
+    private val stateChangeBroadcastId: String,
+    private val frequencyChangeBroadcastId: String? = null
+) : AndromedaTileService() {
+    protected val service by lazy { Tools.getService(this, serviceId) }
+
+    private val formatter by lazy { FormatService.getInstance(this) }
+
+    abstract fun stop()
+    abstract fun start()
+
+    override fun onClick() {
+        super.onClick()
+
+        when (service?.getFeatureState()) {
+            FeatureState.On -> stop()
+            FeatureState.Off -> start()
+            else -> {}
+        }
+    }
+
+    override fun onStartListening() {
+        Tools.subscribe(stateChangeBroadcastId, this::onStateChanged)
+        updateState()
+        if (frequencyChangeBroadcastId != null) {
+            Tools.subscribe(frequencyChangeBroadcastId, this::onFrequencyChanged)
+            updateFrequency()
+        }
+    }
+
+    override fun onStopListening() {
+        Tools.unsubscribe(stateChangeBroadcastId, this::onStateChanged)
+        if (frequencyChangeBroadcastId != null) {
+            Tools.unsubscribe(frequencyChangeBroadcastId, this::onFrequencyChanged)
+        }
+    }
+
+    private fun onFrequencyChanged(data: Bundle): Boolean {
+        updateFrequency()
+        return true
+    }
+
+    private fun onStateChanged(data: Bundle): Boolean {
+        updateState()
+        return true
+    }
+
+    private fun updateState() {
+        setState(
+            when (service?.getFeatureState()) {
+                FeatureState.On -> Tile.STATE_ACTIVE
+                FeatureState.Off -> Tile.STATE_INACTIVE
+                else -> Tile.STATE_UNAVAILABLE
+            }
+        )
+    }
+
+    private fun updateFrequency() {
+        val frequency = service?.getFrequency() ?: return
+        val formatted = formatter.formatDuration(frequency, includeSeconds = true)
+        tryOrLog {
+            setSubtitle(formatted)
+        }
+    }
+}
--- app/src/main/java/com/kylecorry/trail_sense/tools/paths/tiles/BacktrackTile.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/paths/tiles/BacktrackTile.kt
@@ -2,46 +2,28 @@ package com.kylecorry.trail_sense.tools.paths.tiles
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import com.kylecorry.andromeda.core.topics.generic.ITopic
-import com.kylecorry.andromeda.core.topics.generic.map
-import com.kylecorry.andromeda.core.topics.generic.replay
-import com.kylecorry.trail_sense.shared.FeatureState
-import com.kylecorry.trail_sense.shared.FormatService
-import com.kylecorry.trail_sense.shared.tiles.TopicTile
+import com.kylecorry.trail_sense.shared.tiles.ToolServiceTile
 import com.kylecorry.trail_sense.tools.paths.PathsToolRegistration
-import com.kylecorry.trail_sense.tools.paths.infrastructure.subsystem.BacktrackSubsystem
-import com.kylecorry.trail_sense.tools.tools.infrastructure.Tools
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 @RequiresApi(Build.VERSION_CODES.N)
-class BacktrackTile : TopicTile() {
-    private val backtrack by lazy { BacktrackSubsystem.getInstance(this) }
-    private val backtrackService by lazy {
-        Tools.getService(
-            this,
-            PathsToolRegistration.SERVICE_BACKTRACK
-        )
-    }
-    private val formatter by lazy { FormatService.getInstance(this) }
-
-    override val stateTopic: ITopic<FeatureState>
-        get() = backtrack.state.replay()
-
-    override val subtitleTopic: ITopic<String>
-        get() = backtrack.frequency.map { formatter.formatDuration(it, includeSeconds = true) }
-            .replay()
+class BacktrackTile : ToolServiceTile(
+    PathsToolRegistration.SERVICE_BACKTRACK,
+    PathsToolRegistration.BROADCAST_BACKTRACK_STATE_CHANGED,
+    PathsToolRegistration.BROADCAST_BACKTRACK_FREQUENCY_CHANGED
+) {
 
     override fun stop() {
         CoroutineScope(Dispatchers.Default).launch {
-            backtrackService?.disable()
+            service?.disable()
         }
     }
 
     override fun start() {
         startForegroundService {
-            backtrackService?.enable()
+            service?.enable()
         }
     }
 }
