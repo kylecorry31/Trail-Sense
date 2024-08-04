c1f85b5f1a7766f5c3d6cfcc45c2d0b5a1eadf51
Kyle Corry <kylecorry31@gmail.com>
2024-08-04 11:39:15 -0400

    Prevent crash on malformed CSV

--- app/src/main/java/com/kylecorry/trail_sense/shared/io/CsvIOService.kt
+++ app/src/main/java/com/kylecorry/trail_sense/shared/io/CsvIOService.kt
@@ -1,5 +1,6 @@
 package com.kylecorry.trail_sense.shared.io
 
+import com.kylecorry.andromeda.core.tryOrDefault
 import com.kylecorry.andromeda.csv.CSVConvert
 import com.kylecorry.luna.coroutines.onIO
 import com.kylecorry.luna.streams.readText
@@ -13,17 +14,19 @@ class CsvIOService(private val uriPicker: UriPicker, private val uriService: Uri
     }
 
     override suspend fun import(): List<List<String>>? = onIO {
-        val uri =
-            uriPicker.open(
-                listOf(
-                    "text/csv",
-                    "application/csv",
-                    "text/comma-separated-values",
-                    "text/plain"
+        tryOrDefault(null) {
+            val uri =
+                uriPicker.open(
+                    listOf(
+                        "text/csv",
+                        "application/csv",
+                        "text/comma-separated-values",
+                        "text/plain"
+                    )
                 )
-            )
-                ?: return@onIO null
-        val stream = uriService.inputStream(uri) ?: return@onIO null
-        CSVConvert.parse(stream.readText())
+                    ?: return@onIO null
+            val stream = uriService.inputStream(uri) ?: return@onIO null
+            CSVConvert.parse(stream.readText())
+        }
     }
 }
