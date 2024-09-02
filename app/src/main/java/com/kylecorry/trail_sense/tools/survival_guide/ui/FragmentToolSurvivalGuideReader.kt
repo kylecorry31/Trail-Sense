2ef0d86dcf81b5722d45183154e1f74d37c42e6d
Kyle Corry <kylecorry31@gmail.com>
2024-09-02 08:51:45 -0400

    Print guide

--- app/src/main/java/com/kylecorry/trail_sense/tools/survival_guide/ui/FragmentToolSurvivalGuideReader.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/survival_guide/ui/FragmentToolSurvivalGuideReader.kt
@@ -1,16 +1,34 @@
 package com.kylecorry.trail_sense.tools.survival_guide.ui
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.net.toUri
+import androidx.core.view.isVisible
+import com.kylecorry.andromeda.alerts.Alerts
 import com.kylecorry.andromeda.core.coroutines.onIO
+import com.kylecorry.andromeda.core.ui.Views
 import com.kylecorry.andromeda.fragments.BoundFragment
 import com.kylecorry.andromeda.fragments.inBackground
+import com.kylecorry.andromeda.markdown.MarkdownService
+import com.kylecorry.andromeda.pdf.PdfConvert
+import com.kylecorry.andromeda.print.ColorMode
+import com.kylecorry.andromeda.print.Orientation
+import com.kylecorry.andromeda.print.Printer
+import com.kylecorry.andromeda.print.ScaleMode
+import com.kylecorry.luna.coroutines.onMain
+import com.kylecorry.trail_sense.R
 import com.kylecorry.trail_sense.databinding.FragmentSurvivalGuideBinding
+import com.kylecorry.trail_sense.shared.debugging.isDebug
+import com.kylecorry.trail_sense.shared.io.DeleteTempFilesCommand
+import com.kylecorry.trail_sense.shared.io.FileSubsystem
 import com.kylecorry.trail_sense.tools.guide.infrastructure.UserGuideService
 import com.kylecorry.trail_sense.tools.guide.infrastructure.UserGuideUtils
 import com.kylecorry.trail_sense.tools.survival_guide.domain.Chapters
+import kotlinx.coroutines.delay
 
 class FragmentToolSurvivalGuideReader : BoundFragment<FragmentSurvivalGuideBinding>() {
 
@@ -43,6 +61,52 @@ class FragmentToolSurvivalGuideReader : BoundFragment<FragmentSurvivalGuideBindi
                         shouldUppercaseSubheadings = true
                     )
                 )
+            }
+        }
+
+        binding.guideName.rightButton.isVisible = isDebug()
+        binding.guideName.rightButton.setOnClickListener {
+            inBackground {
+                onIO {
+                    val loading =
+                        onMain { Alerts.loading(requireContext(), getString(R.string.loading)) }
+                    try {
+                        // TODO: Extract this to a shared markdown printer
+                        val printer = Printer(requireContext())
+                        printer.setColorMode(ColorMode.Color)
+                        printer.setScaleMode(ScaleMode.Fit)
+                        printer.setOrientation(Orientation.Portrait)
+                        val content =
+                            "# ${chapter.chapter}: ${chapter.title}\n" + UserGuideService(
+                                requireContext()
+                            ).load(
+                                chapterResourceId ?: return@onIO
+                            )
+                        val files = FileSubsystem.getInstance(requireContext())
+                        val file = files.createTemp("pdf")
+                        // TODO: Indented bullet points do not work properly when on a separate page
+                        // TODO: Bullet point continuation does not render properly on a separate page
+                        // TODO: Images are overlapping text
+                        val markdown = MarkdownService(requireContext())
+                        val parsed = markdown.toMarkdown(content)
+                        val textView = Views.text(requireContext(), null) as TextView
+                        textView.textSize = 6f
+                        textView.setTextColor(Color.BLACK)
+                        markdown.setParsedMarkdown(textView, parsed)
+                        // Wait for images to load
+                        // TODO: Find a better way to do this
+                        delay(2000)
+                        val uri = file.toUri()
+                        files.output(uri)?.use {
+                            PdfConvert.toPdf(textView, it)
+                        }
+                        onMain { loading.dismiss() }
+                        printer.print(uri)
+                    } finally {
+                        DeleteTempFilesCommand(requireContext()).execute()
+                        onMain { loading.dismiss() }
+                    }
+                }
             }
         }
     }
