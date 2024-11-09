0ea9519e4ae5097e311a484d2eef0182e016a392
Kyle Corry <kylecorry31@gmail.com>
2024-11-09 16:05:37 -0500

    Add tool summary class
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

---
+++ app/src/main/java/com/kylecorry/trail_sense/tools/tools/infrastructure/ToolSummary.kt
@@ -0,0 +1,17 @@
+package com.kylecorry.trail_sense.tools.tools.infrastructure
+
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import com.kylecorry.trail_sense.tools.tools.ui.ToolSummaryView
+
+data class ToolSummary(
+    val id: String,
+    val name: String,
+    val size: ToolSummarySize = ToolSummarySize.Full,
+    val create: (root: FrameLayout, fragment: Fragment) -> ToolSummaryView
+)
+
+enum class ToolSummarySize {
+    Half,
+    Full
+}
