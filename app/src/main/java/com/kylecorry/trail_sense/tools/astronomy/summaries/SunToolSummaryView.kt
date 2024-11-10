36d4d56ab2e9b380889a44d5b5685f69ccde2b7c
Kyle Corry <kylecorry31@gmail.com>
2024-11-10 07:37:57 -0500

    Add marquee support
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/astronomy/summaries/SunToolSummaryView.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/astronomy/summaries/SunToolSummaryView.kt
@@ -3,9 +3,9 @@ package com.kylecorry.trail_sense.tools.astronomy.summaries
 import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.kylecorry.andromeda.fragments.inBackground
 import com.kylecorry.luna.coroutines.onDefault
 import com.kylecorry.luna.coroutines.onMain
+import com.kylecorry.luna.timer.CoroutineTimer
 import com.kylecorry.trail_sense.R
 import com.kylecorry.trail_sense.shared.FormatService
 import com.kylecorry.trail_sense.shared.openTool
@@ -13,6 +13,8 @@ import com.kylecorry.trail_sense.tools.astronomy.domain.AstronomySubsystem
 import com.kylecorry.trail_sense.tools.astronomy.domain.AstronomyTransition
 import com.kylecorry.trail_sense.tools.tools.infrastructure.Tools
 import com.kylecorry.trail_sense.tools.tools.ui.summaries.SmallSimpleToolSummaryView
+import java.time.Duration
+import java.time.LocalDateTime
 
 class SunToolSummaryView(root: FrameLayout, fragment: Fragment) : SmallSimpleToolSummaryView(
     root,
@@ -21,12 +23,18 @@ class SunToolSummaryView(root: FrameLayout, fragment: Fragment) : SmallSimpleToo
 
     private val formatService = FormatService.getInstance(context)
     private val astronomy = AstronomySubsystem.getInstance(context)
+    private val timer = CoroutineTimer {
+        populateSunDetails()
+    }
 
-    override fun onCreate() {
-        super.onCreate()
-        fragment.inBackground {
-            populateSunDetails()
-        }
+    override fun onResume() {
+        super.onResume()
+        timer.interval(1000 * 60)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        timer.stop()
     }
 
     private suspend fun populateSunDetails() {
@@ -34,13 +42,27 @@ class SunToolSummaryView(root: FrameLayout, fragment: Fragment) : SmallSimpleToo
 
         onMain {
             if (sun.nextRise != null && sun.nextTransition == AstronomyTransition.Rise) {
-                binding.summarySubtitle.text =
+                val time =
                     formatService.formatTime(sun.nextRise.toLocalTime(), includeSeconds = false)
+                val timeUntil = formatService.formatDuration(
+                    Duration.between(
+                        LocalDateTime.now(),
+                        sun.nextRise
+                    )
+                )
+                binding.summarySubtitle.text = "$time ($timeUntil)"
                 binding.summaryTitle.text = context.getString(R.string.sunrise)
                 binding.summaryIcon.setImageResource(R.drawable.ic_sunrise_notification)
             } else if (sun.nextSet != null && sun.nextTransition == AstronomyTransition.Set) {
-                binding.summarySubtitle.text =
+                val time =
                     formatService.formatTime(sun.nextSet.toLocalTime(), includeSeconds = false)
+                val timeUntil = formatService.formatDuration(
+                    Duration.between(
+                        LocalDateTime.now(),
+                        sun.nextSet
+                    )
+                )
+                binding.summarySubtitle.text = "$time ($timeUntil)"
                 binding.summaryTitle.text = context.getString(R.string.sunset)
                 binding.summaryIcon.setImageResource(R.drawable.ic_sunset_notification)
             } else if (sun.isUp) {
