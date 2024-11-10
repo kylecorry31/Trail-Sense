b17747435a0fff2bc6a0d0bd8e68fb4b5e7cd3b0
Kyle Corry <kylecorry31@gmail.com>
2024-11-10 13:23:42 -0500

    Fix widgets on older versions of android
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/astronomy/widgets/MoonToolWidgetView.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/astronomy/widgets/MoonToolWidgetView.kt
@@ -1,12 +1,14 @@
 package com.kylecorry.trail_sense.tools.astronomy.widgets
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.widget.RemoteViews
+import androidx.core.graphics.drawable.toBitmap
 import com.kylecorry.andromeda.core.system.Resources
 import com.kylecorry.luna.coroutines.onMain
 import com.kylecorry.trail_sense.R
 import com.kylecorry.trail_sense.shared.FormatService
-import com.kylecorry.trail_sense.shared.extensions.setImageViewResourceAsIcon
 import com.kylecorry.trail_sense.shared.navigation.NavigationUtils
 import com.kylecorry.trail_sense.tools.astronomy.domain.AstronomySubsystem
 import com.kylecorry.trail_sense.tools.astronomy.ui.MoonPhaseImageMapper
@@ -15,12 +17,18 @@ import com.kylecorry.trail_sense.tools.tools.ui.widgets.SimpleToolWidgetView
 import kotlinx.coroutines.launch
 
 class MoonToolWidgetView : SimpleToolWidgetView() {
+
+    private var lastBitmap: Bitmap? = null
+    private var nextBitmap: Bitmap? = null
+
     override fun onUpdate(context: Context, views: RemoteViews, commit: () -> Unit) {
         scope.launch {
             populateMoonDetails(context, views)
             onMain {
                 commit()
             }
+            lastBitmap?.recycle()
+            lastBitmap = nextBitmap
         }
     }
 
@@ -29,13 +37,34 @@ class MoonToolWidgetView : SimpleToolWidgetView() {
         val formatter = FormatService.getInstance(context)
         val moon = astronomy.moon
         val image = MoonPhaseImageMapper().getPhaseImage(moon.phase)
-        views.setImageViewResourceAsIcon(context, ICON_IMAGEVIEW, image)
-        views.setFloat(ICON_IMAGEVIEW, "setRotation", moon.tilt)
+        val bitmap = Resources.drawable(context, image)?.toBitmap(
+            Resources.dp(context, 36f).toInt(),
+            Resources.dp(context, 36f).toInt(),
+        )
+        val rotated = bitmap?.let { rotate(it, moon.tilt) }
+        bitmap?.recycle()
+        nextBitmap = rotated
+        views.setImageViewBitmap(ICON_IMAGEVIEW, rotated)
         views.setTextViewText(TITLE_TEXTVIEW, context.getString(R.string.moon))
         views.setTextViewText(SUBTITLE_TEXTVIEW, formatter.formatMoonPhase(moon.phase))
         views.setOnClickPendingIntent(
             ROOT,
             NavigationUtils.toolPendingIntent(context, Tools.ASTRONOMY)
         )
+    }
+
+    private fun rotate(bitmap: Bitmap, degrees: Float): Bitmap {
+        val newBitmap = Bitmap.createBitmap(
+            bitmap.width,
+            bitmap.height,
+            bitmap.config ?: Bitmap.Config.ARGB_8888,
+        )
+
+        val canvas = Canvas(newBitmap)
+
+        canvas.rotate(degrees, bitmap.width / 2f, bitmap.height / 2f)
+        canvas.drawBitmap(bitmap, 0f, 0f, null)
+
+        return newBitmap
     }
 }
