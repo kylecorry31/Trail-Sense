a04fc64758748afb4dec907ce23715b708f9ab28
Kyle Corry <kylecorry31@gmail.com>
2024-11-12 16:33:08 -0500

    Add support for in app events
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/tools/ui/widgets/ToolWidgetView.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/tools/ui/widgets/ToolWidgetView.kt
@@ -2,12 +2,15 @@ package com.kylecorry.trail_sense.tools.tools.ui.widgets
 
 import android.content.Context
 import android.widget.RemoteViews
+import androidx.lifecycle.Lifecycle
 
 interface ToolWidgetView {
 
     fun onEnabled(context: Context)
 
     fun onDisabled(context: Context)
+
+    fun onInAppEvent(context: Context, event: Lifecycle.Event, triggerUpdate: () -> Unit)
 
     fun onUpdate(context: Context, views: RemoteViews, commit: () -> Unit)
 
