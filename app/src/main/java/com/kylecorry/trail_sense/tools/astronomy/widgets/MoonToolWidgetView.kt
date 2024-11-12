9d77c555ec0d3e5a133fbc6b5a1655a31f2d9ec3
Kyle Corry <kylecorry31@gmail.com>
2024-11-11 20:00:23 -0500

    Use view rotation on newer APIs
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/astronomy/widgets/MoonToolWidgetView.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/astronomy/widgets/MoonToolWidgetView.kt
@@ -3,12 +3,15 @@ package com.kylecorry.trail_sense.tools.astronomy.widgets
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.os.Build
 import android.widget.RemoteViews
 import androidx.core.graphics.drawable.toBitmap
+import androidx.core.widget.RemoteViewsCompat.setViewRotation
 import com.kylecorry.andromeda.core.system.Resources
 import com.kylecorry.luna.coroutines.onMain
 import com.kylecorry.trail_sense.R
 import com.kylecorry.trail_sense.shared.FormatService
+import com.kylecorry.trail_sense.shared.extensions.setImageViewResourceAsIcon
 import com.kylecorry.trail_sense.shared.navigation.NavigationUtils
 import com.kylecorry.trail_sense.tools.astronomy.domain.AstronomySubsystem
 import com.kylecorry.trail_sense.tools.astronomy.ui.MoonPhaseImageMapper
@@ -39,14 +42,19 @@ class MoonToolWidgetView : SimpleToolWidgetView() {
         val formatter = FormatService.getInstance(context)
         val moon = astronomy.moon
         val image = MoonPhaseImageMapper().getPhaseImage(moon.phase)
-        val bitmap = Resources.drawable(context, image)?.toBitmap(
-            Resources.dp(context, 32f).toInt(),
-            Resources.dp(context, 32f).toInt(),
-        )
-        val rotated = bitmap?.let { rotate(it, moon.tilt) }
-        bitmap?.recycle()
-        nextBitmap = rotated
-        views.setImageViewBitmap(ICON_IMAGEVIEW, rotated)
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+            val bitmap = Resources.drawable(context, image)?.toBitmap(
+                Resources.dp(context, 32f).toInt(),
+                Resources.dp(context, 32f).toInt(),
+            )
+            val rotated = bitmap?.let { rotate(it, moon.tilt) }
+            bitmap?.recycle()
+            nextBitmap = rotated
+            views.setImageViewBitmap(ICON_IMAGEVIEW, rotated)
+        } else {
+            views.setImageViewResourceAsIcon(context, ICON_IMAGEVIEW, image)
+            views.setViewRotation(ICON_IMAGEVIEW, moon.tilt)
+        }
         views.setTextViewText(TITLE_TEXTVIEW, context.getString(R.string.moon))
         views.setTextViewText(SUBTITLE_TEXTVIEW, formatter.formatMoonPhase(moon.phase))
         views.setOnClickPendingIntent(
