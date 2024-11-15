d8e6704da3e85ceef6eca488cc7978159c28c5a9
Kyle Corry <kylecorry31@gmail.com>
2024-11-15 18:46:35 -0500

    Add pressure widget (#2668)
    * Add pressure widget
    
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>
    
    * Switch to default dispatcher
    
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>
    
    ---------
    
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

---
+++ app/src/main/java/com/kylecorry/trail_sense/tools/weather/widgets/AppWidgetPressure.kt
@@ -0,0 +1,6 @@
+package com.kylecorry.trail_sense.tools.weather.widgets
+
+import com.kylecorry.trail_sense.tools.tools.widgets.AppWidgetBase
+import com.kylecorry.trail_sense.tools.weather.WeatherToolRegistration
+
+class AppWidgetPressure : AppWidgetBase(WeatherToolRegistration.WIDGET_PRESSURE_TENDENCY)
