ee44dfd14307291fd986cdf6bf9e6d9fd9cff9f4
Kyle Corry <kylecorry31@gmail.com>
2024-11-16 10:53:54 -0500

    Update location/elevation for all widgets when stale if location is allowed
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/tools/services/WidgetUpdateWorker.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/tools/services/WidgetUpdateWorker.kt
@@ -5,6 +5,14 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.kylecorry.andromeda.background.IPeriodicTaskScheduler
 import com.kylecorry.andromeda.background.PeriodicTaskSchedulerFactory
+import com.kylecorry.andromeda.core.system.Wakelocks
+import com.kylecorry.andromeda.core.tryOrLog
+import com.kylecorry.andromeda.permissions.Permissions
+import com.kylecorry.trail_sense.shared.ParallelCoroutineRunner
+import com.kylecorry.trail_sense.shared.permissions.canGetLocationCustom
+import com.kylecorry.trail_sense.shared.sensors.LocationSubsystem
+import com.kylecorry.trail_sense.shared.sensors.SensorSubsystem
+import com.kylecorry.trail_sense.shared.sensors.SensorSubsystem.SensorRefreshPolicy
 import com.kylecorry.trail_sense.shared.widgets.WidgetUtils
 import com.kylecorry.trail_sense.tools.tools.infrastructure.Tools
 import java.time.Duration
@@ -13,14 +21,50 @@ class WidgetUpdateWorker(context: Context, params: WorkerParameters) :
     CoroutineWorker(context, params) {
 
     override suspend fun doWork(): Result {
-        Tools.getTools(applicationContext).flatMap { it.widgets }.forEach {
-            WidgetUtils.triggerUpdate(applicationContext, it.widgetClass)
+        val wakelock = Wakelocks.get(applicationContext, "WidgetUpdateWorker")
+        wakelock?.acquire(Duration.ofSeconds(30).toMillis())
+        try {
+            // Update stale location/elevation data if needed
+            tryOrLog {
+                updateStaleSensorData()
+            }
+
+            // Update all widgets
+            Tools.getTools(applicationContext).flatMap { it.widgets }.forEach {
+                WidgetUtils.triggerUpdate(applicationContext, it.widgetClass)
+            }
+        } finally {
+            wakelock?.release()
         }
         return Result.success()
     }
 
-    companion object {
+    private suspend fun updateStaleSensorData() {
+        val locationSubsystem = LocationSubsystem.getInstance(applicationContext)
+        val sensorSubsystem = SensorSubsystem.getInstance(applicationContext)
 
+        if (Permissions.canGetLocationCustom(applicationContext)) {
+            val parallelRunner = ParallelCoroutineRunner()
+            parallelRunner.run(listOfNotNull(
+                if (locationSubsystem.locationAge.toMinutes() > 30) {
+                    suspend {
+                        sensorSubsystem.getLocation(SensorRefreshPolicy.Refresh)
+                    }
+                } else {
+                    null
+                },
+                if (locationSubsystem.elevationAge.toMinutes() > 30) {
+                    suspend {
+                        sensorSubsystem.getElevation(SensorRefreshPolicy.Refresh)
+                    }
+                } else {
+                    null
+                }
+            ))
+        }
+    }
+
+    companion object {
         private const val UNIQUE_ID = 267389
 
         fun start(context: Context) {
