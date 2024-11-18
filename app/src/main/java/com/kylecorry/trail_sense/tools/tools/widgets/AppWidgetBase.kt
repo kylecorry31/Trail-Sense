9e2d2f9eb1f2bfd561593be2d7f578a89996af69
Kyle Corry <kylecorry31@gmail.com>
2024-11-18 18:02:14 -0500

    Add dynamic color support to widgets
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/tools/widgets/AppWidgetBase.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/tools/widgets/AppWidgetBase.kt
@@ -3,17 +3,26 @@ package com.kylecorry.trail_sense.tools.tools.widgets
 import android.appwidget.AppWidgetManager
 import android.content.Context
 import android.widget.RemoteViews
+import com.kylecorry.andromeda.core.system.Resources
 import com.kylecorry.andromeda.widgets.AndromedaCoroutineWidget
 import com.kylecorry.trail_sense.R
+import com.kylecorry.trail_sense.shared.UserPreferences
 import com.kylecorry.trail_sense.tools.tools.infrastructure.Tools
 
 abstract class AppWidgetBase(private val widgetId: String) :
-    AndromedaCoroutineWidget(themeToReload = R.style.AppTheme) {
+    AndromedaCoroutineWidget(themeToReload = R.style.WidgetTheme) {
 
     override suspend fun getUpdatedRemoteViews(
         context: Context,
         appWidgetManager: AppWidgetManager
     ): RemoteViews {
+        val prefs = UserPreferences(context)
+        if (prefs.useDynamicColors) {
+            Resources.reloadTheme(context, R.style.WidgetTheme)
+        } else {
+            Resources.reloadTheme(context, R.style.AppTheme)
+        }
+
         val widget = Tools.getWidget(context, widgetId)!!
         return widget.widgetView.getPopulatedView(context)
     }
