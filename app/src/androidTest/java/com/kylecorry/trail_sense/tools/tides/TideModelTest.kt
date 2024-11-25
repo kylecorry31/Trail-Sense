95e1b8605a1b6e10e66ca15b8100399d9b93ca10
Kyle Corry <kylecorry31@gmail.com>
2024-11-25 16:40:12 -0500

    Add more tide model tests
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/androidTest/java/com/kylecorry/trail_sense/tools/tides/TideModelTest.kt
+++ app/src/androidTest/java/com/kylecorry/trail_sense/tools/tides/TideModelTest.kt
@@ -16,6 +16,7 @@ import org.junit.Assert.assertTrue
 import org.junit.Test
 import java.time.Duration
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
@@ -36,7 +37,7 @@ class TideModelTest {
                 time(14, 42) to true,
                 time(21, 2) to false
             ),
-            losAngeles to tides(
+            california to tides(
                 LocalDate.of(2024, 11, 25),
                 pst,
                 time(5, 47) to true,
@@ -60,6 +61,59 @@ class TideModelTest {
                 time(12, 34) to false,
                 time(19, 3) to true
             ),
+            panama to tides(
+                LocalDate.of(2024, 11, 28),
+                est,
+                time(1, 36) to true,
+                time(8, 1) to false,
+                time(14, 9) to true,
+                time(20, 4) to false
+            ),
+            hawaii to tides(
+                LocalDate.of(2024, 11, 28),
+                hawaiiTimeZone,
+                time(1, 52) to true,
+                time(8, 7) to false,
+                time(12, 48) to true,
+                time(19, 10) to false
+            ),
+            fiji to tides(
+                LocalDate.of(2024, 11, 25),
+                fijiTimeZone,
+                time(2, 50) to true,
+                time(8, 59) to false,
+                time(15, 22) to true,
+                time(21, 40) to false
+            ),
+            // Mixed tides
+            penzhinaBay to tides(
+                LocalDate.of(2024, 11, 26),
+                penzhinaBayTimeZone,
+                time(3, 35) to true,
+                time(11, 48) to false
+            ),
+            antarctica to tides(
+                LocalDate.of(2024, 12, 1),
+                antarcticaTimeZone,
+                time(2, 2) to true,
+                time(10, 44) to false
+            ),
+            indonesia to tides(
+                LocalDate.of(2024, 11, 26),
+                wit,
+                time(4, 47) to false,
+                time(10, 50) to true,
+                time(16, 18) to false,
+                time(22, 39) to true
+            ),
+            mexico to tides(
+                LocalDate.of(2024, 11, 25),
+                cst,
+                time(5, 18) to false,
+                time(11, 51) to true,
+                time(16, 47) to false,
+                time(22, 13) to true
+            ),
             // Diurnal tides
             perth to tides(
                 LocalDate.of(2024, 12, 3),
@@ -67,16 +121,49 @@ class TideModelTest {
                 time(6, 42) to false,
                 time(21, 51) to true
             ),
+            // TODO: Tides in the mexico gulf are not working
 //            louisiana to tides(
 //                LocalDate.of(2024, 11, 28),
 //                cst,
-//                time(6, 15) to false,
-//                time(20, 33) to true,
-//            )
+//                time(4, 30) to true,
+//                time(16, 25) to false,
+//            ),
             // New moon
+            rhodeIsland to tides(
+                LocalDate.of(2024, 12, 1),
+                est,
+                time(0, 24) to false,
+                time(7, 22) to true,
+                time(13, 27) to false,
+                time(19, 44) to true
+            ),
             // Full moon
+            rhodeIsland to tides(
+                LocalDate.of(2024, 12, 15),
+                est,
+                time(0, 5) to false,
+                time(7, 19) to true,
+                time(13, 39) to false,
+                time(19, 43) to true
+            ),
             // First quarter
+            rhodeIsland to tides(
+                LocalDate.of(2024, 12, 8),
+                est,
+                time(0, 40) to true,
+                time(6, 0) to false,
+                time(13, 1) to true,
+                time(18, 44) to false
+            ),
             // Third quarter
+            rhodeIsland to tides(
+                LocalDate.of(2024, 12, 22),
+                est,
+                time(0, 46) to true,
+                time(5, 58) to false,
+                time(13, 0) to true,
+                time(18, 33) to false
+            )
         )
 
         val deltas = tests.flatMap {
@@ -89,15 +176,23 @@ class TideModelTest {
         println("Min: ${deltas.minOrNull()?.roundToInt()} m")
         println("Max: ${deltas.maxOrNull()?.roundToInt()} m")
 
-        assertTrue(Statistics.quantile(deltas, 0.9f) < 60)
+        // The majority of tide predictions should be within 45 minutes of the actual tide
+        assertTrue(Statistics.quantile(deltas, 0.9f) < 45)
     }
 
     private val rhodeIsland = Coordinate(41.49008, -71.312796)
-    private val louisiana = Coordinate(30.126666667, -89.221666667)
-    private val losAngeles = Coordinate(33.7833, -118.4167)
+    private val louisiana = Coordinate(30.03, -90.11)
+    private val california = Coordinate(33.7833, -118.4167)
     private val dublin = Coordinate(53.35, -6.22)
     private val hongKong = Coordinate(22.28, 114.18)
     private val perth = Coordinate(-31.98, 115.87)
+    private val panama = Coordinate(8.27, -82.85)
+    private val antarctica = Coordinate(-68.0, 78.5)
+    private val hawaii = Coordinate(19.73, -155.06)
+    private val fiji = Coordinate(-18.13, 178.43)
+    private val penzhinaBay = Coordinate(62.38, 164.50)
+    private val indonesia = Coordinate(-3.68, 128.18)
+    private val mexico = Coordinate(22.22, -97.86)
 
     private val utc = "UTC"
     private val pst = "America/Los_Angeles"
@@ -105,6 +200,11 @@ class TideModelTest {
     private val est = "America/New_York"
     private val hkt = "Asia/Hong_Kong"
     private val awst = "Australia/Perth"
+    private val antarcticaTimeZone = "UTC+7"
+    private val hawaiiTimeZone = "Pacific/Honolulu"
+    private val fijiTimeZone = "Pacific/Fiji"
+    private val penzhinaBayTimeZone = "Asia/Kamchatka"
+    private val wit = "Asia/Jayapura"
 
     private fun time(hour: Int, minute: Int): LocalTime {
         return LocalTime.of(hour, minute)
