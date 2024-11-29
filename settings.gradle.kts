dd688e2d57cbdb998a424b9a97eed0348ba77fa5
Kyle Corry <kylecorry31@gmail.com>
2024-11-29 07:51:45 -0500

    Remove custom gradle properties
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,10 +28,3 @@ dependencyResolutionManagement {
 
 include(":app")
 rootProject.name = "Trail Sense"
-
-buildCache {
-    local {
-        directory = File(rootDir, "build-cache")
-        removeUnusedEntriesAfterDays = 30
-    }
-}
