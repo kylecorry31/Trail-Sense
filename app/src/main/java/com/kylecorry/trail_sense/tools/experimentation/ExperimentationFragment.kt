5d3b3daa195f96289f45309dca2f5d1ebdda1880
Kyle Corry <kylecorry31@gmail.com>
2024-12-06 19:27:05 -0500

    Add celestial location to experimentation tool
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/experimentation/ExperimentationFragment.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/experimentation/ExperimentationFragment.kt
@@ -1,43 +1,27 @@
 package com.kylecorry.trail_sense.tools.experimentation
 
-import android.hardware.SensorManager
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Range
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.kylecorry.andromeda.core.ui.setTextDistinct
+import androidx.camera.view.PreviewView
 import com.kylecorry.andromeda.fragments.BoundFragment
-import com.kylecorry.andromeda.fragments.interval
-import com.kylecorry.andromeda.fragments.observe
-import com.kylecorry.andromeda.signal.CellSignal
-import com.kylecorry.andromeda.signal.CellSignalSensor
+import com.kylecorry.andromeda.fragments.inBackground
+import com.kylecorry.andromeda.pickers.CoroutinePickers
 import com.kylecorry.luna.coroutines.onDefault
-import com.kylecorry.sol.math.SolMath.roundPlaces
-import com.kylecorry.sol.math.SolMath.toDegrees
-import com.kylecorry.sol.science.geology.Geology
+import com.kylecorry.sol.science.astronomy.Astronomy
+import com.kylecorry.sol.science.astronomy.stars.Star
+import com.kylecorry.sol.science.astronomy.stars.StarAltitudeReading
 import com.kylecorry.sol.units.Coordinate
-import com.kylecorry.sol.units.Distance
 import com.kylecorry.trail_sense.databinding.FragmentExperimentationBinding
-import com.kylecorry.trail_sense.shared.FormatService
-import com.kylecorry.trail_sense.shared.UserPreferences
-import com.kylecorry.trail_sense.shared.sensors.SensorService
-import com.kylecorry.trail_sense.shared.toRelativeDistance
-import kotlin.math.abs
-import kotlin.math.hypot
-import kotlin.math.log10
-import kotlin.math.pow
+import com.kylecorry.trail_sense.shared.formatEnumName
+import java.time.ZonedDateTime
 
 class ExperimentationFragment : BoundFragment<FragmentExperimentationBinding>() {
 
-    private var inclination by state(0f)
-    private var strength by state(0f)
-    private var location by state(Coordinate.zero)
-    private val I = FloatArray(9)
-    private val lock = Any()
-    private val formatter by lazy { FormatService.getInstance(requireContext()) }
-    private val prefs by lazy { UserPreferences(requireContext()) }
-    private var signals by state(emptyList<CellSignal>())
+    private var stars by state(listOf<StarAltitudeReading>())
+    private var location by state<Coordinate?>(null)
 
     override fun generateBinding(
         layoutInflater: LayoutInflater, container: ViewGroup?
@@ -47,208 +31,55 @@ class ExperimentationFragment : BoundFragment<FragmentExperimentationBinding>() 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.camera.setScaleType(PreviewView.ScaleType.FILL_CENTER)
+        binding.camera.setShowTorch(false)
+        binding.arView.bind(binding.camera)
+        binding.arView.backgroundFillColor = Color.TRANSPARENT
 
-        val cellSignalSensor = CellSignalSensor(
-            requireContext(),
-            updateCellCache = true,
-            removeUnregisteredSignals = false
-        )
-        observe(cellSignalSensor) {
-            signals = cellSignalSensor.signals
+        binding.recordBtn.setOnClickListener {
+            val inclination = binding.arView.inclination
+            inBackground {
+                val allStars = Star.entries.sortedBy { it.name }
+                val starIdx = CoroutinePickers.item(
+                    requireContext(),
+                    "Star",
+                    allStars.map { formatEnumName(it.name) })
+                if (starIdx != null) {
+                    stars = stars + StarAltitudeReading(
+                        allStars[starIdx],
+                        inclination,
+                        ZonedDateTime.now()
+                    )
+                    // TODO: Pass in the last known location (let user choose whether to do this or not)
+                    // TODO: Let the user pick their last known location on a map
+                    location = onDefault { Astronomy.getLocationFromStars(stars) }
+                }
+            }
         }
-
     }
 
 
     override fun onUpdate() {
         super.onUpdate()
-
-        val distanceText = signals.joinToString("\n\n") { signal ->
-            val (lower, upper) = estimateDistanceBoundsRSRP(signal.dbm.toFloat(), 1800f, 43f)
-            val lowerFormatted = formatter.formatDistance(
-                Distance.meters(lower).convertTo(prefs.baseDistanceUnits).toRelativeDistance(),
-                2
-            )
-            val upperFormatted = formatter.formatDistance(
-                Distance.meters(upper).convertTo(prefs.baseDistanceUnits).toRelativeDistance(),
-                2
-            )
-            "${signal.id} ${signal.network.name}\nLower: $lowerFormatted, Upper: $upperFormatted\n${if (signal.isRegistered) "Registered" else "Unregistered"}\n${
-                formatter.formatPercentage(
-                    signal.strength
-                )
-            }"
+        effect2(stars) {
+            binding.text.text = stars.joinToString("\n") { "${it.star.name}: ${it.altitude}" }
         }
 
-        binding.text.text = distanceText
-
-    }
-
-    fun estimateDistanceBoundsRSRP(
-        rsrp: Float,
-        frequency: Float,
-        transmitPower: Float
-    ): Pair<Float, Float> {
-        val pld0 = 20 * log10(frequency) + 20
-        val pld = transmitPower - rsrp
-        val ns = listOf(2f, 4f)
-        val distances = ns.map { n -> 10f.pow((pld - pld0) / (10 * n)) }
-
-        // Return the lower and upper bounds
-        val lowerBound = distances.minOrNull() ?: 0f
-        val upperBound = distances.maxOrNull() ?: 0f
-
-        return Pair(lowerBound, upperBound)
-    }
-
-    private fun estimateDistanceBounds(
-        rssi: Float,
-        frequency: Float,
-        transmitPower: Float
-    ): Pair<Float, Float> {
-        // Speed of light in m/s
-        val c = 3f * 10f.pow(8)
-
-        // Frequency in Hz
-        val fHz = frequency * 10f.pow(6)
-
-        // Reference path loss at 1 meter (free space)
-        val PL0 = 20 * log10((4 * Math.PI.toFloat() * fHz) / c)
-
-        // Calculate path loss
-        val PL = transmitPower - rssi
-
-        // Path loss exponents for different environments
-        val nValues = listOf(2f, 3f, 4f)
-
-        // Calculate distances for each path loss exponent
-        val distances = nValues.map { n -> 10f.pow((PL - PL0) / (10 * n)) }
-
-        // Return the lower and upper bounds
-        val lowerBound = distances.minOrNull() ?: 0f
-        val upperBound = distances.maxOrNull() ?: 0f
-
-        return Pair(lowerBound, upperBound)
-    }
-
-    private fun setupGeomagneticLocation() {
-        val sensors = SensorService(requireContext())
-        val accelerometer = sensors.getGravity()
-        val magnetometer = sensors.getMagnetometer(true)
-
-        observe(accelerometer) {
-            updateInclination(accelerometer.rawAcceleration, magnetometer.rawMagneticField)
-        }
-
-        observe(magnetometer) {
-            updateInclination(accelerometer.rawAcceleration, magnetometer.rawMagneticField)
-            strength = magnetometer.magneticField.magnitude()
-        }
-
-        interval(1000) {
-            geomagneticLocationGridSearch()
+        effect2(location) {
+            binding.location.text = location?.toString() ?: "Unknown"
         }
     }
 
-    private fun updateInclination(gravity: FloatArray, geomagnetic: FloatArray) {
-        synchronized(lock) {
-            SensorManager.getRotationMatrix(null, I, gravity, geomagnetic)
-            inclination = -SensorManager.getInclination(I).toDegrees()
-        }
-
+    override fun onResume() {
+        super.onResume()
+        binding.arView.start()
+        binding.camera.start()
     }
 
-
-    private fun updateGeomagneticLocation() {
-        val inclinationText = memo("inclination", inclination.roundPlaces(2)) {
-            formatter.formatDegrees(inclination, 2)
-        }
-
-        val strengthText = memo("strength", strength.roundPlaces(2)) {
-            formatter.formatMagneticField(strength, 2)
-        }
-
-        val locationText = memo("location", location) {
-            formatter.formatLocation(location)
-        }
-
-        binding.text.setTextDistinct("$inclinationText\n$strengthText\n${locationText}")
-    }
-
-    private suspend fun geomagneticLocationGridSearch() = onDefault {
-        // TODO: It can limit the search using inclination
-        location = search(
-            Range(-90.0, 90.0),
-            Range(-180.0, 180.0),
-            0.5,
-            strength,
-            inclination
-        )
-
-//        val optimizer = SimulatedAnnealingOptimizer(
-//            100.0,
-//            0.1,
-//            8000
-//        )
-//
-//        val strength = strength
-//        val inclination = inclination
-//
-//        val best = optimizer.optimize(
-//            com.kylecorry.sol.math.Range(-90.0, 90.0),
-//            com.kylecorry.sol.math.Range(-180.0, 180.0),
-//            false
-//        ) { latitude, longitude ->
-//            val posField = Geology.getGeomagneticField(Coordinate(latitude, longitude))
-//            val posInclination = Geology.getGeomagneticInclination(Coordinate(latitude, longitude))
-//            hypot(
-//                abs(posField.magnitude() - strength),
-//                abs(posInclination - inclination)
-//            ).toDouble()
-//        }
-//
-//        location = Coordinate(best.first, best.second)
-
-        // TODO: Progressively increase search resolution
-    }
-
-    // TODO: Return top X best matches
-    private suspend fun search(
-        latRange: Range<Double>,
-        lonRange: Range<Double>,
-        step: Double,
-        strength: Float,
-        inclination: Float
-    ): Coordinate = onDefault {
-        var nearestLocation = Coordinate.zero
-        var nearestDelta = Float.MAX_VALUE
-        val latStart = (latRange.lower * 10).toInt()
-        val latEnd = (latRange.upper * 10).toInt()
-        val lonStart = (lonRange.lower * 10).toInt()
-        val lonEnd = (lonRange.upper * 10).toInt()
-        for (latSearch in latStart..latEnd step (step * 10).toInt()) {
-            for (lonSearch in lonStart..lonEnd step (step * 10).toInt()) {
-                val lat = latSearch / 10.0
-                val lon = lonSearch / 10.0
-                val posField =
-                    Geology.getGeomagneticField(Coordinate(lat, lon))
-                // TODO: Calculate this from the field itself
-                val posInclination =
-                    Geology.getGeomagneticInclination(Coordinate(lat, lon))
-
-                val delta = hypot(
-                    abs(posField.magnitude() - strength),
-                    abs(posInclination - inclination)
-                )
-
-                if (delta < nearestDelta) {
-                    nearestDelta = delta
-                    nearestLocation = Coordinate(lat, lon)
-                }
-            }
-        }
-
-        nearestLocation
+    override fun onPause() {
+        super.onPause()
+        binding.arView.stop()
+        binding.camera.stop()
     }
 
 }
