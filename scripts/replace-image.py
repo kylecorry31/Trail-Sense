f94321e0cd590886b27444801b4a2d4f7668babc
Kyle Corry <kylecorry31@gmail.com>
2025-02-23 09:33:26 -0500

    Update grub image
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

---
+++ scripts/replace-image.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import shutil
+
+# Get the script directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Find all files in the parent directory that match the desired file name (arg 2)
+parent_dir = os.path.dirname(current_dir)
+matches = []
+for root, dirs, files in os.walk(parent_dir):
+    for file in files:
+        if file == sys.argv[2]:
+            matches.append(os.path.join(root, file))
+
+# Replace those files with the file the user provided (arg 1)
+source_file = sys.argv[1]
+for match in matches:
+    shutil.copy2(source_file, match)
+    print('Replaced', match)
