f3a0c700eeb77a34b933f9f5e496e8e886fd0f1b
Kyle Corry <kylecorry31@gmail.com>
2025-04-18 08:22:57 -0400

    Make ballistics calculator experimental
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/shared/UserPreferences.kt
+++ app/src/main/java/com/kylecorry/trail_sense/shared/UserPreferences.kt
@@ -40,6 +40,7 @@ import com.kylecorry.trail_sense.shared.extensions.putLongArray
 import com.kylecorry.trail_sense.shared.preferences.PreferencesSubsystem
 import com.kylecorry.trail_sense.shared.sharing.MapSite
 import com.kylecorry.trail_sense.tools.astronomy.infrastructure.AstronomyPreferences
+import com.kylecorry.trail_sense.tools.ballistics.infrastructure.BallisticsPreferences
 import com.kylecorry.trail_sense.tools.maps.infrastructure.MapPreferences
 import com.kylecorry.trail_sense.tools.navigation.infrastructure.NavigationPreferences
 import com.kylecorry.trail_sense.tools.tools.infrastructure.Tools
@@ -73,6 +74,7 @@ class UserPreferences(ctx: Context) : IDeclinationPreferences {
     val altimeter by lazy { AltimeterPreferences(context) }
     val augmentedReality by lazy { AugmentedRealityPreferences(context) }
     val backup by lazy { BackupPreferences(context) }
+    val ballistics by lazy { BallisticsPreferences(context) }
 
     private val isMetricPreferred = Resources.isMetricPreferred(context)
 
--- app/src/main/java/com/kylecorry/trail_sense/tools/ballistics/ui/FragmentToolBallistics.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/ballistics/ui/FragmentToolBallistics.kt
@@ -3,7 +3,9 @@ package com.kylecorry.trail_sense.tools.ballistics.ui
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import com.kylecorry.andromeda.core.ui.useService
 import com.kylecorry.trail_sense.R
+import com.kylecorry.trail_sense.shared.UserPreferences
 import com.kylecorry.trail_sense.shared.extensions.TrailSenseReactiveFragment
 import com.kylecorry.trail_sense.shared.views.CustomViewPagerAdapter
 
@@ -14,18 +16,24 @@ class FragmentToolBallistics : TrailSenseReactiveFragment(R.layout.fragment_tabs
         val tabsView = useView<TabLayout>(R.id.tabs)
         val viewPagerView = useView<ViewPager2>(R.id.viewpager)
 
-        val pages = useMemo {
-            listOf(
+        val prefs = useService<UserPreferences>()
+
+        val isBallisticsCalculatorEnabled = useMemo(prefs) {
+            prefs.ballistics.isBallisticsCalculatorEnabled
+        }
+
+        val pages = useMemo(isBallisticsCalculatorEnabled) {
+            listOfNotNull(
                 FragmentScopeAdjustment(),
-                FragmentBallisticsCalculator(),
+                if (isBallisticsCalculatorEnabled) FragmentBallisticsCalculator() else null,
                 FragmentEnergyCalculator()
             )
         }
 
-        val pageNames = useMemo {
-            listOf(
+        val pageNames = useMemo(isBallisticsCalculatorEnabled) {
+            listOfNotNull(
                 getString(R.string.scope),
-                getString(R.string.ballistics),
+                if (isBallisticsCalculatorEnabled) getString(R.string.ballistics) else null,
                 getString(R.string.energy)
             )
         }
