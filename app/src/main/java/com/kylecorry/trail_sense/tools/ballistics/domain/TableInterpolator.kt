da10b8a49d733761185a471d68ee6fd545bd07a9
Kyle Corry <kylecorry31@gmail.com>
2025-04-21 13:56:33 -0400

    Switch ballistics to linear interpolator
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/ballistics/domain/TableInterpolator.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/ballistics/domain/TableInterpolator.kt
@@ -1,16 +1,16 @@
 package com.kylecorry.trail_sense.tools.ballistics.domain
 
-import com.kylecorry.sol.math.interpolation.NewtonInterpolator
+import com.kylecorry.sol.math.interpolation.LinearInterpolator
 
 class TableInterpolator(table: Map<Float, Float>) {
 
-    private val newtonInterpolator = NewtonInterpolator()
+    private val interpolator = LinearInterpolator()
     private val entries = table.entries.sortedBy { it.key }
     private val keys = entries.map { it.key }
     private val values = entries.map { it.value }
 
     fun interpolate(value: Float): Float {
-        return newtonInterpolator.interpolate(
+        return interpolator.interpolate(
             value,
             keys,
             values
