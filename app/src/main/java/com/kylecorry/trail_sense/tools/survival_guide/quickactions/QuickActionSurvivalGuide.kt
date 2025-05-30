e3011f04bb33c062e07f3092f24d75c5355a8d91
Tosin <tosinmath007@gmail.com>
2025-05-30 11:49:41 +0100

    Update QuickActionSurvivalGuide

--- app/src/main/java/com/kylecorry/trail_sense/tools/survival_guide/quickactions/QuickActionSurvivalGuide.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/survival_guide/quickactions/QuickActionSurvivalGuide.kt
@@ -3,9 +3,11 @@ package com.kylecorry.trail_sense.tools.survival_guide.quickactions
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.kylecorry.andromeda.fragments.AndromedaFragment
 import com.kylecorry.trail_sense.R
 import com.kylecorry.trail_sense.shared.QuickActionButton
 import com.kylecorry.trail_sense.shared.openTool
+import com.kylecorry.trail_sense.tools.survival_guide.infrastructure.SurvivalUtils
 import com.kylecorry.trail_sense.tools.tools.infrastructure.Tools
 
 class QuickActionSurvivalGuide(btn: ImageButton, fragment: Fragment) :
@@ -17,7 +19,7 @@ class QuickActionSurvivalGuide(btn: ImageButton, fragment: Fragment) :
 
     override fun onClick() {
         super.onClick()
-        // Show Survival Guide
+        SurvivalUtils.showSurvival(fragment)
     }
 
     override fun onLongClick(): Boolean {
