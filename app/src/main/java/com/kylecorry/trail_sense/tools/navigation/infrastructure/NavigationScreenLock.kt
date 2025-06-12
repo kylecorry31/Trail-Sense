dfd4c31f93774d112c7bc52246a52ce7bbb472a1
Kyle Corry <kylecorry31@gmail.com>
2025-06-12 18:20:40 -0400

    Disable screen lock when the fragment is changed
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/navigation/infrastructure/NavigationScreenLock.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/navigation/infrastructure/NavigationScreenLock.kt
@@ -1,10 +1,15 @@
 package com.kylecorry.trail_sense.tools.navigation.infrastructure
 
 import android.app.Activity
+import androidx.navigation.NavController
 import com.kylecorry.andromeda.core.cache.AppServiceRegistry
 import com.kylecorry.andromeda.core.system.Screen
 import com.kylecorry.andromeda.core.tryOrNothing
+import com.kylecorry.luna.timer.CoroutineTimer
+import com.kylecorry.trail_sense.main.MainActivity
 import com.kylecorry.trail_sense.shared.UserPreferences
+import com.kylecorry.trail_sense.shared.extensions.findNavController
+import java.lang.ref.WeakReference
 
 class NavigationScreenLock(private val alwaysLock: Boolean = false) {
 
@@ -12,18 +17,53 @@ class NavigationScreenLock(private val alwaysLock: Boolean = false) {
     private val shouldLock by lazy { prefs.navigation.keepScreenUnlockedWhileNavigating }
     private val navigator = AppServiceRegistry.get<Navigator>()
 
-    fun updateLock(activity: Activity) {
+    private var activityReference: WeakReference<Activity>? = null
+    private var selfDestinationId: Int? = null
+    private val listener = NavController.OnDestinationChangedListener { _, destination, _ ->
+        if (destination.id != selfDestinationId) {
+            releaseLock(activityReference?.get() ?: return@OnDestinationChangedListener)
+        }
+    }
+    private val removeListenerTimer = CoroutineTimer {
         tryOrNothing {
-            Screen.setShowWhenLocked(
-                activity,
-                alwaysLock || (shouldLock && navigator.isNavigating())
-            )
+            activityReference?.get()?.let {
+                removeNavigationListener(it)
+            }
+        }
+    }
+
+    fun updateLock(activity: Activity) {
+        activityReference = WeakReference(activity)
+        tryOrNothing {
+            val shouldShowWhenLocked = alwaysLock || (shouldLock && navigator.isNavigating())
+            if (shouldShowWhenLocked) {
+                Screen.setShowWhenLocked(activity, true)
+                addNavigationListener(activity)
+            } else {
+                releaseLock(activity)
+            }
         }
     }
 
     fun releaseLock(activity: Activity) {
         tryOrNothing {
             Screen.setShowWhenLocked(activity, false)
+            removeListenerTimer.once(1000)
         }
     }
+
+    private fun removeNavigationListener(activity: Activity) {
+        val mainActivity = activity as? MainActivity ?: return
+        val navController = mainActivity.findNavController()
+        navController.removeOnDestinationChangedListener(listener)
+    }
+
+    private fun addNavigationListener(activity: Activity) {
+        val weakActivity = WeakReference(activity)
+        val mainActivity = weakActivity.get() as? MainActivity ?: return
+        val navController = mainActivity.findNavController()
+        selfDestinationId = navController.currentDestination?.id
+        navController.addOnDestinationChangedListener(listener)
+    }
+
 }
