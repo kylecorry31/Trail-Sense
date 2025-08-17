6022b6dfb6c7c0dbf3f2a848af7a263df51fd3bf
Kyle Corry <kylecorry31@gmail.com>
2025-08-17 08:21:24 -0400

    Replace layer settings with layer button
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/map/ui/MapSettingsFragment.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/map/ui/MapSettingsFragment.kt
@@ -3,8 +3,10 @@ package com.kylecorry.trail_sense.tools.map.ui
 import android.os.Bundle
 import com.kylecorry.andromeda.core.cache.AppServiceRegistry
 import com.kylecorry.andromeda.fragments.AndromedaPreferenceFragment
+import com.kylecorry.andromeda.fragments.show
 import com.kylecorry.trail_sense.R
 import com.kylecorry.trail_sense.shared.UserPreferences
+import com.kylecorry.trail_sense.shared.map_layers.preferences.ui.MapLayersBottomSheet
 
 class MapSettingsFragment : AndromedaPreferenceFragment() {
 
@@ -12,7 +14,9 @@ class MapSettingsFragment : AndromedaPreferenceFragment() {
         setPreferencesFromResource(R.xml.map_preferences, rootKey)
 
         // Layers
-        val prefs = AppServiceRegistry.get<UserPreferences>()
-        prefs.map.layerManager.populatePreferences(preferenceScreen)
+        onClick(preference(R.string.pref_map_layer_button)) {
+            val prefs = AppServiceRegistry.get<UserPreferences>()
+            MapLayersBottomSheet(prefs.map.layerManager).show(this)
+        }
     }
 }
