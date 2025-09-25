8cdce270108036301913d251818eb05c65db853b
Kyle Corry <kylecorry31@gmail.com>
2025-09-25 16:48:01 -0400

    Add cell tower tests

--- app/src/androidTest/java/com/kylecorry/trail_sense/tools/signal_finder/CellTowerModelTest.kt
+++ app/src/androidTest/java/com/kylecorry/trail_sense/tools/signal_finder/CellTowerModelTest.kt
@@ -1,35 +1,40 @@
 package com.kylecorry.trail_sense.tools.signal_finder
 
-import androidx.test.platform.app.InstrumentationRegistry
 import com.kylecorry.andromeda.core.cache.AppServiceRegistry
+import com.kylecorry.sol.science.geology.CoordinateBounds
 import com.kylecorry.sol.science.geology.Geofence
 import com.kylecorry.sol.units.Coordinate
 import com.kylecorry.sol.units.Distance
 import com.kylecorry.trail_sense.shared.io.FileSubsystem
+import com.kylecorry.trail_sense.test_utils.TestStatistics.assertQuantile
+import com.kylecorry.trail_sense.test_utils.TestUtils
 import com.kylecorry.trail_sense.tools.signal_finder.infrastructure.CellTowerModel
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class CellTowerModelTest {
 
     @Test
     fun getTowers() = runBlocking {
-        // TODO: Re-add tests
-//        val context = InstrumentationRegistry.getInstrumentation().targetContext
-//        AppServiceRegistry.register(FileSubsystem.getInstance(context))
-//        val tests = listOf(
-//            Coordinate(42.03, -71.97) to true,
-//            Coordinate(42.0, -72.0) to false,
-//            Coordinate(45.09, -61.8) to true,
-//            Coordinate(27.03, 14.43) to true,
-//            Coordinate(-22.53, -55.74) to true,
-//        )
-//
-//        for (test in tests) {
-//            val (coordinate, expected) = test
-//            val towers = CellTowerModel.getTowers(Geofence(coordinate, Distance.miles(1f)))
-//            assertEquals(expected, towers.contains(coordinate))
-//        }
+        val knownCellTowers = listOf(
+            // RI
+            Coordinate(41.887778, -71.755889),
+            Coordinate(41.915583, -71.686472),
+            Coordinate(41.966611, -71.755417),
+            Coordinate(41.974194, -71.779944),
+            // TODO: Add tests for other places
+        )
+
+        AppServiceRegistry.register(FileSubsystem.getInstance(TestUtils.context))
+
+        val errors = knownCellTowers.map { tower ->
+            val towers =
+                CellTowerModel.getTowers(CoordinateBounds.from(Geofence(tower, Distance.miles(5f))))
+            val closest = towers.minBy { it.coordinate.distanceTo(tower) }
+            closest.coordinate.distanceTo(tower)
+        }
+
+        assertQuantile(errors, 550f, 0.5f)
+        assertQuantile(errors, 1000f, 0.9f)
     }
 }
