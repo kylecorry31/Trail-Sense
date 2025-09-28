5f5da6f858eb1f693f9121dc5f26759caf3b63b3
Kyle Corry <kylecorry31@gmail.com>
2025-09-28 08:17:07 -0400

    Improve widget configure page
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/tools/widgets/ToolWidgetConfigureActivity.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/tools/widgets/ToolWidgetConfigureActivity.kt
@@ -3,14 +3,17 @@ package com.kylecorry.trail_sense.tools.tools.widgets
 import android.appwidget.AppWidgetManager
 import android.content.Intent
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
-import com.kylecorry.andromeda.core.cache.AppServiceRegistry
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import com.kylecorry.andromeda.fragments.AndromedaActivity
 import com.kylecorry.andromeda.fragments.ColorTheme
 import com.kylecorry.andromeda.fragments.inBackground
+import com.kylecorry.andromeda.views.toolbar.Toolbar
 import com.kylecorry.trail_sense.R
-import com.kylecorry.trail_sense.shared.UserPreferences
 import com.kylecorry.trail_sense.shared.views.MaterialSpinnerView
 import com.kylecorry.trail_sense.tools.tools.infrastructure.Tools
 
@@ -19,14 +22,25 @@ class ToolWidgetConfigureActivity : AndromedaActivity() {
     private var appWidgetId: Int = AppWidgetManager.INVALID_APPWIDGET_ID
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val userPrefs = AppServiceRegistry.get<UserPreferences>()
-        setColorTheme(ColorTheme.System, userPrefs.useDynamicColors)
-        // TODO: Apply insets
+        setColorTheme(ColorTheme.System, true)
         enableEdgeToEdge()
 
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_tool_widget_configure)
+
+        // Apply insets
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root)) { v, windowInsets ->
+            val insets =
+                windowInsets.getInsets(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout())
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                topMargin = insets.top
+                bottomMargin = insets.bottom
+                leftMargin = insets.left
+                rightMargin = insets.right
+            }
+            windowInsets
+        }
 
         appWidgetId = intent?.extras?.getInt(
             AppWidgetManager.EXTRA_APPWIDGET_ID,
@@ -47,8 +61,12 @@ class ToolWidgetConfigureActivity : AndromedaActivity() {
         }
 
         val prefs = WidgetPreferences(this, widget, appWidgetId)
+        val title = findViewById<Toolbar>(R.id.configure_widget_title)
         val spinner = findViewById<MaterialSpinnerView>(R.id.theme)
         val save = findViewById<Button>(R.id.button_save)
+
+        title.title.text = widget.name
+        spinner.setHint(getString(R.string.pref_theme_title))
 
         val items = listOf(
             WidgetTheme.System to getString(R.string.theme_system),
