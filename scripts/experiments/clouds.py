4a3fa793aedcf2d9071828d46cca16196caca56a
Kyle Corry <kylecorry31@gmail.com>
2025-10-31 07:19:55 -0400

    Add more cloud data points to experiment

--- scripts/experiments/clouds.py
+++ scripts/experiments/clouds.py
@@ -7,53 +7,93 @@ from scipy import ndimage
 
 def load(image_path):
     img = Image.open(image_path)
-    img = img.convert('RGB')
+    img = img.convert("RGB")
     img_rgb = np.array(img) / 255.0
     return img_rgb
 
+
 def save_image(img_array, file_name, mode="RGB"):
-    img_pil = Image.fromarray(img_array, mode)
+    img_pil = Image.fromarray((img_array * 255).astype(np.uint8), mode)
     output_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), file_name)
     img_pil.save(output_path, quality=90)
 
-def classify_pixels(img_rgb):    
-    luminance = img_rgb[..., 2]
+
+def get_cloud_coverage(image, sky):
+    blue_ratio = image[..., 2] / (image[..., 0] + image[..., 1] + 1 / 255)
+    save_image(blue_ratio, "blue_ratio.jpg", mode="L")
+    cloud_coverage_img = 1 - np.clip(np.abs(0.5 - blue_ratio) * 2, 0, 1)
+    cloud_coverage_img[~sky] = 0
+    cloud_coverage_img[cloud_coverage_img > 0.8] = 1
+    save_image(cloud_coverage_img, "cloud_coverage.jpg", mode="L")
+
+    cloud_coverage = np.sum(cloud_coverage_img * sky) / (np.sum(sky) + 1e-6)
+    clear_sky = np.sum((cloud_coverage_img * sky) < 0.2) / (np.sum(sky) + 1e-6)
+    return cloud_coverage, clear_sky
+
+
+def get_cloud_roughness(image, sky):
+    kernel = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+    blue_channel = image[..., 2]
+    sobel_x = ndimage.convolve(blue_channel, kernel)
+    sobel_y = ndimage.convolve(blue_channel, kernel.T)
+    gradient_magnitude = np.sqrt(sobel_x**2 + sobel_y**2)
+    save_image(gradient_magnitude, "gradient_magnitude.jpg", mode="L")
+    gradient_magnitude[~sky] = 0
+    roughness = np.sum(gradient_magnitude) / np.sum(sky)
+    return roughness
+
+
+def get_sky_mask(image):
+    luminance = image[..., 2]
     stdev = np.std(luminance)
 
-    blue_ratio = img_rgb[..., 2] / (img_rgb[..., 0] + img_rgb[..., 1] + 1/255)
-    save_image((blue_ratio * 255).astype(np.uint8), "blue_ratio.jpg", mode='L')
+    blue_ratio = image[..., 2] / (image[..., 0] + image[..., 1] + 1 / 255)
+    save_image(blue_ratio, "blue_ratio.jpg", mode="L")
 
-    save_image((luminance * 255).astype(np.uint8), "luminance.jpg", mode='L')
+    save_image(luminance, "luminance.jpg", mode="L")
 
     # TODO: This should be more robust to different lighting conditions
-    print(stdev)
+    # print(stdev)
     if stdev < 0.15:
-        objects = np.zeros_like(img_rgb[..., 0], dtype=bool)
+        objects = np.zeros_like(image[..., 0], dtype=bool)
     else:
-        objects = (luminance < 0.15)
-    
-    # Dilate the objects
-    width = img_rgb.shape[1]
-    objects = ndimage.binary_dilation(objects, iterations=int(width * 0.01))
-    
-    sky = ~objects
-    clouds = sky
+        objects = luminance < 0.15
 
-    classification_img = np.zeros_like(img_rgb)
-    classification_img[..., 0] = clouds * blue_ratio
+    # Dilate the objects
+    width = image.shape[1]
+    objects = ndimage.binary_dilation(objects, iterations=int(width * 0.01))
+
+    sky = ~objects
+
+    classification_img = np.zeros_like(image)
+    classification_img[..., 0] = sky * blue_ratio
     classification_img[..., 1] = objects
     classification_img[..., 2] = sky
-    save_image((classification_img * 255).astype(np.uint8), "combined_classification.jpg", mode='RGB')
+    save_image(
+        classification_img,
+        "combined_classification.jpg",
+        mode="RGB",
+    )
+    return sky
 
-    cloud_coverage_img = 1 - np.clip(np.abs(0.5 - blue_ratio) * 2, 0, 1)
-    cloud_coverage_img[~clouds] = 0
-    cloud_coverage_img[cloud_coverage_img > 0.8] = 1
-    save_image((cloud_coverage_img * 255).astype(np.uint8), "cloud_coverage.jpg", mode='L')
 
-    cloud_coverage = np.sum(cloud_coverage_img * clouds) / (np.sum(clouds) + 1e-6)
-    print(f"  Estimated Cloud Coverage: {(cloud_coverage * 100):.2f}")
-
-   
 print(sys.argv[1])
-img_rgb = load(sys.argv[1])
-classify_pixels(img_rgb)
+image = load(sys.argv[1])
+aspect = image.shape[1] / image.shape[0]
+new_width = 400
+new_height = 400
+
+pil_image = Image.fromarray((image * 255).astype(np.uint8))
+if aspect > 1:
+    new_height = int(new_height / aspect)
+    pil_image = pil_image.resize((new_width, new_height), Image.BICUBIC)
+else:
+    new_width = int(new_width * aspect)
+    pil_image = pil_image.resize((new_width, new_height), Image.BICUBIC)
+image = np.array(pil_image) / 255.0
+sky = get_sky_mask(image)
+cloud_coverage, clear_sky = get_cloud_coverage(image, sky)
+roughness = get_cloud_roughness(image, sky)
+print(f"Coverage: {(cloud_coverage * 100):.2f}%")
+print(f"Clear: {(clear_sky * 100):.2f}%")
+print(f"Roughness: {(roughness * 100):.2f}%")
