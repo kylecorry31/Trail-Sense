c8b1474eaf582e889686a119de668925e3c0b852
Kyle Corry <kylecorry31@gmail.com>
2026-01-24 10:46:37 -0500

    test: catches tool errors
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/androidTest/java/com/kylecorry/trail_sense/tools/ToolsTest.kt
+++ app/src/androidTest/java/com/kylecorry/trail_sense/tools/ToolsTest.kt
@@ -1,11 +1,18 @@
 package com.kylecorry.trail_sense.tools
 
+import com.kylecorry.andromeda.core.cache.AppServiceRegistry
 import com.kylecorry.trail_sense.R
 import com.kylecorry.trail_sense.shared.extensions.findNavController
+import com.kylecorry.trail_sense.shared.openTool
 import com.kylecorry.trail_sense.shared.preferences.PreferencesSubsystem
+import com.kylecorry.trail_sense.test_utils.AutomationLibrary.click
+import com.kylecorry.trail_sense.test_utils.AutomationLibrary.clickOk
+import com.kylecorry.trail_sense.test_utils.AutomationLibrary.hasText
 import com.kylecorry.trail_sense.test_utils.AutomationLibrary.isTrue
 import com.kylecorry.trail_sense.test_utils.TestUtils
+import com.kylecorry.trail_sense.test_utils.TestUtils.back
 import com.kylecorry.trail_sense.test_utils.ToolTestBase
+import com.kylecorry.trail_sense.tools.field_guide.infrastructure.FieldGuideRepo
 import com.kylecorry.trail_sense.tools.tools.infrastructure.Tools
 import org.junit.Test
 
@@ -46,8 +53,44 @@ class ToolsTest : ToolTestBase(0L) {
 
         // Verify navigation tool is open
         isTrue {
-            Tools.getTools(TestUtils.context).first { it.id == Tools.NAVIGATION }
+            Tools.getTool(TestUtils.context, Tools.NAVIGATION)!!
                 .isOpen(navController.currentDestination?.id ?: 0)
         }
+    }
+
+    @Test
+    fun catchesToolErrors() {
+        // Removing the field guide repo will break the DI for the field guide tool
+        AppServiceRegistry.services.remove(FieldGuideRepo::class.java.name)
+
+        navController.openTool(Tools.FIELD_GUIDE)
+
+        hasText("An error occurred")
+        hasText("Field Guide")
+
+        // Settings
+        click("SETTINGS")
+        isTrue {
+            Tools.getTool(TestUtils.context, Tools.SETTINGS)!!
+                .isOpen(navController.currentDestination?.id ?: 0)
+        }
+
+        back()
+        hasText("An error occurred")
+
+        // View error details
+        click("VIEW ERROR DETAILS")
+        hasText("Build type")
+        clickOk()
+
+        // Other buttons
+        hasText("EMAIL DEVELOPER")
+        hasText("COPY ERROR")
+        hasText("RESTART APP")
+
+        // Fix the issue and reopen tool
+        AppServiceRegistry.register(FieldGuideRepo.getInstance(TestUtils.context))
+        click("REOPEN TOOL")
+        hasText("Disclaimer")
     }
 }
