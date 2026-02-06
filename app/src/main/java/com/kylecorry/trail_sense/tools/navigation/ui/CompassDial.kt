7ce6217861fa0a2c9ad28af3fdc751c1ce842016
Kyle Corry <kylecorry31@gmail.com>
2026-02-06 07:18:43 -0500

    feat: improve compass dial visibility
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/navigation/ui/CompassDial.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/navigation/ui/CompassDial.kt
@@ -1,7 +1,9 @@
 package com.kylecorry.trail_sense.tools.navigation.ui
 
+import android.graphics.Color
 import androidx.annotation.ColorInt
 import com.kylecorry.andromeda.canvas.ICanvasDrawer
+import com.kylecorry.andromeda.canvas.StrokeCap
 import com.kylecorry.andromeda.core.units.PixelCoordinate
 import com.kylecorry.trail_sense.shared.canvas.Dial
 
@@ -13,7 +15,7 @@ class CompassDial(
     @ColorInt private val cardinalTickColor: Int = tickColor
 ) {
 
-    private val tickThicknessDp = 1f
+    private val tickThicknessDp = 1.5f
     private val tickLengthPercent = 0.03f
     private val tickRadiusPercent = 0.9f
     private val ticks = Dial.ticks(
@@ -29,7 +31,16 @@ class CompassDial(
         45
     )
 
+    private val trueCardinalTicks = Dial.ticks(
+        center,
+        tickRadiusPercent * radius,
+        tickLengthPercent * radius,
+        90
+    )
+
     fun draw(drawer: ICanvasDrawer, drawTicks: Boolean = true, drawBackground: Boolean = true) {
+        val trueCardinalTickSize = tickThicknessDp * 2f
+
         drawer.opacity(255)
         drawer.noStroke()
         drawer.fill(backgroundColor)
@@ -39,13 +50,29 @@ class CompassDial(
 
         if (!drawTicks) return
 
-        drawer.strokeWeight(drawer.dp(tickThicknessDp))
+        // Outlines
+        drawer.strokeCap(StrokeCap.Square)
+        drawer.strokeWeight(drawer.dp(tickThicknessDp + 1))
         drawer.noFill()
+        drawer.stroke(Color.BLACK)
+        drawer.path(ticks)
+        drawer.path(cardinalTicks)
+        drawer.strokeWeight(drawer.dp(trueCardinalTickSize + 1))
+        drawer.path(trueCardinalTicks)
+
+
+        // Normal ticks
+        drawer.strokeWeight(drawer.dp(tickThicknessDp))
         drawer.stroke(tickColor)
         drawer.path(ticks)
 
+        // Cardinal ticks
         drawer.stroke(cardinalTickColor)
         drawer.path(cardinalTicks)
+        drawer.strokeWeight(drawer.dp(trueCardinalTickSize))
+        drawer.path(trueCardinalTicks)
+
+        drawer.strokeCap(StrokeCap.Round)
     }
 
 }
