10b97ded55d23e4252bf0145123e8e8bf90a9b00
Kyle Corry <kylecorry31@gmail.com>
2026-02-20 14:23:47 -0500

    fix: improve horse/deer fly inactive calculation
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/climate/domain/PhenologyService.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/climate/domain/PhenologyService.kt
@@ -2,29 +2,24 @@ package com.kylecorry.trail_sense.tools.climate.domain
 
 import com.kylecorry.luna.coroutines.onDefault
 import com.kylecorry.sol.math.Range
-import com.kylecorry.sol.science.astronomy.Astronomy
-import com.kylecorry.sol.science.astronomy.SunTimesMode
 import com.kylecorry.sol.science.ecology.Ecology
 import com.kylecorry.sol.science.ecology.GrowingDegreeDaysCalculationType
 import com.kylecorry.sol.science.ecology.LifecycleEvent
 import com.kylecorry.sol.science.ecology.SpeciesPhenology
 import com.kylecorry.sol.science.ecology.triggers.AboveTemperatureTrigger
-import com.kylecorry.sol.science.ecology.triggers.BelowTemperatureTrigger
 import com.kylecorry.sol.science.ecology.triggers.MinimumGrowingDegreeDaysTrigger
 import com.kylecorry.sol.units.Coordinate
 import com.kylecorry.sol.units.Distance
 import com.kylecorry.sol.units.Temperature
 import com.kylecorry.sol.units.TemperatureUnits
-import com.kylecorry.trail_sense.main.getAppService
+import com.kylecorry.trail_sense.shared.andromeda_temp.BelowTemperatureTrigger
 import com.kylecorry.trail_sense.shared.andromeda_temp.OffsetLifecycleEventTrigger
-import com.kylecorry.trail_sense.shared.sensors.LocationSubsystem
-import com.kylecorry.trail_sense.tools.astronomy.domain.AstronomyService
+import com.kylecorry.trail_sense.shared.andromeda_temp.TemperatureTriggerType
 import com.kylecorry.trail_sense.tools.climate.domain.PhenologyService.Companion.EVENT_ACTIVE_END
 import com.kylecorry.trail_sense.tools.climate.domain.PhenologyService.Companion.EVENT_ACTIVE_START
 import com.kylecorry.trail_sense.tools.weather.infrastructure.subsystem.IWeatherSubsystem
 import java.time.Duration
 import java.time.LocalDate
-import java.time.Month
 
 enum class BiologicalActivityType {
     Insect,
@@ -49,7 +44,7 @@ enum class BiologicalActivity(
                 ),
                 LifecycleEvent(
                     EVENT_ACTIVE_END,
-                    BelowTemperatureTrigger(Temperature.celsius(5f))
+                    BelowTemperatureTrigger(Temperature.celsius(5f), TemperatureTriggerType.Low)
                 )
             ),
             growingDegreeDaysCalculationType = GrowingDegreeDaysCalculationType.BaseMax
@@ -72,7 +67,7 @@ enum class BiologicalActivity(
                 ),
                 LifecycleEvent(
                     EVENT_ACTIVE_END,
-                    BelowTemperatureTrigger(Temperature.celsius(0f))
+                    BelowTemperatureTrigger(Temperature.celsius(0f), TemperatureTriggerType.Low)
                 )
             )
         ),
@@ -109,6 +104,7 @@ enum class BiologicalActivity(
             "EF"
         )
     ),
+
     // Deer/horse flies
     Tabanidae(
         BiologicalActivityType.Insect, SpeciesPhenology(
@@ -120,7 +116,7 @@ enum class BiologicalActivity(
                 ),
                 LifecycleEvent(
                     EVENT_ACTIVE_END,
-                    BelowTemperatureTrigger(Temperature.celsius(18f))
+                    BelowTemperatureTrigger(Temperature.celsius(18f), TemperatureTriggerType.High)
                 )
             )
         ),
@@ -207,14 +203,10 @@ class PhenologyService(private val weather: IWeatherSubsystem) {
         elevation: Distance? = null,
         calibrated: Boolean = true
     ): Map<BiologicalActivity, List<Range<LocalDate>>> = onDefault {
-        val actualLocation = location ?: getAppService<LocationSubsystem>().location
-
         val climate = weather.getClimateClassification(location, elevation, calibrated)
         val temperatures = weather.getTemperatureRanges(year, location, elevation, calibrated)
-        val containsLeapDay =
-            temperatures.any { it.first.month == Month.FEBRUARY && it.first.dayOfMonth == 29 }
-
         val activeDays = mutableMapOf<BiologicalActivity, List<Range<LocalDate>>>()
+        val temperatureMap = temperatures.associateBy { it.first }
 
         for (species in BiologicalActivity.entries) {
             if (climate.code in species.excludedClimates) {
@@ -237,11 +229,8 @@ class PhenologyService(private val weather: IWeatherSubsystem) {
                 // TODO: This isn't needed right now, but it should be an interpolated version of astronomy get daylight length (for performance)
                 { Duration.ofHours(12) }
             ) { date ->
-                (if (date.month == Month.FEBRUARY && date.dayOfMonth == 29 && !containsLeapDay) {
-                    temperatures.first { it.first.month == Month.FEBRUARY && it.first.dayOfMonth == 28 }
-                } else {
-                    temperatures.first { it.first.month == date.month && it.first.dayOfMonth == date.dayOfMonth }
-                }).second
+                val newDate = date.withYear(year)
+                temperatureMap[newDate]?.second ?: Range(Temperature.zero, Temperature.zero)
             }
 
             activeDays[species] =
