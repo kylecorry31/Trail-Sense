bb2a554f63e0b7b39c409a472245a9e080a41a00
Kyle Corry <kylecorry31@gmail.com>
2026-02-27 14:00:21 -0500

    refactor: use broadcasts for paths layer
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/paths/map_layers/PathLayer.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/paths/map_layers/PathLayer.kt
@@ -1,54 +1,15 @@
 package com.kylecorry.trail_sense.tools.paths.map_layers
 
-import android.os.Bundle
-import com.kylecorry.andromeda.core.cache.AppServiceRegistry
-import com.kylecorry.luna.coroutines.BackgroundTask
-import com.kylecorry.luna.coroutines.CoroutineQueueRunner
 import com.kylecorry.trail_sense.shared.map_layers.ui.layers.geojson.GeoJsonLayer
-import com.kylecorry.trail_sense.tools.paths.infrastructure.persistence.PathService
+import com.kylecorry.trail_sense.tools.paths.PathsToolRegistration
 import com.kylecorry.trail_sense.tools.sensors.SensorsToolRegistration
-import com.kylecorry.trail_sense.tools.tools.infrastructure.Tools
 
 class PathLayer :
-    GeoJsonLayer<PathGeoJsonSource>(PathGeoJsonSource(), PathGeoJsonSource.SOURCE_ID) {
-
-    private val pathService = AppServiceRegistry.get<PathService>()
-    private val task = BackgroundTask {
-        listenerRunner.skipIfRunning {
-            pathService.getPaths().collect {
-                wasBacktrackOn = pathService.getBacktrackPathId() != null
-                // Paths changed, so we need to reload the paths
-                reload()
-            }
-        }
-    }
-    private val listenerRunner = CoroutineQueueRunner()
-    private var wasBacktrackOn = false
-
-    private val onLocationChanged = { _: Bundle ->
-        if (wasBacktrackOn) {
-            invalidate()
-            notifyListeners()
-        }
-        true
-    }
-
-    override fun start() {
-        Tools.subscribe(SensorsToolRegistration.BROADCAST_LOCATION_CHANGED, onLocationChanged)
-        task.start()
-    }
-
-    override fun stop() {
-        super.stop()
-        Tools.unsubscribe(SensorsToolRegistration.BROADCAST_LOCATION_CHANGED, onLocationChanged)
-        listenerRunner.cancel()
-        task.stop()
-    }
-
-    fun reload() {
-        source.reload()
-        invalidate()
-        notifyListeners()
-    }
-
-}
+    GeoJsonLayer<PathGeoJsonSource>(
+        PathGeoJsonSource(),
+        PathGeoJsonSource.SOURCE_ID,
+        refreshBroadcasts = listOf(
+            SensorsToolRegistration.BROADCAST_LOCATION_CHANGED,
+            PathsToolRegistration.BROADCAST_PATHS_CHANGED
+        )
+    )
