0f74580030b001c7dd4ea4501a62a28ff8da6b67
Kyle Corry <kylecorry31@gmail.com>
2026-02-27 13:48:24 -0500

    refactor: use broadcasts for beacon layer
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/beacons/map_layers/BeaconLayer.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/beacons/map_layers/BeaconLayer.kt
@@ -1,32 +1,13 @@
 package com.kylecorry.trail_sense.tools.beacons.map_layers
 
-import com.kylecorry.andromeda.core.cache.AppServiceRegistry
-import com.kylecorry.luna.coroutines.BackgroundTask
 import com.kylecorry.trail_sense.shared.map_layers.ui.layers.geojson.GeoJsonLayer
-import com.kylecorry.trail_sense.tools.navigation.infrastructure.Navigator
+import com.kylecorry.trail_sense.tools.navigation.NavigationToolRegistration
 
 class BeaconLayer :
     GeoJsonLayer<BeaconGeoJsonSource>(
         BeaconGeoJsonSource(),
-        BeaconGeoJsonSource.SOURCE_ID
-    ) {
-    private val navigator = AppServiceRegistry.get<Navigator>()
-    private var task = BackgroundTask {
-        navigator.destination.collect {
-            source.highlight(it)
-            invalidate()
-            notifyListeners()
-        }
-    }
-
-    override fun start() {
-        super.start()
-        task.start()
-    }
-
-    override fun stop() {
-        super.stop()
-        task.stop()
-    }
-
-}
+        BeaconGeoJsonSource.SOURCE_ID,
+        refreshBroadcasts = listOf(
+            NavigationToolRegistration.BROADCAST_DESTINATION_CHANGED
+        )
+    )
