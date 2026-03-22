7f590655181adb7d81afc41f1d6134ccd4e23d0e
Kyle Corry <kylecorry31@gmail.com>
2026-03-22 08:41:19 -0400

    build: update version number
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.kylecorry.trail_sense"
         minSdk = 23
         targetSdk = 36
-        versionCode = 141
-        versionName = "7.6.1"
+        versionCode = 142
+        versionName = "7.7.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
