40281f8a0b5931b62b40af1f7865dd955db3a02d
Kyle Corry <kylecorry31@gmail.com>
2026-05-04 15:06:21 -0400

    perf: run mapsforge rendering on default dispatcher
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/map/map_layers/OfflineMapTileSource.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/map/map_layers/OfflineMapTileSource.kt
@@ -3,6 +3,7 @@ package com.kylecorry.trail_sense.tools.map.map_layers
 import android.content.Context
 import android.graphics.Bitmap
 import android.os.Bundle
+import com.kylecorry.luna.coroutines.onDefault
 import com.kylecorry.trail_sense.main.getAppService
 import com.kylecorry.trail_sense.shared.map_layers.tiles.Tile
 import com.kylecorry.trail_sense.shared.map_layers.ui.layers.MapLayerParams
@@ -19,14 +20,14 @@ class OfflineMapTileSource : TileSource {
         context: Context,
         tile: Tile,
         params: Bundle
-    ): Bitmap? {
+    ): Bitmap? = onDefault {
         val featureId = params.getString(MapLayerParams.PARAM_FEATURE_ID)?.toLongOrNull()
         val maps = if (featureId == null) {
             repo.getAllSync().filter { it.visible }
         } else {
             listOfNotNull(repo.get(featureId))
         }
-        return renderer.render(context, maps, tile)
+        renderer.render(context, maps, tile)
     }
 
     override suspend fun cleanup() {
