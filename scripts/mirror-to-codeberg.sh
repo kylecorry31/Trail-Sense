81c1eafd558bad84591b55019abdbb8e1d387b4f
Kyle Corry <kylecorry31@gmail.com>
2026-05-11 19:29:15 -0400

    chore: replace actions with local script
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

---
+++ scripts/mirror-to-codeberg.sh
@@ -0,0 +1,2 @@
+git remote add codeberg https://codeberg.org/kylecorry31/Trail-Sense.git
+git push --mirror codeberg
---
+++ scripts/weekly-update.sh
@@ -0,0 +1,3 @@
+python update_contributors.py
+python string-fixer.py
+python update-guide.py
