bc1cb168fff1a34059d53d3439e34f4fed902aa6
Kyle Corry <kylecorry31@gmail.com>
2026-05-24 07:54:12 -0400

    fix: trail map layer resume with reused executor
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/offline_maps/map_layers/TrailMapsTileSource.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/offline_maps/map_layers/TrailMapsTileSource.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.os.Bundle
 import com.kylecorry.luna.coroutines.onDefault
 import com.kylecorry.trail_sense.main.getAppService
+import com.kylecorry.trail_sense.shared.andromeda_temp.MemoryCachedValue
 import com.kylecorry.trail_sense.shared.concurrency.CustomDispatchers
 import com.kylecorry.trail_sense.shared.map_layers.tiles.Tile
 import com.kylecorry.trail_sense.shared.map_layers.ui.layers.MapLayerParams
@@ -12,14 +13,14 @@ import com.kylecorry.trail_sense.shared.map_layers.ui.layers.tiles.TileSource
 import com.kylecorry.trail_sense.tools.offline_maps.domain.vector_maps.VectorMapFileType
 import com.kylecorry.trail_sense.tools.offline_maps.infrastructure.MapService
 import com.kylecorry.trail_sense.tools.offline_maps.infrastructure.vector_maps.mapsforge.MapsforgeTileRenderer
+import kotlinx.coroutines.ExecutorCoroutineDispatcher
 import kotlinx.coroutines.withContext
 
 class TrailMapsTileSource : TileSource {
 
     private val renderer = MapsforgeTileRenderer()
     private val service = getAppService<MapService>()
-
-    private val dispatcher = CustomDispatchers.newFixedThreadDispatcher(name = "TrailMapsTileSource")
+    private val dispatcher = MemoryCachedValue<ExecutorCoroutineDispatcher>(cleanup = { it.close() })
 
     override suspend fun loadTile(
         context: Context,
@@ -33,14 +34,18 @@ class TrailMapsTileSource : TileSource {
         } else {
             listOfNotNull(service.getVectorMap(featureId))
         }.filter { it.type == VectorMapFileType.Mapsforge }
-        withContext(dispatcher) {
+        withContext(getOrCreateDispatcher()) {
             renderer.render(context, maps, tile, highDetailMode)
         }
     }
 
     override suspend fun cleanup() {
         renderer.clear()
-        dispatcher.close()
+        dispatcher.reset()
+    }
+
+    private suspend fun getOrCreateDispatcher(): ExecutorCoroutineDispatcher {
+        return dispatcher.getOrPut { CustomDispatchers.newFixedThreadDispatcher(name = "TrailMapsTileSource") }
     }
 
     companion object {
