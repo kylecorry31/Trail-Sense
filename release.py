522d44961b908ab01ccbe38752019c6aef0d5711
Kyle Corry <kylecorry31@gmail.com>
2026-05-30 18:26:27 -0400

    chore: include milestone link in release notes
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- release.py
+++ release.py
@@ -1,5 +1,7 @@
 import re
 import os
+import subprocess
+import tempfile
 
 # TODO: Build APK
 # apk = input('Is the APK up to date? ').lower().startswith('y')
@@ -11,7 +13,7 @@ gradle = open('app/build.gradle.kts', 'r')
 contents = gradle.read()
 gradle.close()
 
-version_code = re.search('versionCode = (\d+)', contents).group(1)
+version_code = re.search(r'versionCode = (\d+)', contents).group(1)
 version_name = re.search('versionName = "(.+)"', contents).group(1)
 
 changelog = 'fastlane/metadata/android/en-US/changelogs/' + version_code + '.txt'
@@ -21,4 +23,55 @@ if not os.path.exists(changelog):
 
 print("Creating draft release for version " + version_name + " (" + version_code + ")")
 
-os.system("gh release create " + version_name + " -F " + changelog + " -d -t " + version_name)
+milestone_url = None
+milestones = subprocess.run(
+    [
+        "gh",
+        "api",
+        "repos/:owner/:repo/milestones?state=all&per_page=100",
+        "--paginate",
+        "--jq",
+        ".[] | [.title, .html_url] | @tsv",
+    ],
+    capture_output=True,
+    text=True,
+)
+
+if milestones.returncode == 0:
+    for milestone in milestones.stdout.splitlines():
+        if "\t" not in milestone:
+            continue
+        title, url = milestone.split("\t", 1)
+        if title == version_name:
+            milestone_url = url
+            break
+else:
+    print("Unable to check for release milestone")
+
+with open(changelog, "r") as changelog_file:
+    release_notes = changelog_file.read()
+
+if milestone_url:
+    release_notes += "\n\nMilestone: [" + version_name + "](" + milestone_url + ")\n"
+
+with tempfile.NamedTemporaryFile("w", delete=False) as release_notes_file:
+    release_notes_file.write(release_notes)
+    release_notes_path = release_notes_file.name
+
+try:
+    subprocess.run(
+        [
+            "gh",
+            "release",
+            "create",
+            version_name,
+            "-F",
+            release_notes_path,
+            "-d",
+            "-t",
+            version_name,
+        ],
+        check=True,
+    )
+finally:
+    os.remove(release_notes_path)
