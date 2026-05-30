ef38b7d58b36e458a9250d9fd25863b95a03ab10
Kyle Corry <kylecorry31@gmail.com>
2026-05-30 13:43:58 -0400

    feat: m3 expressive tile buttons
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/shared/views/TileButton.kt
+++ app/src/main/java/com/kylecorry/trail_sense/shared/views/TileButton.kt
@@ -1,30 +1,30 @@
 package com.kylecorry.trail_sense.shared.views
 
 import android.content.Context
-import android.content.res.ColorStateList
 import android.util.AttributeSet
 import android.util.TypedValue
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.DrawableRes
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.setPadding
+import com.google.android.material.button.MaterialButton
 import com.kylecorry.andromeda.core.system.Resources
 import com.kylecorry.trail_sense.R
 import com.kylecorry.trail_sense.shared.CustomUiUtils.getColorOnPrimary
-import com.kylecorry.trail_sense.shared.CustomUiUtils.getPrimaryColor
+import kotlin.math.max
+import kotlin.math.min
 
 class TileButton(context: Context, attrs: AttributeSet?) : ConstraintLayout(context, attrs) {
 
     private var textView: TextView
-    private var icon: ImageView
+    private var button: MaterialButton
 
     private var isOn = false
+    private var tilePadding = resources.getDimensionPixelSize(R.dimen.gesture_zone)
 
     init {
         inflate(context, R.layout.view_tile_button, this)
         textView = findViewById(R.id.tile_text)
-        icon = findViewById(R.id.tile_btn)
+        button = findViewById(R.id.tile_btn)
         val a = context.theme.obtainStyledAttributes(attrs, R.styleable.TileButton, 0, 0)
         setImageResource(a.getResourceId(R.styleable.TileButton_tileIcon, R.drawable.flashlight))
         val padding = a.getDimensionPixelSize(R.styleable.TileButton_tilePadding, -1)
@@ -41,29 +41,20 @@ class TileButton(context: Context, attrs: AttributeSet?) : ConstraintLayout(cont
     }
 
     fun setImageResource(@DrawableRes resId: Int) {
-        icon.setImageResource(resId)
+        button.setIconResource(resId)
         setState(isOn)
     }
 
     fun setState(on: Boolean) {
         isOn = on
-        if (isOn) {
-            icon.backgroundTintList = ColorStateList.valueOf(
-                Resources.getPrimaryColor(icon.context)
-            )
-            textView.setTextColor(
-                Resources.getColorOnPrimary(icon.context)
-            )
-            icon.imageTintList = ColorStateList.valueOf(
-                Resources.getColorOnPrimary(icon.context)
-            )
-        } else {
-            textView.setTextColor(Resources.androidTextColorSecondary(icon.context))
-            icon.imageTintList =
-                ColorStateList.valueOf(Resources.androidTextColorSecondary(icon.context))
-            icon.backgroundTintList =
-                ColorStateList.valueOf(Resources.androidBackgroundColorSecondary(icon.context))
-        }
+        button.isChecked = on
+        textView.setTextColor(
+            if (on) {
+                Resources.getColorOnPrimary(context)
+            } else {
+                Resources.androidTextColorSecondary(context)
+            }
+        )
     }
 
     fun setText(text: String?) {
@@ -71,8 +62,32 @@ class TileButton(context: Context, attrs: AttributeSet?) : ConstraintLayout(cont
     }
 
     fun setTilePadding(padding: Int) {
-        icon.setPadding(padding)
+        tilePadding = padding
+        updateIconSize()
     }
 
+    override fun setOnClickListener(listener: OnClickListener?) {
+        button.setOnClickListener(listener)
+    }
+
+    override fun setOnLongClickListener(listener: OnLongClickListener?) {
+        button.setOnLongClickListener(listener)
+    }
+
+    override fun setOnTouchListener(listener: OnTouchListener?) {
+        button.setOnTouchListener(listener)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        updateIconSize()
+    }
+
+    private fun updateIconSize() {
+        if (width == 0 || height == 0) {
+            return
+        }
+        button.iconSize = max(0, min(width, height) - tilePadding * 2)
+    }
 
 }
