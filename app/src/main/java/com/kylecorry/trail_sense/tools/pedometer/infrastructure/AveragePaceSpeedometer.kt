214b2ef4eec22a0e45415ba51dad87aa7c8cbfe4
Kyle Corry <kylecorry31@gmail.com>
2026-08-03 15:56:38 -0400

    pedometer: refresh average pedometer on reset
    When the steps change, immediately trigger a refresh instead of waiting 10 seconds. This fixes the bug where clicking reset does not immediately clear the average speed on the pedometer fragment.
    
    The runner ensures that it can run alongside the timer and will queue up one refresh if another is in flight.
    
    Issue: #3982
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/pedometer/infrastructure/AveragePaceSpeedometer.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/pedometer/infrastructure/AveragePaceSpeedometer.kt
@@ -1,15 +1,19 @@
 package com.kylecorry.trail_sense.tools.pedometer.infrastructure
 
+import android.os.Bundle
 import com.kylecorry.andromeda.core.sensors.AbstractSensor
 import com.kylecorry.andromeda.core.sensors.ISpeedometer
+import com.kylecorry.luna.concurrency.CoroutineQueueRunner
 import com.kylecorry.luna.time.CoroutineTimer
 import com.kylecorry.sol.units.Speed
-import com.kylecorry.trail_sense.tools.pedometer.infrastructure.IPedometerPreferences
 import com.kylecorry.trail_sense.shared.ZERO_SPEED
+import com.kylecorry.trail_sense.tools.pedometer.PedometerToolRegistration
 import com.kylecorry.trail_sense.tools.pedometer.domain.AveragePaceTimeMode
 import com.kylecorry.trail_sense.tools.pedometer.domain.IPaceCalculator
 import com.kylecorry.trail_sense.tools.pedometer.domain.IStepTrackerService
 import com.kylecorry.trail_sense.tools.pedometer.domain.StepTrackingPeriod
+import com.kylecorry.trail_sense.tools.pedometer.infrastructure.IPedometerPreferences
+import com.kylecorry.trail_sense.tools.tools.infrastructure.Tools
 import java.time.Duration
 
 class AveragePaceSpeedometer(
@@ -18,15 +22,10 @@ class AveragePaceSpeedometer(
     private val pedometerPreferences: IPedometerPreferences
 ) : AbstractSensor(), ISpeedometer {
 
-    private val timer = CoroutineTimer {
-        val stepPeriod = stepTrackerService.getOpenStepTrackingPeriod() ?: run {
-            reset()
-            return@CoroutineTimer
-        }
-        speed = paceCalculator.speed(stepPeriod.steps, getDuration(stepPeriod))
-        hasValidReading = true
+    private val runner = CoroutineQueueRunner()
 
-        notifyListeners()
+    private val timer = CoroutineTimer {
+        refresh()
     }
 
     override var hasValidReading: Boolean = false
@@ -37,15 +36,35 @@ class AveragePaceSpeedometer(
 
     override fun startImpl() {
         timer.interval(10000)
+        Tools.subscribe(PedometerToolRegistration.BROADCAST_STEPS_CHANGED, this::onStepsChanged)
     }
 
     override fun stopImpl() {
         timer.stop()
+        Tools.unsubscribe(PedometerToolRegistration.BROADCAST_STEPS_CHANGED, this::onStepsChanged)
+        runner.cancel()
     }
 
     private fun reset() {
         hasValidReading = false
         speed = ZERO_SPEED
+    }
+
+    private suspend fun onStepsChanged(data: Bundle) {
+        refresh()
+    }
+
+    private suspend fun refresh() {
+        runner.enqueue {
+            val stepPeriod = stepTrackerService.getOpenStepTrackingPeriod() ?: run {
+                reset()
+                return@enqueue
+            }
+            speed = paceCalculator.speed(stepPeriod.steps, getDuration(stepPeriod))
+            hasValidReading = true
+
+            notifyListeners()
+        }
     }
 
     private fun getDuration(period: StepTrackingPeriod): Duration {
