b8f0a442ab0718c893ebc1da801a04cf2b0a91d0
Kyle Corry <kylecorry31@gmail.com>
2026-09-12 19:36:33 -0400

    paths: hold wakelock while backtrack is recording
    The wakelock was only being held for a couple of milliseconds while it queued it in the coroutine queue, which could allow the device to fall asleep during a doze maintenance window.
    
    Issue: #4056
    Signed-off-by: Kyle Corry <kylecorry31@gmail.com>

--- app/src/main/java/com/kylecorry/trail_sense/tools/paths/infrastructure/services/BacktrackService.kt
+++ app/src/main/java/com/kylecorry/trail_sense/tools/paths/infrastructure/services/BacktrackService.kt
@@ -7,7 +7,6 @@ import com.kylecorry.andromeda.background.TaskSchedulerFactory
 import com.kylecorry.andromeda.background.services.ForegroundInfo
 import com.kylecorry.andromeda.background.services.IntervalService
 import com.kylecorry.andromeda.sense.location.GPS
-import com.kylecorry.luna.concurrency.CoroutineQueueRunner
 import com.kylecorry.luna.time.CoroutineTimer
 import com.kylecorry.luna.time.FlowableTimer
 import com.kylecorry.luna.time.ITimer
@@ -20,6 +19,7 @@ import com.kylecorry.trail_sense.tools.paths.PathsToolRegistration
 import com.kylecorry.trail_sense.tools.paths.infrastructure.alerts.BacktrackAlerter
 import com.kylecorry.trail_sense.tools.paths.infrastructure.commands.BacktrackCommand
 import com.kylecorry.trail_sense.tools.tools.infrastructure.Tools
+import kotlinx.coroutines.sync.Mutex
 import java.time.Duration
 
 class BacktrackService :
@@ -68,11 +68,17 @@ class BacktrackService :
     override val period: Duration
         get() = prefs.backtrackRecordFrequency
 
-    private val runner = CoroutineQueueRunner()
+    private val recordLock = Mutex()
 
     override suspend fun doWork() {
-        runner.skipIfRunning {
+        if (!recordLock.tryLock()) {
+            return
+        }
+
+        try {
             backtrackCommand.execute()
+        } finally {
+            recordLock.unlock()
         }
     }
 
@@ -85,7 +91,6 @@ class BacktrackService :
 
     override fun onDestroy() {
         isRunning = false
-        runner.cancel()
         stopService(true)
         super.onDestroy()
     }
